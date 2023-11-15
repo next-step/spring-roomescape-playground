53ab245fbb85c018e2d92409cf5903e3a4d5e7b1
llddang <bodyness@naver.com>
2023-11-14 22:44:08 -0500

    [FEAT] MVC 1단계 홈 화면 구현

---
+++ src/main/java/roomescape/HomeController.java
@@ -0,0 +1,13 @@
+package roomescape;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+  @GetMapping(value = {"/", "/home"})
+  public String home(Model model){
+    return "home";
+  }
+}
