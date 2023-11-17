a440b20c36c22c8886db4028fdb70d3d62d7876f
shkisme <shkisme0130@gmail.com>
2023-11-17 23:30:22 +0900

    feat: 예약 리스트 조회 API 구현

--- src/main/java/roomescape/domain/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/domain/reservation/controller/ReservationController.java
@@ -1,13 +1,26 @@
 package roomescape.domain.reservation.controller;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import roomescape.domain.reservation.dao.ReservationRepository;
+import roomescape.domain.reservation.model.Reservation;
 
 @Controller
+@RequiredArgsConstructor
 public class ReservationController {
+    private final ReservationRepository reservationRepository;
 
     @GetMapping("/reservation")
     public String getReservationPage() {
         return "reservation";
+    }
+
+    @ResponseBody
+    @GetMapping("/reservations")
+    public List<Reservation> getReservations() {
+        return reservationRepository.findAll();
     }
 }
