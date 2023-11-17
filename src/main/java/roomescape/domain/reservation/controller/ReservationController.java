44f4c786ec59d26af2298a37f9465538eb377f4c
shkisme <shkisme0130@gmail.com>
2023-11-18 02:54:27 +0900

    fix: RestController -> Controller로 변경

--- src/main/java/roomescape/domain/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/domain/reservation/controller/ReservationController.java
@@ -7,19 +7,19 @@ import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.reservation.dao.ReservationRepository;
 import roomescape.domain.reservation.dto.CreateReservationRequest;
 import roomescape.domain.reservation.model.Reservation;
 
 @Validated
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ReservationController {
     private final ReservationRepository reservationRepository;
