1dae0c3f8ea4d5bbd4308a75001143b13be2cf0d
llddang <bodyness@naver.com>
2023-11-17 17:51:10 -0500

    [FEAT] MVC 3단계 예약 추가/취소 구현

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -20,6 +20,12 @@ public class Reservation {
         this.time = time;
     }
 
+    public Reservation(String name, LocalDate date, LocalTime time){
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
     public Long getId(){
         return id;
     }
@@ -32,5 +38,9 @@ public class Reservation {
     public String getTime(){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         return time.format(formatter);
+    }
+
+    public static Reservation toEntity(Reservation reservation, Long id){
+        return new Reservation(id, reservation.name, reservation.date, reservation.time);
     }
 }
--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,14 +1,24 @@
 package roomescape;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ReservationController {
     private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
 
     @GetMapping("reservation")
     public String reservation(){
@@ -18,5 +28,28 @@ public class ReservationController {
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> read(){
         return ResponseEntity.ok().body(reservations);
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> create(@RequestBody Reservation reservation){
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        reservations.add(newReservation);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/reservations/" + newReservation.getId()));
+
+        return new ResponseEntity<>(newReservation, headers, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        Reservation reservation = reservations.stream()
+            .filter(it -> Objects.equals(it.getId(), id))
+            .findFirst()
+            .orElseThrow(RuntimeException::new);
+
+        reservations.remove(reservation);
+
+        return ResponseEntity.noContent().build();
     }
 }
