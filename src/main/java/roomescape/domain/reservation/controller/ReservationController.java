8887f47edbf9a92a487b4638bce3b98c48e166a2
shkisme <shkisme0130@gmail.com>
2023-11-18 00:22:13 +0900

    feat: 예약 추가 API 구현

--- src/main/java/roomescape/domain/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/domain/reservation/controller/ReservationController.java
@@ -1,11 +1,18 @@
 package roomescape.domain.reservation.controller;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
+import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import roomescape.domain.reservation.dao.ReservationRepository;
+import roomescape.domain.reservation.dto.CreateReservationRequest;
 import roomescape.domain.reservation.model.Reservation;
 
 @Controller
@@ -22,5 +29,15 @@ public class ReservationController {
     @GetMapping("/reservations")
     public List<Reservation> getReservations() {
         return reservationRepository.findAll();
+    }
+
+    @ResponseBody
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> createReservation(@RequestBody CreateReservationRequest requestDto) {
+        Reservation reservation = requestDto.toEntity();
+        reservationRepository.save(reservation);
+        return ResponseEntity.status(CREATED)
+                .location(URI.create("/reservations/" + reservation.getId()))
+                .body(reservation);
     }
 }
