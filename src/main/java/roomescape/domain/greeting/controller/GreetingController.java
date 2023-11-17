c11569b6de21bb540310820b9faac19b63365dc9
shkisme <shkisme0130@gmail.com>
2023-11-17 23:13:13 +0900

    refactor: 디렉토리 구조 변경

--- src/main/java/roomescape/domain/greeting/controller/GreetingController.java
+++ src/main/java/roomescape/domain/greeting/controller/GreetingController.java
@@ -1,10 +1,10 @@
-package roomescape.controller;
+package roomescape.domain.greeting.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.domain.Greeting;
+import roomescape.domain.greeting.model.Greeting;
 
 @RestController
 public class GreetingController {
--- src/main/java/roomescape/domain/greeting/model/Greeting.java
+++ src/main/java/roomescape/domain/greeting/model/Greeting.java
@@ -1,4 +1,4 @@
-package roomescape.domain;
+package roomescape.domain.greeting.model;
 
 public class Greeting {
 
