ac2080ec8827f905417d94416e60708a1b2272f2
amaran-th <songsy405@naver.com>
2023-11-17 16:17:28 +0900

    test: 예약 목록 조회 요청 API 검증 테스트 구현

---
+++ src/test/java/roomescape/api/ReservationControllerTest.java
@@ -0,0 +1,24 @@
+package roomescape.api;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.hamcrest.Matchers.is;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class ReservationControllerTest {
+
+    @Test
+    @DisplayName("예약 목록을 조회할 수 있다.")
+    void getReservations() {
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(2));
+    }
+}
