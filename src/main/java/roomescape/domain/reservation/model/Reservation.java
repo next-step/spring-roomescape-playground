20b7671907f391a1525ed774d840497cde16e501
shkisme <shkisme0130@gmail.com>
2023-11-18 00:21:55 +0900

    feat: 예약 객체 빌더 작성

--- src/main/java/roomescape/domain/reservation/model/Reservation.java
+++ src/main/java/roomescape/domain/reservation/model/Reservation.java
@@ -2,13 +2,24 @@ package roomescape.domain.reservation.model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import lombok.RequiredArgsConstructor;
+import java.util.concurrent.atomic.AtomicLong;
+import lombok.Builder;
+import lombok.Getter;
 
-@RequiredArgsConstructor
+@Getter
 public class Reservation {
+    private static AtomicLong INDEX = new AtomicLong(0);
 
     private Long id;
     private String name;
     private LocalDate date;
     private LocalTime time;
+
+    @Builder
+    private Reservation(String name, LocalDate date, LocalTime time) {
+        this.id = INDEX.incrementAndGet();
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
 }
