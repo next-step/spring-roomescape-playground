24ffb09ee766e69187d77bff7e1126591e9f6391
pipisebastian <yrt7998@pusan.ac.kr>
2023-11-17 20:07:57 +0900

    feat: 예외처리 추가

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -37,6 +37,13 @@ public class ReservationController {
 
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> create(@RequestBody Reservation reservation){
+        if (reservation.getName() == null || reservation.getDate() == null || reservation.getTime() == null) {
+            throw new NotFoundReservationException("빈 값이 존재합니다!");
+        }
+        if (reservations.stream().anyMatch(r -> Objects.equals(r.getName(), reservation.getName()))) {
+            throw new NotFoundReservationException("이미 존재하는 id 입니다!");
+        }
+
         Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
         reservations.add(newReservation);
 
@@ -50,12 +57,21 @@ public class ReservationController {
         Reservation reservation = reservations.stream()
                 .filter(it -> Objects.equals(it.getId(), id))
                 .findFirst()
-                .orElseThrow();
+                .orElseThrow(() -> new NotFoundReservationException("id를 찾을 수 없습니다!"));
 
         reservations.remove(reservation);
 
         return ResponseEntity.noContent().build();
     }
 
+    @ExceptionHandler(NotFoundReservationException.class)
+    public ResponseEntity<String> handleException(NotFoundReservationException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
 
+    public static class NotFoundReservationException extends RuntimeException {
+        public NotFoundReservationException(String message) {
+            super(message);
+        }
+    }
 }
