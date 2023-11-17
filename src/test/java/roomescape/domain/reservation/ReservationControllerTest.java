f00842bebe098c285c3156baf9e22a61279edf62
shkisme <shkisme0130@gmail.com>
2023-11-17 23:37:06 +0900

    test: 예약 리스트 조회 응답 성공 테스트 작성

--- src/test/java/roomescape/domain/reservation/ReservationControllerTest.java
+++ src/test/java/roomescape/domain/reservation/ReservationControllerTest.java
@@ -15,4 +15,14 @@ public class ReservationControllerTest extends RoomescapeApplicationTest {
                 .then().log().all()
                 .statusCode(200);
     }
+
+    @Test
+    @DisplayName("예약 리스트 조회 시 200 OK를 반환한다.")
+    public void 예약_리스트_조회_시_200_OK를_반환한다() {
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200);
+        //  .body("size()", is(3)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
+    }
 }
