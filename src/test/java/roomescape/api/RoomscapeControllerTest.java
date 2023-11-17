14f47d6ed239dedbade40dcbf3489f654112206c
amaran-th <songsy405@naver.com>
2023-11-17 15:14:38 +0900

    test: RoomscapeController 테스트의 파일명, 테스트명 변경

--- src/test/java/roomescape/api/RoomscapeControllerTest.java
+++ src/test/java/roomescape/api/RoomscapeControllerTest.java
@@ -1,16 +1,18 @@
-package roomescape;
+package roomescape.api;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+public class RoomscapeControllerTest {
 
     @Test
-    void 일단계() {
+    @DisplayName("/경로로 GET 요청을 보내면 홈 화면을 반환한다.")
+    void home() {
         RestAssured.given().log().all()
                 .when().get("/")
                 .then().log().all()
