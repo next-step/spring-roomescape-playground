5b243865e4732b4966912f4fc1fa72dc749ed6cc
lcqff <yeonnu82g@gmail.com>
2023-11-17 15:56:23 +0900

    feat: 예약 조회 기능 작성

--- src/main/java/roomescape/controller/RoomescapeController.java
+++ src/main/java/roomescape/controller/RoomescapeController.java
@@ -1,13 +1,17 @@
 package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RoomescapeController {
     @GetMapping("/")
-    public String loadHome() {
+    public String loadAdminPage() {
         return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String loadReservationPage() {
+        return "reservation";
     }
 }
