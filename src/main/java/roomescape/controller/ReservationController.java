438dafa548e616586c1fde469713e7f917bf110b
lcqff <yeonnu82g@gmail.com>
2023-11-17 16:53:38 +0900

    feat: 예약 추가, 취소 기능 작성

---
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -0,0 +1,50 @@
+package roomescape.controller;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import roomescape.repository.Reservation;
+
+@Controller
+public class ReservationController {
+    private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(1);
+
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        reservations.add(newReservation);
+        return ResponseEntity
+                .created(URI.create("/reservations/" + newReservation.getId()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(newReservation);
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> readReservation() {
+        return ResponseEntity.ok().body(reservations);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+
+        reservations.remove(reservation);
+
+        return ResponseEntity.noContent().build();
+    }
+}
