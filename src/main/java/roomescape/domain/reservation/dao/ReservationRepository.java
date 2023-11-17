7ec64471e99583b5401f996ed183de729406ec58
shkisme <shkisme0130@gmail.com>
2023-11-17 23:30:08 +0900

    feat: ReservationRepository 생성, 모든 예약을 조회하는 메서드 작성

---
+++ src/main/java/roomescape/domain/reservation/dao/ReservationRepository.java
@@ -0,0 +1,16 @@
+package roomescape.domain.reservation.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import roomescape.domain.reservation.model.Reservation;
+
+@Repository
+public class ReservationRepository {
+
+    private final List<Reservation> reservations = new ArrayList<>();
+
+    public List<Reservation> findAll() {
+        return reservations;
+    }
+}
