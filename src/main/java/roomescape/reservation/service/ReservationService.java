7e71926829da53b2b6666cdc11d3cceead9daa5b
sososo0 <sh0000@pusan.ac.kr>
2023-11-19 01:14:06 +0900

    fix: Add transactional annotation in reservation service

--- src/main/java/roomescape/reservation/service/ReservationService.java
+++ src/main/java/roomescape/reservation/service/ReservationService.java
@@ -8,7 +8,6 @@ import roomescape.reservation.dto.request.ReservationRequest;
 import roomescape.reservation.dto.response.ReservationResponse;
 import roomescape.reservation.repository.ReservationRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Transactional(readOnly = true)
@@ -18,16 +17,19 @@ public class ReservationService {
 
     private final ReservationRepository reservationRepository;
 
+    @Transactional
     public List<Reservation> findReservations() {
         return reservationRepository.findAll();
     }
 
+    @Transactional
     public ReservationResponse.createReservationDto saveReservation(ReservationRequest.CreateReservationDto request) {
         Reservation reservation = request.toEntity();
         reservationRepository.save(reservation);
         return new ReservationResponse.createReservationDto(reservation);
     }
 
+    @Transactional
     public void removeReservation(Long id) {
         Reservation reservation = reservationRepository.findById(id)
                 .orElseThrow();
