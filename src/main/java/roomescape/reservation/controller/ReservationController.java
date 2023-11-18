19fc434d78a9b8955f9b198a474e47f565cbd62f
sososo0 <sh0000@pusan.ac.kr>
2023-11-18 23:03:39 +0900

    fix: Rename get reservation controller func

--- src/main/java/roomescape/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/reservation/controller/ReservationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ReservationController {
     @GetMapping("/reservation")
-    public String reservation(){
+    public String getReservation(){
         return "reservation";
     }
 }
