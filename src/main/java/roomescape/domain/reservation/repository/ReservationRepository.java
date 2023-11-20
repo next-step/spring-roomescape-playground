4aefd7c1e4f6f4ecfe3f79c8196fe3f2cfffee6b
이수빈 <02ggang9@gmail.com>
2023-11-20 17:28:22 +0900

    [Spring MVC] 짱구(이수빈) 미션 제출합니다. (#10)
    * Add files via upload
    
    * Delete domain/reservation directory
    
    * Delete global directory
    
    * Add files via upload
    
    * Delete src/test/java/roomescape/MissionStepTest.java
    
    * Add files via upload

---
+++ src/main/java/roomescape/domain/reservation/repository/ReservationRepository.java
@@ -0,0 +1,54 @@
+package roomescape.domain.reservation.repository;
+
+import org.springframework.stereotype.Repository;
+import roomescape.domain.reservation.entity.Reservation;
+import roomescape.global.BusinessException;
+import roomescape.global.ErrorCode;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.IntStream;
+
+import static roomescape.global.ErrorCode.*;
+
+@Repository
+public class ReservationRepository {
+
+    private static final Map<Long, Reservation> db = new HashMap<>();
+
+    private static AtomicLong id = new AtomicLong(1);
+
+    public void save(Reservation reservation) {
+        db.put(id.getAndIncrement(), reservation);
+
+    }
+
+    public List<Reservation> findAll() {
+        return db.values()
+                .stream()
+                .toList();
+    }
+
+    public Reservation findById(Long id) {
+        return db.values()
+                .stream()
+                .filter(reservation -> reservation.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new BusinessException(id, "reservationId", RESERVATION_NOT_FOUND));
+    }
+
+    public void deleteById(Long id) {
+        List<Long> reservationLogIds = db.entrySet().stream()
+                .filter(entry -> entry.getValue().getId().equals(id))
+                .map(Map.Entry::getKey)
+                .toList();
+
+        reservationLogIds.forEach(db::remove);
+    }
+
+    public void clear() {
+        db.clear();
+        id.set(0);
+    }
+
+}
