8e1553da19a7c5d0cb196e8c11c1d1b3b3b36be0
최준호 <junho5336@gmail.com>
2023-11-20 14:08:34 +0900

    feat: 2단계 - 예약 조회

---
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -0,0 +1,33 @@
+package roomescape.controller;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import roomescape.domain.Reservation;
+
+@Controller
+public class ReservationController {
+
+    private final AtomicLong id = new AtomicLong(1L);
+    private final List<Reservation> reservations = List.of(
+        new Reservation(id.getAndAdd(1L), "브라운", LocalDate.now(), LocalTime.now()),
+        new Reservation(id.getAndAdd(1L), "브리", LocalDate.now(), LocalTime.now()),
+        new Reservation(id.getAndAdd(1L), "주노", LocalDate.now(), LocalTime.now())
+    );
+
+    @GetMapping("/reservation")
+    public String reservationPage(Model model) {
+        return "reservation.html";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> reservations() {
+        return reservations;
+    }
+}
