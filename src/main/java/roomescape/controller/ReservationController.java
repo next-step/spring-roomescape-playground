d519262d11c8d68b830b01a70f70bc9a6b65c829
최준호 <junho5336@gmail.com>
2023-11-22 04:24:34 +0900

    feat: 예외 발생 시 400 응답 추가

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,5 +1,6 @@
 package roomescape.controller;
 
+import jakarta.validation.Valid;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import roomescape.controller.dto.CreateReservation;
 import roomescape.controller.dto.ReservationResponse;
@@ -35,7 +37,7 @@ public class ReservationController {
     }
 
     @PostMapping("/reservations")
-    public ResponseEntity<Reservation> create(CreateReservation request) {
+    public ResponseEntity<Reservation> create(@Valid @RequestBody CreateReservation request) {
         Reservation reservation = request.toReservation(id.incrementAndGet());
         reservations.add(reservation);
         return ResponseEntity.created(URI.create("/reservations/" + reservation.getId())).body(reservation);
@@ -46,7 +48,7 @@ public class ReservationController {
         Reservation reservation = reservations.stream()
             .filter(it -> it.getId() == id)
             .findAny()
-            .orElseGet(null);
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 예약입니다."));
 
         reservations.remove(reservation);
         return ResponseEntity.noContent().build();
--- src/main/java/roomescape/controller/dto/CreateReservation.java
+++ src/main/java/roomescape/controller/dto/CreateReservation.java
@@ -1,14 +1,16 @@
 package roomescape.controller.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import org.springframework.format.annotation.DateTimeFormat;
 import roomescape.domain.Reservation;
 
 public record CreateReservation(
-    @DateTimeFormat(pattern = "yyyy-mm-dd") LocalDate date,
-    String name,
-    @DateTimeFormat(pattern = "hh:mm") LocalTime time
+    @NotNull @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
+    @NotBlank String name,
+    @NotNull @DateTimeFormat(pattern = "hh:mm") LocalTime time
 ) {
 
     public Reservation toReservation(Long id) {
