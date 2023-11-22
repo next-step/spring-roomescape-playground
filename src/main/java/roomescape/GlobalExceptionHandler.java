dbf18944eafcc64a66659eb4bfa2141cc2804147
Invidam <hansupark0208@gmail.com>
2023-11-22 23:51:22 +0900

    feat: 로깅 이용하여 검증하도록 추가

--- src/main/java/roomescape/GlobalExceptionHandler.java
+++ src/main/java/roomescape/GlobalExceptionHandler.java
@@ -1,18 +1,24 @@
 package roomescape;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
 	@ExceptionHandler(DomainEmptyFieldException.class)
-	public ResponseEntity handleException(DomainEmptyFieldException e) {
+	public ResponseEntity<Void> handleException(DomainEmptyFieldException e) {
+		logger.warn("warn", e);
 		return ResponseEntity.badRequest().build();
 	}
 
 	@ExceptionHandler(NotFoundReservationException.class)
-	public ResponseEntity handleException(NotFoundReservationException e) {
+	public ResponseEntity<Void> handleException(NotFoundReservationException e) {
+		logger.warn("warn", e);
 		return ResponseEntity.badRequest().build();
 	}
 }
