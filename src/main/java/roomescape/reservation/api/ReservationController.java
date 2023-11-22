af70eaff56e022281a85da5fa48e31bf83db9804
Jang seo yun <81643702+pipisebastian@users.noreply.github.com>
2023-11-22 12:33:13 +0900

    [Spring MVC] pipi(장서윤) 미션 제출합니다 (#5)
    * chore: build.gradle 추가
    
    * feat: 홈화면 응답 구현
    
    * feat: Reservation 클래스 구현
    
    * feat: Reservation 컨트롤러 구현
    
    * feat: Reservation post, delete 메서드 구현
    
    * test: 미션2 테스트 코드 작성
    
    * test: 미션3 테스트 코드 작성
    
    * test: 미션4 테스트 코드 작성
    
    * feat: 예외처리 추가
    
    * refactor :  코드리뷰 반영 및 폴더구조 변경

---
+++ src/main/java/roomescape/reservation/api/ReservationController.java
@@ -0,0 +1,64 @@
+package roomescape.reservation.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import roomescape.reservation.domain.Reservation;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+    private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
+
+    @GetMapping
+    public ResponseEntity<List<Reservation>> read() {
+        return ResponseEntity.status(HttpStatus.OK).body(reservations);
+    }
+
+    @PostMapping
+    public ResponseEntity<Reservation> create(@RequestBody Reservation reservation){
+        if (reservation.getName() == null || reservation.getDate() == null || reservation.getTime().isEmpty()) {
+            throw new NotFoundReservationException("빈 값이 존재합니다!");
+        }
+        if (reservations.stream().anyMatch(r -> Objects.equals(r.getName(), reservation.getName()))) {
+            throw new NotFoundReservationException("이미 존재하는 id 입니다!");
+        }
+
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        reservations.add(newReservation);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .location(URI.create("/reservations/" + newReservation.getId()))
+                .body(newReservation);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElseThrow(() -> new NotFoundReservationException("id를 찾을 수 없습니다!"));
+
+        reservations.remove(reservation);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(NotFoundReservationException.class)
+    public ResponseEntity<String> handleException(NotFoundReservationException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    public static class NotFoundReservationException extends RuntimeException {
+        public NotFoundReservationException(String message) {
+            super(message);
+        }
+    }
+}
