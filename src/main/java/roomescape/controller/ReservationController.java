62b45e47abbe888265146cf8267d6e0af4ab4e81
lcqff <yeonnu82g@gmail.com>
2023-11-23 18:06:12 +0900

    feat: 데이터 베이스를 사용한 데이터 조회 기능 추가

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -4,12 +4,12 @@ import static roomescape.utils.ErrorMessage.*;
 import static roomescape.validation.ReservationValidation.validate;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,13 +17,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.exception.NotFoundReservationException;
 import roomescape.domain.Reservation;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
-    private final List<Reservation> reservations = new ArrayList<>();
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    //private final List<Reservation> reservations = new ArrayList<>();
     private final AtomicLong index = new AtomicLong(1);
 
 
@@ -32,7 +34,9 @@ public class ReservationController {
         validate(reservation);
 
         Reservation newReservation = Reservation.createWithId(reservation, index.getAndIncrement());
-        reservations.add(newReservation);
+        //reservations.add(newReservation);
+        jdbcTemplate.update("insert into reservation (name, date, time) values (?, ?, ?)", reservation.name(),
+                reservation.date(), reservation.time());
         return ResponseEntity
                 .created(URI.create("/reservations/" + newReservation.id()))
                 .contentType(MediaType.APPLICATION_JSON)
@@ -41,17 +45,26 @@ public class ReservationController {
 
     @GetMapping
     public ResponseEntity<List<Reservation>> readReservation() {
+        List<Reservation> reservations = jdbcTemplate.query(
+                "select id, name, date, time from reservation",
+                (resultSet, rowNum) -> Reservation.of(
+                        resultSet.getLong("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("date"),
+                        resultSet.getString("time")
+                ));
+
         return ResponseEntity.ok().body(reservations);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable AtomicLong id) {
-        Reservation reservation = reservations.stream()
-                .filter(it -> Objects.equals(it.id().get(), id.get()))
-                .findFirst()
-                .orElseThrow(() -> new NotFoundReservationException(NON_EXISTING_RESERVATION));
-
-        reservations.remove(reservation);
+//        Reservation reservation = reservations.stream()
+//                .filter(it -> Objects.equals(it.id().get(), id.get()))
+//                .findFirst()
+//                .orElseThrow(() -> new NotFoundReservationException(NON_EXISTING_RESERVATION));
+//
+//        reservations.remove(reservation);
 
         return ResponseEntity.noContent().build();
     }
--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -4,7 +4,13 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public record Reservation(AtomicLong id, String name, String date, String time) {
 
+    public static Reservation of(long id, String name, String date, String time) {
+        return new Reservation(new AtomicLong(id), name, date, time);
+
+    }
+
     public static Reservation createWithId(Reservation reservation, long id) {
         return new Reservation(new AtomicLong(id), reservation.name, reservation.date, reservation.time);
     }
+
 }
