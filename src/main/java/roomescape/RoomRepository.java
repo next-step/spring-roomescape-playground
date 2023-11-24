adfffd7d177ec9e8959708d8384307e710c011ba
Invidam <hansupark0208@gmail.com>
2023-11-24 13:32:36 +0900

    refactor: 불필요한 static 키워드 제거

--- src/main/java/roomescape/RoomRepository.java
+++ src/main/java/roomescape/RoomRepository.java
@@ -6,29 +6,29 @@
   import java.util.concurrent.atomic.AtomicLong;
 
 public class RoomRepository {
-	private static AtomicLong counter = new AtomicLong();
-	private static Map<Long, Room> repository = new HashMap<>();
+	private static final AtomicLong counter = new AtomicLong();
+	private static final Map<Long, Room> repository = new HashMap<>();
 
-	public static Long save(Room room) {
+	public Long save(Room room) {
 		room.setId(counter.incrementAndGet());
 		repository.put(room.getId(), room);
 		return room.getId();
 	}
 
-	public static Room findById(Long id) {
+	public Room findById(Long id) {
 		return repository.get(id);
 	}
 
-	public static List<Room> findAll() {
+	public List<Room> findAll() {
 		return repository.values().stream().toList();
 	}
 
-	public static void deleteById(Long id) {
+	public void deleteById(Long id) {
 		Room result = repository.remove(id);
 		if(result == null) throw new NotFoundReservationException("삭제하려는 예약이 존재하지 않습니다.");
 	}
 
-	public static void clear() {
+	public void clear() {
 		counter.set(0);
 		repository.clear();
 	}
