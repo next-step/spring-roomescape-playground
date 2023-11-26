145468b8850183cbd2b995881b87d66e3e5abf7e
songsunkook <songsunkook@gmail.com>
2023-11-27 00:50:58 +0900

    feat: 예약 추가 / 취소 (임시) 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,9 +1,17 @@
 package roomescape.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import roomescape.dto.Reservation;
@@ -22,5 +30,20 @@ public class ReservationController {
     @GetMapping("/reservations")
     public List<Reservation> reservations() {
         return reservationService.getAllReservations();
+    }
+
+    @ResponseBody
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation) throws URISyntaxException {
+        Reservation responseReservation = reservationService.addReservation(reservation);
+        return ResponseEntity.created(new URI("/reservations/" + responseReservation.id()))
+            .body(responseReservation);
+    }
+
+    @ResponseBody
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id) {
+        reservationService.deleteReservation(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -2,21 +2,28 @@ package roomescape.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 import roomescape.dto.Reservation;
 
 public class ReservationService {
     private List<Reservation> reservations = new ArrayList<>();
-
-    // 1-2 예약조회 테스트용
-    {
-        reservations.addAll(List.of(
-            new Reservation(1, "브라운", "2023-01-01", "10:00"),
-            new Reservation(2, "브라운", "2023-01-02", "11:00")
-        ));
-    }
+    private AtomicLong index = new AtomicLong(1);
 
     public List<Reservation> getAllReservations() {
-        return reservations;
+        return reservations.stream()
+            .filter(reservation -> reservation.id() != null)
+            .toList();
+    }
+
+    public Reservation addReservation(Reservation reservation) {
+        reservations.add(new Reservation(index.get(), reservation.name(), reservation.date(), reservation.time()));
+        return reservations.get((int)index.getAndIncrement() - 1);
+    }
+
+    public void deleteReservation(Long id) {
+        // id 유지를 위해 리스트에서 제거된 객체는 모든 요소를 null로 유지
+        // DB가 적용되면서 바뀔 예정
+        reservations.set(id.intValue() - 1, new Reservation(null, null, null, null));
     }
 }
