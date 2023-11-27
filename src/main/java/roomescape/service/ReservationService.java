2d4be327a1abdece8f32cc81dc71e16842e460dd
songsunkook <songsunkook@gmail.com>
2023-11-27 21:46:41 +0900

    feat: 예외 처리 구현

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -7,8 +7,8 @@ import java.util.concurrent.atomic.AtomicLong;
 import roomescape.dto.Reservation;
 
 public class ReservationService {
-    private List<Reservation> reservations = new ArrayList<>();
-    private AtomicLong index = new AtomicLong(1);
+    private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(1);
 
     public List<Reservation> getAllReservations() {
         return reservations.stream()
@@ -17,13 +17,27 @@ public class ReservationService {
     }
 
     public Reservation addReservation(Reservation reservation) {
+        validateAddReservation(reservation);
         reservations.add(new Reservation(index.get(), reservation.name(), reservation.date(), reservation.time()));
         return reservations.get((int)index.getAndIncrement() - 1);
+    }
+
+    private void validateAddReservation(Reservation reservation) {
+        if (reservation.name().isEmpty() || reservation.date().isEmpty() || reservation.time().isEmpty()) {
+            throw new IllegalArgumentException("필수 필드가 비어있습니다.");
+        }
     }
 
     public void deleteReservation(Long id) {
         // id 유지를 위해 리스트에서 제거된 객체는 모든 요소를 null로 유지
         // DB가 적용되면서 바뀔 예정
+        validateDeleteReservation(id);
         reservations.set(id.intValue() - 1, new Reservation(null, null, null, null));
+    }
+
+    private void validateDeleteReservation(Long id) {
+        if (reservations.size() <= id || reservations.get(id.intValue()).id() == null) {
+            throw new IllegalArgumentException("존재하지 않는 id입니다.");
+        }
     }
 }
