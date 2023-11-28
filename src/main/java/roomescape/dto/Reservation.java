67eb2eb19fd4b3dc65c145ef7deb58ddd6de8f61
stopmin <geemin0@naver.com>
2023-11-28 14:30:22 +0900

    fix: constructor 수정

--- src/main/java/roomescape/dto/Reservation.java
+++ src/main/java/roomescape/dto/Reservation.java
@@ -2,24 +2,31 @@ package roomescape.dto;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.concurrent.atomic.AtomicLong;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class Reservation {
-    private long id = 0;
+    private long id;
     private String name;
     private LocalDate date;
+    private LocalTime time;
 
-    public Reservation(long id, String name, LocalDate date, LocalTime time) {
+    @JsonCreator
+    public Reservation(
+            @JsonProperty("id") long id,
+            @JsonProperty("name") String name,
+            @JsonProperty("date") LocalDate date,
+            @JsonProperty("time") LocalTime time
+    ) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
     }
 
-    private LocalTime time;
 
     public long getId() {
         return id;
