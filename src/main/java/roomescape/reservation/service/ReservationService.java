f0ecacd9f6141e27bc7784cccd10751a2783afcc
pipisebastian <pipisebastian@pusan.ac.kr>
2023-12-02 13:40:32 +0900

    refactor : transaction, lombok 어노테이션 추가

--- src/main/java/roomescape/reservation/service/ReservationService.java
+++ src/main/java/roomescape/reservation/service/ReservationService.java
@@ -1,7 +1,8 @@
 package roomescape.reservation.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.reservation.dao.QueryRepository;
 import roomescape.reservation.dao.UpdateRepository;
 import roomescape.reservation.domain.Reservation;
@@ -11,21 +12,18 @@ import java.time.LocalTime;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ReservationService {
 
     private final QueryRepository queryRepository;
     private final UpdateRepository updateRepository;
 
-    @Autowired
-    public ReservationService(QueryRepository queryRepository, UpdateRepository updateRepository) {
-        this.queryRepository = queryRepository;
-        this.updateRepository = updateRepository;
-    }
-
+    @Transactional(readOnly = true)
     public List<Reservation> getAllReservations() {
         return queryRepository.getAllReservations();
     }
 
+    @Transactional
     public Reservation createReservation(String name, LocalDate date, LocalTime time) {
         if (date == null || name == null || time == null ) {
             throw new NotFoundReservationException("빈 값이 존재합니다!");
@@ -34,6 +32,7 @@ public class ReservationService {
         return updateRepository.insert(name, date, time);
     }
 
+    @Transactional
     public void deleteReservation(Long id) {
         updateRepository.delete(id);
     }
