4a147b4a4fb1317036f8a92397099a848c6d6a81
sososo0 <sh0000@pusan.ac.kr>
2023-12-03 18:17:11 +0900

    feat: Add find reservation and time query

--- src/main/java/roomescape/time/repository/TimeRepository.java
+++ src/main/java/roomescape/time/repository/TimeRepository.java
@@ -1,7 +1,23 @@
 package roomescape.time.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import roomescape.time.domain.Time;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface TimeRepository extends JpaRepository<Time, Long> {
+    @Query(
+            "SELECT " +
+                    "r.id as reservation_id, " +
+                    "r.name, " +
+                    "r.date, " +
+                    "t.id as time_id, " +
+                    "t.time as time_value " +
+                    "FROM Reservation as r INNER JOIN Time as t ON r.time.id = t.id " +
+                    "WHERE t.id = :timeId"
+    )
+    List<Object[]> findReservationsWithTimeById(@Param("timeId") Long timeId);
 }
