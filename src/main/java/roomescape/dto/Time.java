73e6cf262e02459b09370c9f46ed351b7fc5f654
stopmin <geemin0@naver.com>
2023-12-05 23:57:59 +0900

    chore: time 객체 추가

---
+++ src/main/java/roomescape/dto/Time.java
@@ -0,0 +1,26 @@
+package roomescape.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.time.LocalTime;
+
+@Getter
+public class Time {
+    private long id;
+    private LocalTime time;
+
+    @JsonCreator
+    public Time(
+            @JsonProperty("id") long id,
+            @JsonProperty("time") LocalTime time
+    ) {
+        this.id = id;
+        this.time = time;
+    }
+
+    public void setId(long generatedId) {
+        this.id = generatedId;
+    }
+}
