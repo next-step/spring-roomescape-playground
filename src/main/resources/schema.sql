5e627afc5032d994f426c311bf1dbe8f8f9122c6
JunYoung Jang <police0023@naver.com>
2023-12-22 18:39:12 +0900

    refactor:충돌해결v5

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,9 +6,9 @@ CREATE TABLE time
 );
 CREATE TABLE reservation
 (
-    id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255) NOT NULL,
-    date    VARCHAR(255) NOT NULL,
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    date VARCHAR(255) NOT NULL,
     time_id BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES time (id)
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,4 +1,4 @@
-package roomescape.testFile;
+package roomescape;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
