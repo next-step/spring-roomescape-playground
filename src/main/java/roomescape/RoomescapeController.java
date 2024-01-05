14a32bcee8f221d43732783e95a73d39ce5363bb
Choi-JY <Choi-JY1107@github.com>
2024-01-06 02:56:54 +0900

    feat(step2): reservations url 매핑 추가

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -1,10 +1,19 @@
 package roomescape;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RoomescapeController {
+
+    private final List<Reservation> reservations = new ArrayList<>();
+
     @GetMapping("/")
     public String home() {
         return "home";
@@ -13,5 +22,19 @@ public class RoomescapeController {
     @GetMapping("/reservation")
     public String reservation() {
         return "reservation";
+    }
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> reservations() {
+        for (int i = 0; i < 3; ++i) {
+            Reservation reservation = new Reservation(
+                1L,
+                "test" + i,
+                LocalDate.now(),
+                LocalTime.now()
+            );
+            reservations.add(reservation);
+        }
+
+        return ResponseEntity.ok().body(reservations);
     }
 }
