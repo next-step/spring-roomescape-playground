ae621dec26c2785b57bf61574ebcae8fdfb87334
Choi-JY <Choi-JY1107@github.com>
2024-01-07 01:19:35 +0900

    test(step4): step4 테스트 코드 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -5,12 +5,12 @@ import static org.hamcrest.core.Is.*;
 import java.util.HashMap;
 import java.util.Map;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -19,9 +19,9 @@ public class MissionStepTest {
     @Test
     void 일단계() {
         RestAssured.given().log().all()
-                .when().get("/")
-                .then().log().all()
-                .statusCode(200);
+            .when().get("/")
+            .then().log().all()
+            .statusCode(200);
     }
 
     @Test
@@ -70,5 +70,27 @@ public class MissionStepTest {
             .then().log().all()
             .statusCode(200)
             .body("size()", is(0));
+    }
+
+    @Test
+    void 사단계() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "");
+        params.put("time", "");
+
+        // 필요한 인자가 없는 경우
+        RestAssured.given().log().all()
+            .contentType(ContentType.JSON)
+            .body(params)
+            .when().post("/reservations")
+            .then().log().all()
+            .statusCode(400);
+
+        // 삭제할 예약이 없는 경우
+        RestAssured.given().log().all()
+            .when().delete("/reservations/1")
+            .then().log().all()
+            .statusCode(400);
     }
 }
