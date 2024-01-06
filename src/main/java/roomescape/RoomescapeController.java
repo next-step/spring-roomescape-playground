0350e9de4f7e49124fb9470617a3f241b252ccb4
Choi-JY <Choi-JY1107@github.com>
2024-01-07 01:18:37 +0900

    feat(step3): 예약 삭제 기능 구현

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -5,10 +5,12 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,8 +49,14 @@ public class RoomescapeController {
         return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
     }
 
-    @GetMapping("/reservations/{id}")
+    @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> getReservations(@PathVariable Long id) {
-        return ResponseEntity.ok().build();
+        Reservation reservation = reservations.stream()
+            .filter(it -> Objects.equals(it.getId(), id))
+            .findFirst()
+            .orElseThrow(RuntimeException::new);
+
+        reservations.remove(reservation);
+        return ResponseEntity.noContent().build();
     }
 }
