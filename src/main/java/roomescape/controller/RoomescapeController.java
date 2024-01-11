99b7e8b26e766344a3c2a4e097e86e646adb0f2c
최재영 <52996979+Choi-JY1107@users.noreply.github.com>
2024-01-11 19:51:00 +0900

    [Spring MVC] 최재영 미션 제출합니다 (#54)
    * feat(step1): thymeleaf, web 관련 의존성 추가
    
    * feat(step1): home.html 반환하는 url 매핑 추가
    
    * test(step2): step2 테스트 코드 추가
    
    * feat(step2): Reservation model 생성
    
    * feat(step2): reservation url 매핑 추가
    
    * refactor(step2): Reservation 모델의 date, time 자료형 변경
    
    * feat(step2): reservations url 매핑 추가
    
    * test(step3): step3 테스트 코드 추가
    
    * refactor(step3): controller 메소드 명 변경
    
    * feat(step3): Reservation model에 toEntity, update 메소드 추가
    
    * feat(step3): String 형식의 date, time 입력 기능 추가
    
    * refactor(step3): 시간 저장 시 JsonFormat 대신 DateTimeFormat 으로 변경
    
    * feat(step3): 예약 추가 기능 구현
    
    * feat(step3): 예약 삭제 기능 구현
    
    * test(step4): step4 테스트 코드 추가
    
    * feat(step4): InvalidReservation, NotFoundReservation Exception 추가
    
    * feat(step4): 잘못된 예약 정보 입력 및 조회 시 bad Request 반환하는 기능 추가
    
    * refactor: domain, controller 폴더 생성
    
    ---------
    
    Co-authored-by: Choi-JY <Choi-JY1107@github.com>

---
+++ src/main/java/roomescape/controller/RoomescapeController.java
@@ -0,0 +1,89 @@
+package roomescape.controller;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import roomescape.domain.Reservation;
+import roomescape.exception.InvalidReservationException;
+import roomescape.exception.NotFoundReservationException;
+
+@Controller
+public class RoomescapeController {
+
+    private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(1);
+
+    private static boolean isValidReservation(Reservation reservation) {
+        if (reservation == null)
+            return false;
+        if (reservation.getName().isEmpty() || reservation.getName().isBlank())
+            return false;
+        if (reservation.getDate() == null)
+            return false;
+        if (reservation.getTime() == null)
+            return false;
+
+        return true;
+    }
+
+    @ExceptionHandler({NotFoundReservationException.class, InvalidReservationException.class})
+    public ResponseEntity handleException(RuntimeException e) {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/")
+    public String getHome() {
+        return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String getReservation() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> getReservations() {
+        if (index.get() == 1L) {
+            reservations.add(new Reservation(index.getAndIncrement(), "test" + 1, LocalDate.now(), LocalTime.now()));
+            reservations.add(new Reservation(index.getAndIncrement(), "test" + 2, LocalDate.now(), LocalTime.now()));
+            reservations.add(new Reservation(index.getAndIncrement(), "test" + 3, LocalDate.now(), LocalTime.now()));
+        }
+        return ResponseEntity.ok().body(reservations);
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
+        if (!isValidReservation(reservation)) {
+            throw new InvalidReservationException();
+        }
+
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        reservations.add(newReservation);
+        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> getReservations(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+            .filter(it -> Objects.equals(it.getId(), id))
+            .findFirst()
+            .orElseThrow(NotFoundReservationException::new);
+
+        reservations.remove(reservation);
+        return ResponseEntity.noContent().build();
+    }
+}
