47372f5af41f8d72ef9a0bc5fd6518f8d0dbc540
kwonyj1022 <kwonyj1022@naver.com>
2024-01-11 17:06:32 +0900

    feat: 어드민 메인 페이지 응답 구현

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ### 1단계 요구사항
 - [x] 웹 관련 gradle 의존성 추가
-- [ ] localhost:8080 요청 시 templates/home.html 파일 응답
+- [x] localhost:8080 요청 시 templates/home.html 파일 응답
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,19 +1,22 @@
 package roomescape;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+@SuppressWarnings("NonAsciiCharacters")
+class MissionStepTest {
 
     @Test
+    @DisplayName("어드민 메인 페이지를 반환한다")
     void 일단계() {
         RestAssured.given().log().all()
-                .when().get("/")
-                .then().log().all()
-                .statusCode(200);
+                   .when().get("/")
+                   .then().log().all()
+                   .statusCode(200);
     }
 }
