1dfb11d86cf6a1746bdbaa1bf9484f9c63dc7aa4
Seonho Kim <5061ksh@naver.com>
2024-01-13 01:18:00 +0900

    Initial commit for Reservation data accessing

---
+++ src/main/java/roomescape/ReservationDAO.java
@@ -0,0 +1,32 @@
+package roomescape;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ReservationDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    private final RowMapper<Reservation> reservationRowMapper = (resultSet, rowNum) -> {
+        Reservation reservation = new Reservation(
+                resultSet.getLong("id"),
+                resultSet.getString("name"),
+                resultSet.getString("date"),
+                resultSet.getString("time")
+        );
+        return reservation;
+    };
+
+    public ReservationDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Reservation> findAllReservations() {
+        String sql = "SELECT id, name, date, time FROM reservation";
+        return jdbcTemplate.query(sql, reservationRowMapper);
+    }
+}
