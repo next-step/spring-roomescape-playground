fa121a9bc50e9dd3c0da411003dba455ccb94dc3
PARK9898 <wjddjs21@naver.com>
2024-01-13 00:42:45 +0900

    feat: 예약 클래 생성

---
+++ src/main/java/roomescape/domain/Reservation.java
@@ -0,0 +1,12 @@
+package roomescape.domain;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+    private Long id;
+    private String name;
+    private LocalDate date;
+    private LocalTime time;
+}
