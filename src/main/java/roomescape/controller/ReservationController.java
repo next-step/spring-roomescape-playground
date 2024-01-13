a375b06226761abbf976c05caa8c31eb5c0f078e
kwonyj1022 <kwonyj1022@naver.com>
2024-01-13 15:27:32 +0900

    feat: 예약 삭제 API 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,7 +2,9 @@ package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +16,7 @@ import roomescape.dto.ReadReservationResponse;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
@@ -50,5 +53,17 @@ public class ReservationController {
 
         return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId()))
                              .body(CreateReservationResponse.from(newReservation));
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable final Long id) {
+        final Reservation target = reservations.stream()
+                                               .filter(reservation -> Objects.equals(reservation.getId(), id))
+                                               .findAny()
+                                               .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 예약입니다."));
+        reservations.remove(target);
+
+        return ResponseEntity.noContent()
+                             .build();
     }
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -68,4 +68,39 @@ class MissionStepTest {
                    .statusCode(200)
                    .body("size()", is(1));
     }
+
+    @Test
+    @DisplayName("예약을 삭제한다")
+    void 삼단계_2() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+                   .contentType(ContentType.JSON)
+                   .body(params)
+                   .when().post("/reservations")
+                   .then().log().all()
+                   .statusCode(201)
+                   .header("Location", "/reservations/1")
+                   .body("id", is(1));
+
+        RestAssured.given().log().all()
+                   .when().get("/reservations")
+                   .then().log().all()
+                   .statusCode(200)
+                   .body("size()", is(1));
+
+        RestAssured.given().log().all()
+                   .when().delete("/reservations/1")
+                   .then().log().all()
+                   .statusCode(204);
+
+        RestAssured.given().log().all()
+                   .when().get("/reservations")
+                   .then().log().all()
+                   .statusCode(200)
+                   .body("size()", is(0));
+    }
 }
