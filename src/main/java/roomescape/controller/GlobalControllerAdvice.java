eb0b5970949267030aeb2bb44d1aebea811333c7
hum02 <ehdgma02@gmail.com>
2024-01-13 15:25:07 +0900

    refactor:controllerAdvice 네이밍 변경

--- src/main/java/roomescape/controller/GlobalControllerAdvice.java
+++ src/main/java/roomescape/controller/GlobalControllerAdvice.java
@@ -1,10 +1,11 @@
 package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@org.springframework.web.bind.annotation.ControllerAdvice
-public class ControllerAdvice {
+@ControllerAdvice
+public class GlobalControllerAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handle(IllegalArgumentException e) {
