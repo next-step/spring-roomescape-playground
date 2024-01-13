cdc2f8b3d81c64e86a33ce90213be47a3abb0aaa
PARK9898 <wjddjs21@naver.com>
2024-01-13 17:16:08 +0900

    feat: 예약 삭제 메서드 구현 & custom exception 생성(예약을 찾지 못할 경우)

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -6,12 +6,15 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import roomescape.domain.Reservation;
 import roomescape.dto.reservation.ReservationRequest;
+import roomescape.exception.ReservationNotFoundException;
 
 @Controller
 public class ReservationController {
@@ -44,5 +47,19 @@ public class ReservationController {
                 .status(HttpStatus.CREATED)
                 .header("Location", "/reservations/" + reservation.getId())
                 .body(reservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Reservation> deleteReservation(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ReservationNotFoundException(ReservationNotFoundException.RESERVATION_NOT_FOUND_MESSAGE));
+
+        reservations.remove(reservation);
+
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
     }
 }
