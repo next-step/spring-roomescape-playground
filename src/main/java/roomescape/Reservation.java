7f77a0b55a1d5cbcce98da5f953ddd18e3dcc5e2
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-01-14 03:13:00 +0900

    depend: h2 의존성 추가

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -1,11 +1,19 @@
 package roomescape;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Entity
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @NotBlank
     private String name;
