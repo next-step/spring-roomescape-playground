ba6b2d31727c76a449393937e6a38aed49ea5e8c
chlee <chlee@student.42seoul.kr>
2024-01-18 15:28:42 +0900

    modify: exception handler annotation

--- src/main/java/roomescape/exception/ReservationExceptionHandler.java
+++ src/main/java/roomescape/exception/ReservationExceptionHandler.java
@@ -2,17 +2,17 @@ package roomescape.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import roomescape.controller.ReservationController;
 
 @ControllerAdvice(assignableTypes = ReservationController.class)
-public class ExceptionHandler {
-
-    @org.springframework.web.bind.annotation.ExceptionHandler(BadRequestReservationException.class)
+public class ReservationExceptionHandler {
+    @ExceptionHandler(BadRequestReservationException.class)
     public ResponseEntity<Void> handleBadRequestReservationException() {
         return ResponseEntity.badRequest().build();
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundReservationException.class)
+    @ExceptionHandler(NotFoundReservationException.class)
     public ResponseEntity<Void> handleNotFoundReservationException() {
         return ResponseEntity.badRequest().build(); //NotFoundException(404) returns BadRequest(400)?
     }
