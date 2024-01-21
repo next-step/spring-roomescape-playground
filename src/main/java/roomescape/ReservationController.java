c646ea16829056c7f68eee3a6ebb6e3a394bc9d1
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-01-22 03:24:27 +0900

    refactor: step10 - ReservationController에서 jdbcTemplate 필드 없애기

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -3,19 +3,12 @@ package roomescape;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class ReservationController {
-    private static final AtomicLong idCounter = new AtomicLong();
-    private final List<Reservation> reservations = new ArrayList<>();
 
     @Autowired
     ReservationQueryingDAO reservationQueryingDAO;
