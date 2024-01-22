5d04d051ee4fd6a65c4b79b80bc01e5d1deec310
chlee <chlee@student.42seoul.kr>
2024-01-22 20:39:56 +0900

    MODIFY: controller use repository

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,20 +1,17 @@
 package roomescape.controller;
 
 import jakarta.validation.Valid;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
+import roomescape.Repository.ReservationRepository;
 import roomescape.domain.Reservation;
 import roomescape.exception.BadRequestReservationException;
-import roomescape.exception.NotFoundReservationException;
 
 
-import java.io.FileNotFoundException;
 import java.net.URI;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -23,30 +20,17 @@ import java.util.concurrent.atomic.AtomicLong;
 @Controller
 public class ReservationController {
 
-    private List<Reservation> reservationList = new ArrayList<>();
-    private AtomicLong index = new AtomicLong(1);
-
-    // for second-stage testing purposes
-//    {
-//        Reservation test1 = new Reservation(index.getAndIncrement(), "name1", LocalDate.of(2023, 8, 8), LocalTime.of(11, 0));
-//        Reservation test2 = new Reservation(index.getAndIncrement(), "name2", LocalDate.of(2023, 8, 8), LocalTime.of(12, 0));
-//        Reservation test3 = new Reservation(index.getAndIncrement(), "name3", LocalDate.of(2023, 8, 9), LocalTime.of(13, 0));
-//
-//        reservationList.add(test1);
-//        reservationList.add(test2);
-//        reservationList.add(test3);
-//    }
+    @Autowired
+    private ReservationRepository reservationRepository;
 
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> getReservations() {
-        return ResponseEntity.ok(reservationList);
+        return ResponseEntity.ok(reservationRepository.findAll());
     }
 
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> addReservation(@Valid @RequestBody Reservation newReservation) {
-        Reservation reservation = Reservation.toEntity(newReservation, index.getAndIncrement());
-
-        reservationList.add(reservation);
+        Reservation reservation = reservationRepository.save(newReservation);
         return ResponseEntity
                 .created(URI.create("/reservations/" + reservation.getId()))
                 .body(reservation);
@@ -54,12 +38,7 @@ public class ReservationController {
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id) {
-        Reservation reservation = reservationList.stream()
-                .filter(it -> Objects.equals(it.getId(), id))
-                .findFirst()
-                .orElseThrow(BadRequestReservationException::new);
-
-        reservationList.remove(reservation);
+        reservationRepository.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
