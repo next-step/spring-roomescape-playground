e26fcf92ca2e8c6f4bd7dedfdbb8d7c1bddbc9b7
PARK9898 <wjddjs21@naver.com>
2024-01-23 21:25:32 +0900

    feat: 예약 저장 기능 구현

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -1,12 +1,16 @@
 package roomescape.repository;
 
 import java.util.List;
+import java.util.Map;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.Reservation;
+import roomescape.dto.reservation.ReservationRequest;
 
 @Repository
 public class ReservationRepository {
@@ -31,5 +35,20 @@ public class ReservationRepository {
 
     public List<Reservation> findAllReservation() {
         return jdbcTemplate.query("select * from reservation", reservationRowMapper());
+    }
+
+    public Long saveReservation(ReservationRequest reservationRequest) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("reservation").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = Map.of(
+                "name", reservationRequest.getName(),
+                "date", reservationRequest.getDate(),
+                "time", reservationRequest.getTime()
+        );
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+
+        return key.longValue();
     }
 }
