cef2ed16f65ec788d3ac7a0b9ba194085dd033d9
Seonho Kim <5061ksh@naver.com>
2024-01-24 22:30:56 +0900

    refactor: deleted redundant import statements

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -2,7 +2,6 @@ package roomescape;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
