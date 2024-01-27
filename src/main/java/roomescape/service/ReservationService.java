b4f539a53d1af8bcf174d11b87a3699c77e5bd53
PARK9898 <wjddjs21@naver.com>
2024-01-28 07:08:43 +0900

    feat: 예약 삭제 메서드 구현 & 예외처리

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,9 +1,11 @@
 package roomescape.service;
 
 import java.util.List;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import roomescape.domain.Reservation;
 import roomescape.dto.reservation.ReservationRequest;
+import roomescape.exception.ReservationNotFoundException;
 import roomescape.repository.ReservationRepository;
 
 @Service
@@ -28,5 +30,14 @@ public class ReservationService {
 
     public List<Reservation> findAllReservation() {
         return reservationRepository.findAllReservation();
+    }
+
+    public void deleteReservationById(Long id) {
+        try {
+            reservationRepository.findReservationById(id);
+            reservationRepository.deleteReservationById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ReservationNotFoundException(ReservationNotFoundException.RESERVATION_NOT_FOUND_MESSAGE);
+        }
     }
 }
