2f82082d1830a8d4262c02748204c5fabc46a021
PARK9898 <wjddjs21@naver.com>
2024-01-28 07:10:01 +0900

    refactor: 불필요 커스텀 에외 삭제

--- src/main/java/roomescape/exception/InvalidReservationException.java
+++
@@ -1,9 +0,0 @@
-package roomescape.exception;
-
-public class InvalidReservationException extends RuntimeException{
-    public static final String INVALID_RESERVATION_MESSAGE= "유효하지 않은 값의 예약입니다.";
-
-    public InvalidReservationException(String message) {
-        super(message);
-    }
-}
