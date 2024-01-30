2ead5cbce31f1a1965ad7a86c238366183ea7ff1
Choi-JY <Choi-JY1107@github.com>
2024-01-31 01:18:44 +0900

    refactor: TimeController 와 ReservationController 분리

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import roomescape.domain.Reservation;
-import roomescape.domain.Time;
 import roomescape.domain.dto.ReservationAddRequest;
 import roomescape.service.ReservationService;
 
@@ -42,27 +41,6 @@ public class ReservationController {
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> reservationRemove(@PathVariable Long id) {
         reservationService.removeReservation(id);
-
-        return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping("/times")
-    public ResponseEntity<List<Time>> timeList() {
-        List<Time> timeList = reservationService.findTimeList();
-
-        return ResponseEntity.ok().body(timeList);
-    }
-
-    @PostMapping("/times")
-    public ResponseEntity<Time> timeSave(@RequestBody Time time) {
-        Time newTime = reservationService.addTime(time);
-
-        return ResponseEntity.created(URI.create("/times/" + newTime.getId())).body(newTime);
-    }
-
-    @DeleteMapping("/times/{id}")
-    public ResponseEntity<Void> timeRemove(@PathVariable Long id) {
-        reservationService.removeTime(id);
 
         return ResponseEntity.noContent().build();
     }
---
+++ src/main/java/roomescape/controller/TimeController.java
@@ -0,0 +1,43 @@
+package roomescape.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import roomescape.domain.Time;
+import roomescape.service.ReservationService;
+
+public class TimeController {
+    private final ReservationService reservationService;
+
+    public TimeController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping("/times")
+    public ResponseEntity<List<Time>> timeList() {
+        List<Time> timeList = reservationService.findTimeList();
+
+        return ResponseEntity.ok().body(timeList);
+    }
+
+    @PostMapping("/times")
+    public ResponseEntity<Time> timeSave(@RequestBody Time time) {
+        Time newTime = reservationService.addTime(time);
+
+        return ResponseEntity.created(URI.create("/times/" + newTime.getId())).body(newTime);
+    }
+
+    @DeleteMapping("/times/{id}")
+    public ResponseEntity<Void> timeRemove(@PathVariable Long id) {
+        reservationService.removeTime(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
