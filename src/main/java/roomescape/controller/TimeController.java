097df3354d812839dddc701f8ac7b2b3cf637061
Choi-JY <Choi-JY1107@github.com>
2024-01-31 02:08:21 +0900

    fix: TimeController 에 Controller 어노테이션 추가

--- src/main/java/roomescape/controller/TimeController.java
+++ src/main/java/roomescape/controller/TimeController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import roomescape.domain.Time;
 import roomescape.service.ReservationService;
 
+@Controller
 public class TimeController {
     private final ReservationService reservationService;
 
