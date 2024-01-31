3637f3d97d505a489dfc78b645c5e0caf83d6842
PARK9898 <wjddjs21@naver.com>
2024-01-31 19:15:52 +0900

    feat: time dto 구현

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -6,17 +6,22 @@ import java.util.List;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import roomescape.domain.Reservation;
+import roomescape.domain.Time;
 import roomescape.dto.reservation.ReservationRequest;
+import roomescape.dto.time.TimeRequest;
 import roomescape.exception.InvalidReservationException;
 import roomescape.exception.ReservationNotFoundException;
 import roomescape.repository.ReservationRepository;
+import roomescape.repository.TimeDao;
 
 @Service
 public class ReservationService {
     private final ReservationRepository reservationRepository;
+    private final TimeDao timeDao;
 
-    public ReservationService(ReservationRepository reservationRepository) {
+    public ReservationService(ReservationRepository reservationRepository, TimeDao timeDao) {
         this.reservationRepository = reservationRepository;
+        this.timeDao = timeDao;
     }
 
     public Long saveReservation(ReservationRequest reservationRequest) {
@@ -46,4 +51,15 @@ public class ReservationService {
             throw new ReservationNotFoundException("해당 id의 예약을 찾을 수 없습니다");
         }
     }
+    public Long saveTime(TimeRequest timeRequest) {
+        if(timeRequest.getTime() == null) {
+            throw new InvalidReservationException("");
+        }
+        Time time = new Time(
+                null,
+                timeRequest.getTime()
+        );
+        return timeDao.saveTime(time);
+    }
+
 }
