66cc4e504263962f6796a89aba758f4998b356df
kwonyj1022 <kwonyj1022@naver.com>
2024-02-02 15:10:56 +0900

    test: 10단계 요구사항 확인 테스트 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -13,7 +13,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import roomescape.domain.Reservation;
+import roomescape.presentation.ReservationController;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -32,6 +34,9 @@ class MissionStepTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private ReservationController reservationController;
 
     @Test
     @DisplayName("어드민 메인 페이지를 반환한다")
@@ -369,5 +374,20 @@ class MissionStepTest {
                    .when().post("/reservations")
                    .then().log().all()
                    .statusCode(400);
+    }
+
+    @Test
+    @DisplayName("controller에 있던 데이터베이스 관련 로직을 다른 클래스로 분리한 것을 확인한다")
+    void 십단계() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
     }
 }
