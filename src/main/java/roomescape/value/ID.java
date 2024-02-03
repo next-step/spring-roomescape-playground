98b4743cb7ee09e46cb9eb98c631ce1ae727bec2
sans29374 <sansu7904@gmail.com>
2024-02-03 13:28:09 +0900

    2&3 단계 구현

---
+++ src/main/java/roomescape/value/ID.java
@@ -0,0 +1,14 @@
+package roomescape.value;
+
+public class ID {
+
+    private int id;
+
+    public ID(int id) {
+        this.id = id;
+    }
+
+    public int getID() {
+        return id;
+    }
+}
