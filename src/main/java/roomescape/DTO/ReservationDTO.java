e1e654e4df37b7101c883e56df6bbc676f70deb4
sans29374 <sansu7904@gmail.com>
2024-02-05 12:28:23 +0900

    BindingResult를 적용한 validation으로 수정

--- src/main/java/roomescape/DTO/ReservationDTO.java
+++ src/main/java/roomescape/DTO/ReservationDTO.java
@@ -1,18 +1,28 @@
 package roomescape.DTO;
 
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
+import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-@AllArgsConstructor
-@Getter
+@Data
+@Setter(AccessLevel.NONE)
 @Builder
 public class ReservationDTO {
     private Long id;
+
+    @NotNull(message = "Name cannot be null")
     private String name;
+
+    @NotNull(message = "Date cannot be null")
+    @PastOrPresent(message = "Date must be past or present")
     private LocalDate date;
+
+    @NotNull(message = "Time cannot be null")
     private LocalTime time;
 }
