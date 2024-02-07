b16603fd606c1365a83c8e8e3a3b731572a4691f
Seonho Kim <5061ksh@naver.com>
2024-02-08 00:52:47 +0900

    add: fully implemented DTO code

--- src/main/java/roomescape/dto/TimeResponse.java
+++ src/main/java/roomescape/dto/TimeResponse.java
@@ -1,4 +1,27 @@
 package roomescape.dto;
 
+import java.time.LocalTime;
+import roomescape.domain.Time;
+
 public class TimeResponse {
+    private final Long id;
+    private final LocalTime time;
+
+    public TimeResponse(final Long id, final LocalTime time) {
+        this.id = id;
+        this.time = time;
+    }
+
+    public TimeResponse(final Time time) {
+        this.id = time.getId();
+        this.time = time.getTime();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
 }
