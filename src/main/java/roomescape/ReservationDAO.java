122f264d31f32e6551d4442b08dc4a51cd1fedee
Bobae Kim <rlaqhqo0418@naver.com>
2024-02-12 23:48:29 +0900

    feat: DAO 분리

---
+++ src/main/java/roomescape/ReservationDAO.java
@@ -0,0 +1,54 @@
+package roomescape;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.domain.Reservation;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+
+@Repository
+public class ReservationDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Long insert(Reservation reservation) {
+        String sql = "insert into reservation (name, date, time) values (?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    sql, new String[]{"id"});
+            ps.setString(1, reservation.getName());
+            ps.setObject(2, reservation.getDate());
+            ps.setObject(3, reservation.getTime());
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
+    }
+
+    public List<Reservation> findAllReservations() {
+        String sql = "select id, name, date, time from reservation";
+        return jdbcTemplate.query(
+                sql,
+                (resultSet, rowNum) -> {
+                    Reservation reservation = new Reservation(
+                            resultSet.getLong("id"),
+                            resultSet.getString("name"),
+                            resultSet.getDate("date").toLocalDate(),
+                            resultSet.getTime("time").toLocalTime()
+                    );
+                    return reservation;
+                });
+    }
+
+    public void delete(Long id) {
+        String sql = "delete from reservation where id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+}
--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,66 +1,43 @@
 package roomescape.controller;
 
 import java.net.URI;
-import java.sql.PreparedStatement;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.*;
 import roomescape.ErrorCode;
 import roomescape.NotFoundReservationException;
+import roomescape.ReservationDAO;
 import roomescape.domain.Reservation;
 
 @RestController
 public class ReservationController {
+    private final ReservationDAO reservationDAO;
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    public ReservationController(JdbcTemplate jdbcTemplate) {
+        reservationDAO = new ReservationDAO(jdbcTemplate);
+    }
 
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation) {
         if(reservation.getName().isEmpty() || reservation.getDate() == null || reservation.getTime() == null) {
             throw new NotFoundReservationException(ErrorCode.INVALID_ARGUMENT);
         }
-        String sql = "insert into reservation (name, date, time) values (?, ?, ?)";
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(
-                    sql, new String[]{"id"});
-            ps.setString(1, reservation.getName());
-            ps.setObject(2, reservation.getDate());
-            ps.setObject(3, reservation.getTime());
-            return ps;
-        }, keyHolder);
-
-        Long id = keyHolder.getKey().longValue();
+        Long id = reservationDAO.insert(reservation);
         Reservation newReservation = Reservation.toEntity(reservation, id);
         return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
     }
 
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> getReservations() {
-        String sql = "select id, name, date, time from reservation";
-        return ResponseEntity.ok(jdbcTemplate.query(
-                sql,
-                (resultSet, rowNum) -> {
-                    Reservation reservation = new Reservation(
-                            resultSet.getLong("id"),
-                            resultSet.getString("name"),
-                            resultSet.getDate("date").toLocalDate(),
-                            resultSet.getTime("time").toLocalTime()
-                    );
-                    return reservation;
-                }));
+        return ResponseEntity.ok(reservationDAO.findAllReservations());
     }
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
-        String sql = "delete from reservation where id = ?";
-        jdbcTemplate.update(sql, Long.valueOf(id));
+
+        reservationDAO.delete(id);
         return ResponseEntity.noContent().build();
     }
 
