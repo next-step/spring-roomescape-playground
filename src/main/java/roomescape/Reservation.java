78a4355f645aff1fa28499d663ceeadfaefb5c21
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-02-15 15:04:47 +0900

    2단계-예약조회: 예약 컨트롤러 구현

---
+++ src/main/java/roomescape/Reservation.java
@@ -0,0 +1,16 @@
+package roomescape;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class Reservation {
+    private Long id;
+    private String name;
+    private String date;
+    private String time;
+
+    public Reservation(){
+
+    }
+
+}
