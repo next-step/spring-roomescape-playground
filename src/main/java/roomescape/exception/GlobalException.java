1fdcbf40eeeb58cba74e8fbae7db02958c9c7f17
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-02-15 22:12:32 +0900

    4단계-예외처리: 에러코드 구현

---
+++ src/main/java/roomescape/exception/GlobalException.java
@@ -0,0 +1,19 @@
+package roomescape.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(NotFoundReservationException.class)
+    public ResponseEntity<String> handleException (NotFoundReservationException e){
+        return ResponseEntity.badRequest().body(ErrorCode.NOT_FOUND_RESERVATION.getMessage());
+    }
+
+    @ExceptionHandler(NoParameterException.class)
+    public ResponseEntity<String> handleException (NoParameterException e){
+        return ResponseEntity.badRequest().body(ErrorCode.NO_PARAMETER.getMessage());
+    }
+}
