ba6d7ed90f16b8525156e3f137fae7f5c6f2da32
Hyoseop Song <crad_on25@naver.com>
2024-02-17 21:06:30 +0900

    feat: 메인 화면 접근 컨트롤러

---
+++ src/test/java/roomescape/controller/HomeControllerTest.java
@@ -0,0 +1,18 @@
+package roomescape.controller;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class HomeControllerTest {
+	@Test
+	void 메인_페이지_접근_성공() {
+		RestAssured.given().log().all()
+				.when().get("/")
+				.then().log().all()
+				.statusCode(200);
+	}
+}
