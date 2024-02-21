a5d695f93102633faf74b084a6c8ae3ce36ad789
Ojimin <ogm0205@gmail.com>
2024-02-21 20:06:01 +0900

    [Spring MVC] 오지민 미션 제출합니다. (#107)
    * feat: Spring MVC 1단계 미션 수행
    
    * feat: Spring MVC 2단계 미션 수행
    
    * feat: Spring MVC 3단계 미션 수행
    
    * feat: 4단계 미션 수행

---
+++ src/main/java/roomescape/ReservationController.java
@@ -0,0 +1,60 @@
+package roomescape;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+public class ReservationController {
+
+    private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(1);
+
+    @GetMapping("/")
+    public String readHome() {
+        return "Home";
+    }
+
+    @GetMapping("/reservation")
+    public String readReservation() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> getReservations() {
+        return ResponseEntity.ok(reservations);
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        reservations.add(newReservation);
+        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId()))
+                .body(newReservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable(name = "id") Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+        reservations.remove(reservation);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class, RuntimeException.class})
+    public ResponseEntity handleException(Exception e) {
+        return ResponseEntity.badRequest().build();
+    }
+
+
+}
