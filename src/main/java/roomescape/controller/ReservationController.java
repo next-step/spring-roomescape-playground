2ed2dcd726c63e4b17ff7494fd682117ff001c7d
Park Sungbin <46699595+ImTotem@users.noreply.github.com>
2024-02-21 09:44:33 +0900

    [Spring MVC] 박성빈 미션 제출합니다. (#111)
    * feat: 1단계 - 홈 화면
    
    * feat: 2단계 - 예약 조회
    
    * feat: 3단계 - 예약 추가 / 취소
    
    * feat: 4단계 - 예외 처리
    
    * style: 줄바꿈 정리
    
    * refactor: body 추가
    
    * refactor: Exception 변경

---
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -0,0 +1,63 @@
+package roomescape.controller;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.validation.Valid;
+import roomescape.controller.dto.ReservationCreate;
+import roomescape.controller.dto.ReservationResponse;
+import roomescape.domain.Reservation;
+
+@Controller
+public class ReservationController {
+
+    private final AtomicLong id = new AtomicLong(1);
+    private final List<Reservation> reservations = new ArrayList<>();
+
+    @GetMapping("/reservation")
+    public String getReservation() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<ReservationResponse> getReservations() {
+        return reservations.stream()
+            .map(ReservationResponse::from)
+            .toList();
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<ReservationResponse> post(@RequestBody @Valid ReservationCreate request) {
+        Reservation reservation = request.toReservation(id.getAndIncrement());
+
+        reservations.add(reservation);
+        
+        return ResponseEntity.created(URI.create("/reservations/" + reservation.getId()))
+            .body(ReservationResponse.from(reservation));
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+            .filter(it -> it.getId() == id)
+            .findAny()
+            .orElseThrow(() -> new NoSuchElementException("존재하지 않는 예약입니다."));
+
+        reservations.remove(reservation);
+
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/roomescape/domain/Reservation.java
@@ -0,0 +1,52 @@
+package roomescape.domain;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+
+public class Reservation {
+    private final long id;
+    private final String name;
+    private final LocalDate date;
+    private final LocalTime time;
+
+    public Reservation(long id, String name, LocalDate date, LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+
+        if (!(o instanceof Reservation that))
+            return false;
+
+        return id == that.id && Objects.equals(name, that.name) && Objects.equals(date, that.date)
+            && Objects.equals(time, that.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, date, time);
+    }
+}
