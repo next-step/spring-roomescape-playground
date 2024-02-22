6fd9c0bf5189c908e29894b458349596c5a7a135
Hyoseop Song <crad_on25@naver.com>
2024-02-22 17:56:29 +0900

    refactor: Delete unused lombok annotation in Entity

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -2,8 +2,6 @@ package roomescape.domain;
 
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
 
-@Getter
 public record Reservation(Long id, @NotBlank String name, @NotBlank String date, @NotBlank String time) {
 }
