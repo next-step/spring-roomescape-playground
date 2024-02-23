df1510f1d0b16731ba33a394ddadbb917fc65cfc
Hyoseop Song <crad_on25@naver.com>
2024-02-23 17:14:38 +0900

    test: jdbc connection test

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -1,17 +1,23 @@
 package roomescape.controller;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.domain.Reservation;
 import roomescape.repository.ReservationRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -20,6 +26,9 @@ public class ReservationControllerTest {
 
 	@Autowired
 	private ReservationRepository reservationRepository;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@BeforeEach
 	void setUp() {
@@ -95,5 +104,32 @@ public class ReservationControllerTest {
 				.when().delete("/reservations/1")
 				.then().log().all()
 				.statusCode(400);
+	}
+
+	@Test
+	void JDBC_커넥션_테스트() {
+		try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+			assertThat(connection).isNotNull();
+			assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+			assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	void JDBC_예약조회_동기화() {
+		jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05",
+				"15:40");
+
+		List<Reservation> reservations = RestAssured.given().log().all()
+				.when().get("/reservations")
+				.then().log().all()
+				.statusCode(200).extract()
+				.jsonPath().getList(".", Reservation.class);
+
+		Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+
+		assertThat(reservations.size()).isEqualTo(count);
 	}
 }
