a237a1cfc1029d394f319c57af109bac7b162a18
Hyoseop Song <crad_on25@naver.com>
2024-03-02 23:24:40 +0900

    refactor: 예약 도메인 서비스 TimeRepository 의존성 추가

--- src/main/java/roomescape/config/ReservationServiceConfig.java
+++ src/main/java/roomescape/config/ReservationServiceConfig.java
@@ -5,21 +5,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import roomescape.repository.ReservationRepository;
+import roomescape.repository.TimeRepository;
 import roomescape.service.DefaultReservationService;
 import roomescape.service.JdbcReservationService;
+import roomescape.service.JdbcTimeService;
 import roomescape.service.ReservationService;
+import roomescape.service.TimeService;
 
 @Configuration
 public class ReservationServiceConfig {
 	@Bean
 	@Profile("jdbc")
-	public ReservationService JdbcReservationService(JdbcTemplate jdbcTemplate) {
-		return new JdbcReservationService(jdbcTemplate);
+	public ReservationService JdbcReservationService(JdbcTemplate jdbcTemplate, TimeRepository timeRepository) {
+		return new JdbcReservationService(jdbcTemplate, timeRepository);
+	}
+
+	@Bean
+	@Profile("jdbc")
+	public TimeService JdbcTimeService(JdbcTemplate jdbcTemplate) {
+		return new JdbcTimeService(jdbcTemplate);
 	}
 
 	@Bean
 	@Profile("default")
-	public ReservationService DefaultRefaultReservationService(ReservationRepository reservationRepository) {
-		return new DefaultReservationService(reservationRepository);
+	public ReservationService DefaultRefaultReservationService(ReservationRepository reservationRepository,
+															   TimeRepository timeRepository) {
+		return new DefaultReservationService(reservationRepository, timeRepository);
 	}
 }
