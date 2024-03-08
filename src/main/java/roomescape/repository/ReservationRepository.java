9985c67b380a010464c810f12f30207731938b9a
Hyoseop Song <crad_on25@naver.com>
2024-03-08 23:02:16 +0900

    refactor: change to findAll entity snapshot return

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -1,6 +1,8 @@
 package roomescape.repository;
+
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Repository;
@@ -9,7 +11,7 @@ import roomescape.exception.ReservationException;
 
 @Repository
 public class ReservationRepository {
-  	private final Map<Long, Reservation> reservations;
+	private final Map<Long, Reservation> reservations;
 	private final AtomicLong id;
 
 	public ReservationRepository() {
@@ -18,7 +20,7 @@ public class ReservationRepository {
 	}
 
 	public Collection<Reservation> findAll() {
-		return reservations.values();
+		return List.copyOf(reservations.values());
 	}
 
 	public Long save(Reservation reservation) {
