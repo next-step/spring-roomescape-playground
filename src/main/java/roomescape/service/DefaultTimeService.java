e62322247970b182e8c2ac33dbba9a052349a5ad
Hyoseop Song <crad_on25@naver.com>
2024-03-08 23:05:52 +0900

    feat: implement in-memory DefaultTimeService

---
+++ src/main/java/roomescape/service/DefaultTimeService.java
@@ -0,0 +1,34 @@
+package roomescape.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import roomescape.domain.Time;
+import roomescape.dto.time.TimeResponseDTO.AddTimeResponse;
+import roomescape.dto.time.TimeResponseDTO.QueryTimeResponse;
+import roomescape.repository.TimeRepository;
+
+@Profile("default")
+@RequiredArgsConstructor
+public class DefaultTimeService implements TimeService {
+	private final TimeRepository timeRepository;
+
+	@Override
+	public List<QueryTimeResponse> getAllTimes() {
+		List<Time> times = timeRepository.findAll();
+		return times.stream()
+				.map(time -> new QueryTimeResponse(time.id(), time.value()))
+				.toList();
+	}
+
+	@Override
+	public AddTimeResponse addTime(String time) {
+		Long id = timeRepository.save(new Time(null, time));
+		return new AddTimeResponse(id, time);
+	}
+
+	@Override
+	public void deleteTime(Long id) {
+		timeRepository.deleteById(id);
+	}
+}
