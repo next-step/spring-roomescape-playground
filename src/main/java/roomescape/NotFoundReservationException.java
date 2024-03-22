b5947509c2223396479afc051b4274bdc6edd9ab
joamksh <joamksh@naver.com>
2024-03-22 15:46:15 +0900

    [Spring MVC] 김수현 미션 제출합니다. (#148)
    * 최종 제출
    
    * Create README.md

---
+++ src/main/java/roomescape/NotFoundReservationException.java
@@ -0,0 +1,12 @@
+package roomescape;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class NotFoundReservationException extends RuntimeException {
+    public NotFoundReservationException(String message) {
+        super(message);
+    }
+}
+
