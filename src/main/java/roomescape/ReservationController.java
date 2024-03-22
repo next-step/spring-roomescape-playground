13c077acfa25b42d3f3f063a037c8e1f4b61368b
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-03-22 15:45:46 +0900

    [Spring MVC] 이가연 미션 제출합니다. (#147)
    * 예약 추가 API 미완성
    
    * this() 피드백 반영
    
    this()를 사용해서 생성자 중복코드를 줄였어요.
    
    * Service 파일 추가
    
    데이터 처리 로직을 컨트롤러와 분리해 재작성했어요
    
    * 조회창 응답 핸들러 수정
    
    * ResponseEntity & 반환타입 수정
    
    * [Spring MVC] 송지은 미션 제출합니다. (#142)
    
    * docs: 요구사항 분석
    
    * chore: gradle 의존성 추가
    
    * feat: Home.html과 루트 경로 맵핑
    
    * docs: 요구사항 추가 및 체크리스트 업데이트
    
    * feat: 예약 관리 페이지 응답 및 조회 API 구현
    
    * feat: 예약 추가 & 취소 API 구현
    
    * feat: API 요청 에러 처리
    
    * feat: 기본 test 코드 추가
    
    * feat: 삭제할 예약이 없는 경우 에러발생하도록 변경
    
    * refactor: id 값 부여에 AtomicLong 사용
    
    * refactor: ide의 추천 refactor 적용
    
    * refactor: Reservation id 값 final 적용 및 Long 타입으로 변경
    
    * refactor: 속성값에 final 붙이고 작동하도록 변경
    
    * refactor: ReservationRequest convert
    
    ---------
    
    Co-authored-by: Rulu <79538610+hafnium1923@users.noreply.github.com>

---
+++ src/main/java/roomescape/ReservationController.java
@@ -0,0 +1,64 @@
+package roomescape;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+public class ReservationController {
+
+    List<Reservation> reservations = new ArrayList<>();
+    ReservationService reservationService;
+
+    //생성자로 ReservationService 에 대한 의존성을 주입해요.
+    public ReservationController (ReservationService reservationService){
+        this.reservationService=reservationService;
+    }
+
+    @GetMapping("/")
+    public String GoToHome(){
+        // 수정 전
+        // return "templates/home";
+        // 수정 후
+        return "home";
+    }
+
+    //reservation 이동
+    @GetMapping("/reservation")
+    public void reservation(){
+    }
+
+    @ResponseBody
+    @GetMapping("/reservations")
+    public List<Reservation> read(){
+        return reservationService.getAllReservations();
+    }
+    final AtomicLong index = new AtomicLong(0);
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> create(@RequestBody Reservation reservation) {
+        Reservation newReservation = Reservation.toEntity(index,reservation);
+        reservations.add(newReservation);
+        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+        reservations.remove(reservation);
+        return ResponseEntity.noContent().build();
+    }
+}
