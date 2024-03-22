9169406afd63491fc0fccef45e7f1af4a242032f
Rulu <79538610+hafnium1923@users.noreply.github.com>
2024-03-22 10:30:37 +0900

    [Spring MVC] 송지은 미션 제출합니다. (#142)
    * docs: 요구사항 분석
    
    * chore: gradle 의존성 추가
    
    * feat: Home.html과 루트 경로 맵핑
    
    * docs: 요구사항 추가 및 체크리스트 업데이트
    
    * feat: 예약 관리 페이지 응답 및 조회 API 구현
    
    * feat: 예약 추가 & 취소 API 구현
    
    * feat: API 요청 에러 처리
    
    * feat: 기본 test 코드 추가
    
    * feat: 삭제할 예약이 없는 경우 에러발생하도록 변경
    
    * refactor: id 값 부여에 AtomicLong 사용
    
    * refactor: ide의 추천 refactor 적용
    
    * refactor: Reservation id 값 final 적용 및 Long 타입으로 변경
    
    * refactor: 속성값에 final 붙이고 작동하도록 변경
    
    * refactor: ReservationRequest convert

---
+++ src/main/java/roomescape/reservation/repository/ReservationRepository.java
@@ -0,0 +1,44 @@
+package roomescape.reservation.repository;
+
+import org.springframework.stereotype.Repository;
+import roomescape.reservation.model.Reservation;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class ReservationRepository {
+    private static final Map<Long,Reservation> store = new HashMap<>();
+    private final AtomicLong index = new AtomicLong(0);
+
+    public Reservation save(Reservation reservation) {
+        Reservation addedReservation = new Reservation(index.incrementAndGet(), reservation.getName(), reservation.getDate(), reservation.getTime());
+        store.put(addedReservation.getId(), addedReservation);
+
+        return addedReservation;
+    }
+
+    public List<Reservation> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public Reservation findById(Long id) {
+        return Optional.ofNullable(store.get(id)).orElse(store.get(id));
+    }
+    public void deleteById(final Long id) {
+        store.remove(id);
+    }
+
+//    public void update (Long id, Reservation updateReservation) {
+//        Reservation findReservation = findById(id);
+//
+//        findReservation.setName(updateReservation.getName());
+//        findReservation.setDate(updateReservation.getDate());
+//        findReservation.setTime(updateReservation.getTime());
+//    }
+
+}
