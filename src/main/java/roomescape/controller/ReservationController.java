28dc23f18f0f9fc06bbe149d70106705f393a7ae
hyojean <hrn0314@naver.com>
2024-03-26 19:17:56 +0900

    [Spring JDBC] 미션 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,9 +1,12 @@
 package roomescape.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import roomescape.domain.Reservation;
+import roomescape.dao.ReservationDAO;
+import roomescape.dto.Reservation;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -17,19 +20,51 @@ public class ReservationController {
     private List<Reservation> reservations = new ArrayList<>();
     private AtomicLong index = new AtomicLong(1);
 
-    @GetMapping("/reservations")
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    /*@GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> read() {
         // TODO: 저장된 모든 reservation 정보를 반환한다.
         return ResponseEntity.ok().body(reservations);
+    }*/
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> read() {
+        // TODO: 저장된 모든 reservation 정보를 반환한다.
+        reservations = new ReservationDAO(jdbcTemplate).findAllReservations();
+        return ResponseEntity.ok().body(reservations);
     }
+
+    /*@PostMapping("/reservations")
+    public ResponseEntity<Reservation> create(@RequestBody Reservation reservation) {
+        // TODO: reservation 정보를 받아서 생성한다.
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        reservations.add(newReservation);
+        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
+    }*/
 
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> create(@RequestBody Reservation reservation) {
         // TODO: reservation 정보를 받아서 생성한다.
         Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
         reservations.add(newReservation);
-        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
+        Long id = new ReservationDAO(jdbcTemplate).insertWithKeyHolder(newReservation);
+        return ResponseEntity.created(URI.create("/reservations/" + id)).body(newReservation);
     }
+
+    /*@DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        // TODO: url 상의 id 정보를 받아 reservation을 삭제한다.
+        Reservation reservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+
+        reservations.remove(reservation);
+
+        return ResponseEntity.noContent().build();
+    }*/
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
@@ -39,7 +74,7 @@ public class ReservationController {
                 .findFirst()
                 .orElseThrow(RuntimeException::new);
 
-        reservations.remove(reservation);
+        new ReservationDAO(jdbcTemplate).delete(id);
 
         return ResponseEntity.noContent().build();
     }
