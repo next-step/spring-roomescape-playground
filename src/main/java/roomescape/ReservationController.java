9cc28411ec2460c9977c323fc7961e7d5705dd3c
joamksh <joamksh@naver.com>
2024-03-26 19:46:05 +0900

    week2 Spring JDBC 6단계-데이터 조회하기 6단계 테스트 + @controller, @RestController구분해서 새롭게 viewController제작

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,34 +1,33 @@
 package roomescape;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-@Controller
+@RestController
 public class ReservationController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     private List<Reservation> reservations = new ArrayList<>();
     private AtomicLong index = new AtomicLong(0);
 
-    @GetMapping("/")
-    public String showHomePage() {
-        return "home";
-    }
-
-    @GetMapping("/reservation")
-    public String showReservationPage() {
-        return "reservation";
-    }
-
     @GetMapping("/reservations")
-    @ResponseBody
     public List<Reservation> getReservations() {
-        return reservations;
+        return jdbcTemplate.query("SELECT id, name, date, time FROM reservation",
+                (rs, rowNum) -> new Reservation (
+                        rs.getLong("id"),
+                        rs.getString("name"),
+                        rs.getString("date"),
+                        rs.getString("time")
+                ));
     }
 
     @PostMapping("/reservations")
@@ -47,7 +46,6 @@ public class ReservationController {
                 .body(reservation);
     }
 
-
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> cancelReservation(@PathVariable long id) {
         boolean removed = reservations.removeIf(reservation -> reservation.getId() == id);
@@ -56,5 +54,4 @@ public class ReservationController {
         }
         return ResponseEntity.noContent().build();
     }
-
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import java.sql.Connection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.Matchers.is;
@@ -107,5 +108,20 @@ public class MissionStepTest {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Test
+    void 육단계() {
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES(?, ?, ?)", "브라운", "2023-08-05", "15:40");
+
+        List<Reservation> reservations = RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200).extract()
+                .jsonPath().getList(".", Reservation.class);
+
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+
+        assertThat(reservations.size()).isEqualTo(count);
     }
 }
