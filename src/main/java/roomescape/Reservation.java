6261a6819fd6cd3faa24e845391e956ae20d12ea
pykido <animoto1@naver.com>
2024-03-28 21:40:31 +0900

    feat: [Spring MVC] 미션 제출

---
+++ src/main/java/roomescape/Reservation.java
@@ -0,0 +1,51 @@
+package roomescape;
+
+public class Reservation {
+
+    // 객체 정보
+    private Long id;
+    private String name;
+    private String date;
+    private String time;
+
+    // getter and setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    // 생성자
+    public Reservation(Long id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+}
