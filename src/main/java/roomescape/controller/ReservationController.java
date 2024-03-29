dbc4275aea9389094fb2116893fe903a1aff62c4
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2024-03-30 00:19:13 +0900

    feat: 3단계-예약 추가/취소 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,36 +1,15 @@
 package roomescape.controller;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import roomescape.domain.Reservation;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("reservation")
 public class ReservationController {
 
-    private final List<Reservation> reservations = new ArrayList<>();
-
-    public ReservationController(){
-        reservations.add(new Reservation(1L, "치이카와", LocalDate.now(), LocalTime.now()));
-        reservations.add(new Reservation(2L, "하치와레", LocalDate.now(), LocalTime.now()));
-        reservations.add(new Reservation(3L, "우사기", LocalDate.now(), LocalTime.now()));
-    }
-
-    @GetMapping("reservation")
-    public String reservation(){
+    @GetMapping
+    public String reservation() {
         return "reservation";
-    }
-
-    @GetMapping("reservations")
-    @ResponseBody
-    public List<Reservation> reservations(){
-        return reservations;
     }
 }
