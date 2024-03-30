aabbf359e5f6f32b128f9dde4599d409faae1d34
chaeseungyun <thslr57@naver.com>
2024-03-30 17:35:39 +0900

    feat: 예약 조회 api 생성

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -1,12 +1,30 @@
 package roomescape;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class RoomescapeController {
+
+  private List<Reservation> reservations = new ArrayList<>();
   @GetMapping("/")
   public String home() {
     return "home";
+  }
+
+  @GetMapping("/reservation")
+  public String reservation() {
+    return "reservation";
+  }
+
+  @GetMapping("/reservations")
+  @ResponseBody
+  public List<Reservation> reservationList() {
+    return reservations;
   }
 }
