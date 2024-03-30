46d558326194b55483908f1badf7568cf51abeac
lunak <03somin0324@gmail.com>
2024-03-30 20:50:17 +0900

    feat: 2단계 - 예약 조회 구현

---
+++ src/main/java/roomescape/Reservation.java
@@ -0,0 +1,17 @@
+package roomescape;
+
+public class Reservation {
+    private int id;
+    private String name;
+    private String date;
+    private String time;
+
+    public Reservation(int id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+
+}
