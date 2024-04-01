bc1030e13e14b4038b9e75c2146e9b70962d25ec
hunsy9 <juniper0917@gmail.com>
2024-04-01 19:43:21 +0900

    feat: 미션 3단계_예약추가/취소

--- src/main/java/roomescape/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/reservation/controller/ReservationController.java
@@ -1,22 +1,23 @@
 package roomescape.reservation.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import roomescape.reservation.domain.Reservation;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ReservationController {
     private List<Reservation> reservations = new ArrayList<>();
-    public ReservationController(){
-        // 예약 생성 로직이 없으므로, ReservationController가 생성될 때, 임의로 더미데이터를 넣었습니다.
-        reservations.add(new Reservation(1L, "유승훈1", "2023-01-01", "10:00"));
-        reservations.add(new Reservation(2L, "유승훈2", "2023-01-02", "11:00"));
-        reservations.add(new Reservation(3L, "유승훈3", "2023-01-03", "12:00"));
-    }
+
+    private AtomicLong index = new AtomicLong(1);
+
     @GetMapping("/reservation")
     public String reservation(){
         return "reservation"; //reservation.html 파일을 반환합니다.
@@ -26,5 +27,24 @@ public class ReservationController {
     @ResponseBody
     public List<Reservation> reservationList() {
         return reservations; // 더미데이터(Reservation의 리스트)를 반환합니다.
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation){
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        reservations.add(newReservation);
+        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable Long id){
+        Reservation reservation = reservations.stream()
+                .filter(item -> Objects.equals(item.getId(), id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+
+        reservations.remove(reservation);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -1,10 +1,29 @@
 package roomescape.reservation.domain;
 
+import jakarta.annotation.Nullable;
+
 public class Reservation {
     private Long id;
     private String name;
     private String date;
     private String time;
+
+    public Reservation(){
+
+    }
+
+    public Reservation(Long id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Reservation(String name, String date, String time) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
 
     public Long getId() {
         return id;
@@ -22,10 +41,7 @@ public class Reservation {
         return time;
     }
 
-    public Reservation(Long id, String name, String date, String time) {
-        this.id = id;
-        this.name = name;
-        this.date = date;
-        this.time = time;
+    public static Reservation toEntity(Reservation reservation, Long id) {
+        return new Reservation(id, reservation.name, reservation.date, reservation.time);
     }
 }
