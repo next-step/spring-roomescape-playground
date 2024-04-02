ab392d424e0ab7d3c2f6c45497b41070ba89c963
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2024-04-02 19:43:50 +0900

    chore: 패키지 정리

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -8,7 +8,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor
