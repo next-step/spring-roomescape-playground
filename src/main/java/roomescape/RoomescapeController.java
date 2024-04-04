9d42da2c0f864650e085a5fc0421c71767286827
somin <110970349+LUNA-KK@users.noreply.github.com>
2024-04-04 09:04:47 +0900

    [Spring MVC] 김소민 미션 제출합니다. (#165)
    * feat: 1단계 - 홈 화면 구현
    
    * feat: 2단계 - 예약 조회 구현
    
    * feat: 3단계 - 예약 추가 / 취소 구현
    
    * feat: 4단계 - 예외 처리 구현
    
    * feat: 경로 변수 생략
    
    * feat: atomic 초기값 할당
    
    * feat: 메소드명 변경
    
    * feat: 예외처리 변경
    
    * fix: 띄어쓰기 삭제

---
+++ src/main/java/roomescape/RoomescapeController.java
@@ -0,0 +1,64 @@
+package roomescape;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Controller
+public class RoomescapeController {
+    private List<Reservation> reservations = new ArrayList<>();
+
+    AtomicInteger atomic = new AtomicInteger(0);
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public ResponseEntity<List<Reservation>> read() {
+        return ResponseEntity.ok(reservations);
+    }
+
+    @PostMapping("/reservations")
+    @ResponseBody
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
+        if(reservation.getName().isEmpty() || reservation.getDate().isEmpty() || reservation.getDate().isEmpty()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Reservation newReservation = new Reservation(atomic.incrementAndGet(), reservation.getName(), reservation.getDate(), reservation.getTime());
+        reservations.add(newReservation);
+
+        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Void> delete(@PathVariable() int id) {
+        if(reservations.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Reservation deletedReservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("삭제할 예약이 없습니다."));
+        reservations.remove(deletedReservation);
+
+        return null;
+    }
+}
