f937cd8ae9288ef7dbfd4a8b69b52c6b6fe9eedb
공태현 <dev0taehyeon@gmail.com>
2024-04-04 23:43:58 +0900

    MVC1 Step 3 : POST / DELETE API 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,31 +1,41 @@
 package roomescape.controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 // 현재는 데이터베이스가 없으니, 임의의 DTO 를 컨트롤러에 생성!
 
 @Controller
 public class ReservationController {
+
+    public static List<Reservations> db = new ArrayList<>();
     public static class Reservations {
-        public Long id;
+        public int id; // List 를 사용하여 저장할 때, index 와 List 의 Length 를 편하게 사용하기 위해, Long -> int 로 변경
         public String name;
         public String date;
         public String time;
 
-        public Reservations(Long id, String name, String date, String time) {
+        public Reservations(int id, String name, String date, String time) {
             this.id = id;
             this.name = name;
             this.date = date;
             this.time = time;
         }
     }
+
+
+
     @GetMapping("/reservation")
     public String reservation() {
         return "reservation";
@@ -33,19 +43,51 @@ public class ReservationController {
 
     @GetMapping("/reservations")
     @ResponseBody
-    public List<Reservations> reservations() {
+    public ResponseEntity<List<Reservations>> reservations() {
         // 테스트에 맞게 3개를 생성하고 리턴
 
-       Reservations r1 = new Reservations(1L, "브라운", "2023-01-01", "10:00");
-        Reservations r2 = new Reservations(2L, "퍼플", "2023-01-02", "11:00");
-        Reservations r3 = new Reservations(3L, "그린", "2023-01-03", "12:00");
-
-        List<Reservations> response = new ArrayList<>();
-        response.add(r1);
-        response.add(r2);
-        response.add(r3);
-
-        return response;
+//        Reservations r1 = new Reservations(1L, "브라운", "2023-01-01", "10:00");
+//        Reservations r2 = new Reservations(2L, "퍼플", "2023-01-02", "11:00");
+//        Reservations r3 = new Reservations(3L, "그린", "2023-01-03", "12:00");
+//
+//        List<Reservations> response = new ArrayList<>();
+//        response.add(r1);
+//        response.add(r2);
+//        response.add(r3);
+        // 삼단계 미션으로 인한 변경.
+        return new ResponseEntity<>(db, HttpStatus.OK);
     }
 
+
+    @PostMapping("/reservations")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Reservations> reservations(@RequestBody Reservations request) {
+        /**
+         * DB 가 없으니 일단, Controller 의 Static 변수에 저장하자.
+         */
+
+        // AtomicLong 을 사용하는 것보단,
+        // DB 의 크기를 알고, 직접 가져온다면 더 좋지 않을까?
+        int index = db.size();
+
+        // Index 설정.
+        request.id = index + 1;
+
+        // DB 에 저장.
+        db.add(request);
+        return reservations(request.id);
+    }
+    @GetMapping("/reservations/{id}")
+    public ResponseEntity<Reservations> reservations(@PathVariable int id) {
+
+        System.out.println("/reservations/" + id);
+        return ResponseEntity.created(URI.create("/reservations/" + id)).body(db.get(id - 1));
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Object> reservationDelete(@PathVariable int id) {
+        db.remove(id - 1); // 제거.
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,9 +1,13 @@
 package roomescape;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.is;
 
@@ -31,10 +35,45 @@ public class MissionStepTest {
 
     @Test
     void 이단계2() {
+
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(3));
+                .body("size()", is(0)); // Static List 를 사용함으로써, 0 으로 교체.
+    }
+
+    @Test
+    void 삼단계1() {
+
+        Map<String , String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201)
+                .header("Location", "/reservations/1")
+                .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(204);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
     }
 }
