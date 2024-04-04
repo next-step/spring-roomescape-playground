9d959a84d198f9557c182dc9c221bba17fdbfd34
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-04-04 17:25:29 +0900

    8단계: 시간 관리 기능

--- src/main/java/roomescape/controller/TimeController.java
+++ src/main/java/roomescape/controller/TimeController.java
@@ -1,14 +1,39 @@
 package roomescape.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import jakarta.validation.Valid;
+import roomescape.dto.request.TimeRequest;
+import roomescape.dto.response.TimeResponse;
+import roomescape.service.TimeService;
 
 //시간 홈 화면 반환
 @Controller
 public class TimeController {
 
+    private final TimeService timeService;
+
+    public TimeController(TimeService timeService) {
+        this.timeService = timeService;
+    }
+
     @GetMapping("/time")
     public String getTime() {
         return "time";
+    }
+
+    @PostMapping("/times")
+    public ResponseEntity<TimeResponse> createTime(@Valid @RequestBody TimeRequest timeRequest) throws
+            URISyntaxException {
+            Long id = timeService.createTime(timeRequest);
+            TimeResponse timeResponse = timeService.getTime(id);
+            return ResponseEntity.created(new URI("/times/" + timeResponse.getId())).body(timeResponse);
     }
 }
