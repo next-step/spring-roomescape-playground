551b4a835390555f14b90954881680ab8f192d45
DESKTOP-F45SLBC\bnj30 <bjkim3059@gmail.com>
2024-04-06 14:09:49 +0900

    [Spring MVC:2단계-예약조회] feat: controller 폴더 생성, ReservationController.java, Reservation.java 생성, reservation, reservations url 매핑하는 world, read 메소드 생성

---
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -0,0 +1,26 @@
+package roomescape.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ReservationController {
+
+    List<Reservation> reservations = new ArrayList<>();
+
+    @GetMapping("/reservation")
+    public String world() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> read() {
+        return ResponseEntity.ok().body(reservations);
+    }
+
+
+}
