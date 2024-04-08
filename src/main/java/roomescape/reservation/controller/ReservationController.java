42e31a2bdedc5ef6e225717ebed345e00f83f78c
유승훈 <101303791+hunsy9@users.noreply.github.com>
2024-04-08 11:26:16 +0900

    [Spring MVC] hunsy9(유승훈) 미션 제출합니다. (#167)
    * feat: 미션 1단계_홈화면
    
    * feat: 미션 2단계_예약조회
    
    * feat: 미션 3단계_예약추가/취소
    
    * feat: 미션 4단계_예외처리
    
    * feat: 코드리뷰 피드백 반영

---
+++ src/main/java/roomescape/reservation/controller/ReservationController.java
@@ -0,0 +1,56 @@
+package roomescape.reservation.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import roomescape.reservation.domain.Reservation;
+import roomescape.reservation.exception.InvalidReservationException;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+public class ReservationController {
+    private List<Reservation> reservations = new ArrayList<>();
+
+    private AtomicLong index = new AtomicLong(1);
+
+    @GetMapping("/reservation")
+    public String reservation(){
+        return "reservation"; //reservation.html 파일을 반환합니다.
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> getReservations() {
+        return reservations; // 더미데이터(Reservation의 리스트)를 반환합니다.
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation){
+
+        if(reservation.getName().isEmpty() || reservation.getTime().isEmpty() || reservation.getDate().isEmpty()){
+            throw new InvalidReservationException("Invalid reservation data, Field Empty");
+        }
+
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        reservations.add(newReservation);
+        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable Long id){
+        Reservation reservation = reservations.stream()
+                .filter(item -> Objects.equals(item.getId(), id))
+                .findFirst()
+                .orElseThrow(() -> new InvalidReservationException("Reservation not found with id: " + id));
+
+        reservations.remove(reservation);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
---
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -0,0 +1,47 @@
+package roomescape.reservation.domain;
+
+import jakarta.annotation.Nullable;
+
+public class Reservation {
+    private Long id;
+    private String name;
+    private String date;
+    private String time;
+
+    private Reservation(){
+
+    }
+
+    public Reservation(Long id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Reservation(String name, String date, String time) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public static Reservation toEntity(Reservation reservation, Long id) {
+        return new Reservation(id, reservation.name, reservation.date, reservation.time);
+    }
+}
