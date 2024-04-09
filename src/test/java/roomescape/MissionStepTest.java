0f16ef0ccd3af305186a2aa843c712c263cf2737
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-04-09 18:52:50 +0900

    10단계 계층화 리팩터링
    미리 분류해놔서 딱히 수정할건 없네요..!

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.Controller.ReservationController;
 import roomescape.Domain.Reservation;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.List;
@@ -191,6 +193,23 @@ public class MissionStepTest {
                 .when().post("/reservations")
                 .then().log().all()
                 .statusCode(400);
+    }
+
+    @Autowired
+    private ReservationController reservationController;
+
+    @Test
+    void 십단계() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
     }
 
 
