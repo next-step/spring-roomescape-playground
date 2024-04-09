dcf5045bb34ccf4e334e28794fd6ce7ff9998b0d
Rulu <hafnium1923@gmail.com>
2024-04-09 20:24:47 +0900

    feat: new-reservation 페이지 연결

--- src/main/java/roomescape/presentation/HomeController.java
+++ src/main/java/roomescape/presentation/HomeController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/reservation")
     public String reservation() {
-        return "reservation";
+        return "new-reservation";
+    }
+
+    @GetMapping("/time")
+    public String time() {
+        return "time";
     }
 }
