39a0a4b0421533d80d43bf536a3e2f7f49e7a734
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2024-04-11 14:36:11 +0900

    refactor: 쿼리 제거, 예외 수정

--- src/main/java/roomescape/repository/JdbcReservationRepository.java
+++ src/main/java/roomescape/repository/JdbcReservationRepository.java
@@ -1,10 +1,13 @@
 package roomescape.repository;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import roomescape.domain.Reservation;
@@ -23,16 +26,25 @@ public class JdbcReservationRepository implements ReservationRepository {
     @Override
     public Reservation save(Reservation request) {
         String sql = "INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)";
-        jdbcTemplate.update(sql, request.getName(), request.getDate(), request.getTime());
 
-        Long id = jdbcTemplate.queryForObject("SELECT MAX(id) FROM reservation", Long.class);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                sql, new String[] {"id"});
+            ps.setString(1, request.getName());
+            ps.setObject(2, request.getDate());
+            ps.setObject(3, request.getTime());
+            return ps;
+        }, keyHolder);
+
+        Long id = keyHolder.getKey().longValue();
         return new Reservation(id, request.getName(), request.getDate(), request.getTime());
     }
 
     @Override
     public void deleteById(Long id) {
         String sql = "DELETE FROM reservation WHERE id = ?";
-        if(jdbcTemplate.update(sql, id) == 0)
+        if (jdbcTemplate.update(sql, id) == 0)
             throw new IllegalArgumentException("존재하지 않는 예약입니다.");
     }
 
