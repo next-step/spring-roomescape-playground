51822220ef5956fa1f2774df80ac0353e74a9215
pykido <animoto1@naver.com>
2024-04-15 23:16:50 +0900

    feat : 사용하지 않는 setter 삭제

--- src/main/java/roomescape/dto/Reservation.java
+++ src/main/java/roomescape/dto/Reservation.java
@@ -29,25 +29,19 @@ public class Reservation {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+
 
     public String getDate() {
         return date;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
+
 
     public String getTime() {
         return time;
     }
 
-    public void setTime(String time) {
-        this.time = time;
-    }
+
 
 
 }
