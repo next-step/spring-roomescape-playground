db5250094b0a789a9f533ea81046ef5f2113284f
pykido <animoto1@naver.com>
2024-04-21 21:06:32 +0900

    feat : 불필요한 setter 삭제

--- src/main/java/roomescape/dto/Reservation.java
+++ src/main/java/roomescape/dto/Reservation.java
@@ -16,37 +16,22 @@ public class Reservation {
         this.time = time;
     }
 
-    // getter, setter
+    // getter
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getDate() {
         return date;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
 
     public String getTime() {
         return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
     }
 
 
