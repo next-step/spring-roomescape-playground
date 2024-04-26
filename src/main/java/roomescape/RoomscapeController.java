e4a59a4f374d628648b023b8a96e6ad6df432b58
Kayoung Yoon <dev.kayoung@gmail.com>
2024-04-26 22:17:06 +0900

    예약 조회 API 구현

--- src/main/java/roomescape/RoomscapeController.java
+++ src/main/java/roomescape/RoomscapeController.java
@@ -1,13 +1,19 @@
 package roomescape;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class RoomscapeController {
+
+    private List<Reservation> reservations = List.of(
+            new Reservation(1L, "브라운", "2023-01-01", "10:00"),
+            new Reservation(2L, "브라운", "2023-01-02", "11:00")
+    );
 
     @GetMapping
     public String home() {
@@ -18,4 +24,11 @@ public class RoomscapeController {
     public String admin() {
         return "reservation";
     }
+
+    @ResponseBody
+    @GetMapping("/reservations")
+    public List<Reservation> readAllReservations() {
+        return reservations;
+    }
+
 }
