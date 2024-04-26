47f01ff9db612c961ec4c7d28a4a25a0c5ab14a2
pykido <animoto1@naver.com>
2024-04-26 16:53:27 +0900

    feat : 9단계 (테이블 스키마 재정의) 외래키 지정을 통해 reservation 테이블과 time 테이블의 관계를 설정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,9 +7,10 @@ CREATE TABLE time
 
 CREATE TABLE reservation
 (
-    id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255) NOT NULL,
-    date    VARCHAR(255) NOT NULL,
-    time    VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    date VARCHAR(255) NOT NULL,
+    time_id BIGINT,                           // TODO: 수정
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES time(id) // TODO: 추가
+    );
