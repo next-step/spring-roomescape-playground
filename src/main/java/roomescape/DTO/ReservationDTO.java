04b9766865419df921d5d644fb326ca75cc9008b
chaeseungyun <thslr57@naver.com>
2024-04-26 17:55:49 +0900

    refactor: Validation 올바르게 사용

--- src/main/java/roomescape/DTO/ReservationDTO.java
+++ src/main/java/roomescape/DTO/ReservationDTO.java
@@ -1,20 +1,17 @@
 package roomescape.DTO;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record ReservationDTO (
         @NotBlank  String name,
         @NotBlank String date,
-        @NotBlank Long time
+        @NotNull Long time
 ){
     public ReservationDTO(String name, String date, Long time) {
       this.name = name;
       this.date = date;
       this.time = time;
-
-      if(name.isEmpty() || date.isEmpty() || time == null){
-        throw new IllegalArgumentException("필요한 인자가 부족합니다!");
-      }
     }
 }
 
