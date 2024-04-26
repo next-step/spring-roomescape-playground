b14e3478f5713c6a1c7232b74e0e76b7419a13ab
Kayoung Yoon <dev.kayoung@gmail.com>
2024-04-26 22:04:17 +0900

    1단계 요구사항 구현

---
+++ src/main/java/roomescape/RoomscapeController.java
@@ -0,0 +1,15 @@
+package roomescape;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class RoomscapeController {
+
+    @GetMapping
+    public String home() {
+        return "home";
+    }
+}
