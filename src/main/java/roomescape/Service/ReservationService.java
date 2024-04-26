486443e222c1ae38cf6ea1ef76e235e65d90ced1
somin <110970349+LUNA-KK@users.noreply.github.com>
2024-04-26 17:35:38 +0900

    chore: 개행 추가
    Co-authored-by: duehee <149302959+duehee@users.noreply.github.com>

--- src/main/java/roomescape/Service/ReservationService.java
+++ src/main/java/roomescape/Service/ReservationService.java
@@ -17,7 +17,7 @@ public class ReservationService {
         return reservationRepository.getAllReservation();
     }
 
-    public Reservation createdReservation(ReservationDTO reservation){
+    public Reservation createdReservation(ReservationCreateRequestDTO reservationCreateRequest) {
         String time = reservationRepository.getTimeById(reservation.time());
         Reservation request = new Reservation(null, reservation.name(), reservation.date(), new Time(reservation.time(), time));
         return reservationRepository.createdReservation(request);
