98b851d36d8b8314f3682a062293505af794aea2
seobbang <seobbangway@naver.com>
2024-05-03 00:01:43 +0900

    feat: 3단계 요구사항 - 예약 추가 API 구현

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -1,19 +1,19 @@
 package roomescape;
 
 public class Reservation {
-    private int id;
+    private long id;
     private String name;
     private String date;
     private String time;
 
-    public Reservation(int id, String name, String date, String time) {
+    public Reservation(long id, String name, String date, String time) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
     }
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
---
+++ src/main/java/roomescape/ReservationRequestDto.java
@@ -0,0 +1,19 @@
+package roomescape;
+
+public class ReservationRequestDto {
+    private String name;
+    private String date;
+    private String time;
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
