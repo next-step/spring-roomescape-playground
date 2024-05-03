d1007fcc361594b068c048c80e24ecf3c345909e
spig0126 <spig0126@gmail.com>
2024-05-03 23:03:27 +0900

    Feat: 예약 추가/취소

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,25 +1,30 @@
 package roomescape.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 import roomescape.domain.Reservation;
+
+import static java.lang.String.valueOf;
 
 
 @Controller
 public class ReservationController {
-    private List<Reservation> reservations = Arrays.asList(
-            new Reservation(1, "브라운", "2023-01-01", "10:00"),
-            new Reservation(2, "블루", "2023-02-01", "11:00"),
-            new Reservation(3, "옐로우", "2023-05-01", "12:00")
-    );
+    private List<Reservation> reservations = new ArrayList<>(Arrays.asList(
+            new Reservation("브라운", "2023-01-01", "10:00"),
+            new Reservation("블루", "2023-02-01", "11:00"),
+            new Reservation("옐로우", "2023-05-01", "12:00")
+    ));
 
     @GetMapping("/reservation")
     public String reservation() {
@@ -31,4 +36,35 @@ public class ReservationController {
     public ResponseEntity<List<Reservation>> getReservations(){
         return ResponseEntity.ok(this.reservations);
     }
+
+    @GetMapping("/reservations/{id}")
+    @ResponseBody
+    public ResponseEntity<Reservation> getReservation(@PathVariable long id){
+        for (Reservation reservation : this.reservations) {
+            if (reservation.getId() == id) {
+                return ResponseEntity.ok(reservation);
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/reservations")
+    @ResponseBody
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation newReservation, HttpServletRequest request) {
+        this.reservations.add(newReservation);
+        String uri = "/reservations/" + valueOf(newReservation.getId());
+        return ResponseEntity.created(URI.create(uri)).body(newReservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservatioin(@PathVariable long id){
+        for (Reservation reservation : this.reservations) {
+            if (reservation.getId() == id) {
+                this.reservations.remove(reservation);
+                break;
+            }
+        }
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,24 +1,32 @@
 package roomescape.domain;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Reservation {
-    private int id;
+    private static AtomicLong index = new AtomicLong(0);
+
+    private long id;
     private String name;
     private String date;
     private String time;
 
-    public Reservation(int id, String name, String date, String time) {
-        this.id = id;
+    public Reservation(){
+        this.id = index.incrementAndGet();
+    }
+
+    public Reservation(String name, String date, String time) {
+        this.id = index.incrementAndGet();
         this.name = name;
         this.date = date;
         this.time = time;
     }
 
-    public int getId() {return this.id;}
+    public long getId() {return this.id;}
     public String getName() {return this.name;}
     public String getDate() {return this.date;}
     public String getTime() {return this.time;}
 
-    public int setId(int id) {return this.id = id;}
+    public long setId() {return this.id = index.incrementAndGet();}
     public String setName(String name) {return this.name = name;}
     public String setDate(String date) {return this.date = date;}
     public String setTime(String time) {return this.time = time;}
