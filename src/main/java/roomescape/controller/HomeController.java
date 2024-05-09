d103e003d4d4155b39209a9d890607dc8b355a3c
mereorfish <sungwon326@naver.com>
2024-05-09 19:19:48 +0900

    feat: 1단계 완료

---
+++ src/main/java/roomescape/controller/HomeController.java
@@ -0,0 +1,11 @@
+package roomescape.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String gotoHome(){
+        return "home";
+    }
+}
