da4f71393a1dc795567a26b8afbd8ca3757c58f7
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-05-10 15:46:19 +0900

    refactor: [2단계] 패키지 이동

--- src/main/java/roomescape/common/presentation/IndexController.java
+++ src/main/java/roomescape/common/presentation/IndexController.java
@@ -1,4 +1,4 @@
-package roomescape.presentation;
+package roomescape.common.presentation;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
