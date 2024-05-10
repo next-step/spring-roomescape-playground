90d9eba41fef789b0f644172fdc41233aeb78278
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-05-10 16:00:36 +0900

    test: [3단계] 학습 테스트 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -3,7 +3,10 @@ package roomescape;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,12 +40,50 @@ public class MissionStepTest {
 
         @Test
         void 예약_목록_요청() {
+            RestAssured.given().log().all()
+                    .when().get("/reservations")
+                    .then().log().all()
+                    .statusCode(200)
+                    .body("size()", is(0));
+        }
+    }
+
+    @Nested
+    class 삼단계 {
+
+        @Test
+        void 예약_생성_요청() {
             // given
-            new Reservation(
-                    1L,
-                    "브라운",
-                    LocalDateTime.now()
+            Map<String, String> params = Map.of(
+                    "name", "브라운",
+                    "date", "2023-08-05",
+                    "time", "15:40"
             );
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/reservations")
+                    .then().log().all()
+                    .statusCode(201)
+                    .header("Location", "/reservations/1")
+                    .body("id", is(1));
+
+            RestAssured.given().log().all()
+                    .when().get("/reservations")
+                    .then().log().all()
+                    .statusCode(200)
+                    .body("size()", is(1));
+        }
+
+        @Test
+        void 예약_삭제_요쳥() {
+            예약_생성_요청();
+
+            RestAssured.given().log().all()
+                    .when().delete("/reservations/1")
+                    .then().log().all()
+                    .statusCode(204);
 
             RestAssured.given().log().all()
                     .when().get("/reservations")
