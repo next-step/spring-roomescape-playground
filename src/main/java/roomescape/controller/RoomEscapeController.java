a90f84a1b7268dff8f0abbd2c9c1c50eb45464ec
mangsuyo <mangsuyo3610@gmail.com>
2024-05-11 00:43:41 +0900

    feat: reservation 페이지 렌더링

--- src/main/java/roomescape/controller/RoomEscapeController.java
+++ src/main/java/roomescape/controller/RoomEscapeController.java
@@ -3,11 +3,25 @@ package roomescape.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import roomescape.service.ReservationService;
+
 @Controller
 public class RoomEscapeController {
+
+	private final ReservationService reservationService;
+
+	public RoomEscapeController(ReservationService reservationService) {
+		this.reservationService = reservationService;
+	}
 
 	@GetMapping("/")
 	public String renderHomePage() {
 		return "home";
 	}
+
+	@GetMapping("/reservation")
+	public String renderReservationPage() {
+		return "reservation";
+	}
+
 }
