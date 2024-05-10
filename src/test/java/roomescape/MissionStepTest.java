87eae9817c10fbcaa4448ded8f50a8d51a0cf30c
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-05-10 17:02:44 +0900

    test: [4단계] 학습 테스트 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -4,16 +4,11 @@ import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import roomescape.reservation.domain.Reservation;
-import roomescape.reservation.domain.ReservationRepository;
 
 @SuppressWarnings("NonAsciiCharacters")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -90,6 +85,36 @@ public class MissionStepTest {
                     .then().log().all()
                     .statusCode(200)
                     .body("size()", is(0));
+        }
+    }
+
+    @Nested
+    class 사단계 {
+
+        @Test
+        void 필요한_인자가_없는_경우() {
+            Map<String, String> params = Map.of(
+                    "name", "브라운",
+                    "date", "2023-08-05",
+                    "time", ""
+            );
+
+            // 필요한 인자가 없는 경우
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/reservations")
+                    .then().log().all()
+                    .statusCode(400);
+        }
+
+        @Test
+        void 삭제할_예약이_없는_경우() {
+            // 삭제할 예약이 없는 경우
+            RestAssured.given().log().all()
+                    .when().delete("/reservations/1")
+                    .then().log().all()
+                    .statusCode(400);
         }
     }
 }
