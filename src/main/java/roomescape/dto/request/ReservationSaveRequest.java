f2be6a69026de28c9c24a4011dc65f225b2319b7
mangsuyo <mangsuyo3610@gmail.com>
2024-05-11 01:33:45 +0900

    feat: 예약 저장 요청 dto

---
+++ src/main/java/roomescape/dto/request/ReservationSaveRequest.java
@@ -0,0 +1,20 @@
+package roomescape.dto.request;
+
+public class ReservationSaveRequest {
+
+	private String name;
+	private String date;
+	private String time;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+}
