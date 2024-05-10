15164841e617377ddb6855231b7428ea5f860d35
Han <hkjbrian@naver.com>
2024-05-10 16:58:13 +0900

    step1, gradle 의존성 추가, 및 Controller 생성

---
+++ src/main/java/roomescape/controller/RoomescapeController.java
@@ -0,0 +1,13 @@
+package roomescape.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RoomescapeController {
+
+    @GetMapping("/")
+    public String mission() {
+        return "home";
+    }
+}
