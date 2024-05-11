661cb68e683d8a6dceecc27ac9623d17a74a91e6
Han <hkjbrian@naver.com>
2024-05-11 23:53:30 +0900

    step2 예약조회화면, 예약 조회 반환 추가

--- src/main/java/roomescape/controller/RoomescapeController.java
+++ src/main/java/roomescape/controller/RoomescapeController.java
@@ -1,13 +1,31 @@
 package roomescape.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import roomescape.domain.Reservation;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class RoomescapeController {
 
+    List<Reservation> reservations = new ArrayList<Reservation>();
+
     @GetMapping("/")
-    public String mission() {
+    public String welcomePage() {
         return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String reservationPage() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> checkReservations(){
+        reservations.add(new Reservation(1L,"hkj","2024","05-11"));
+        return ResponseEntity.ok(reservations);
     }
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -4,16 +4,30 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.domain.Reservation;
+
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+
 public class MissionStepTest {
 
     @Test
-    void 일단계() {
+    void 이단계() {
         RestAssured.given().log().all()
-                .when().get("/")
+                .when().get("/reservation")
                 .then().log().all()
                 .statusCode(200);
+    }
+
+    @Test
+    void 예약목록요청() {
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
     }
 }
