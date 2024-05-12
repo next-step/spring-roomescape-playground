6443dcda72a892cfcbc00fa1b241e084287af880
PlusUltraCode <kkd06144@naver.com>
2024-05-13 08:34:00 +0900

    feat: 4단계 예외처리 추가

--- src/main/java/roomescape/controller/HomeController.java
+++ src/main/java/roomescape/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String homePage(){
+    public String homePage() {
         return "home";
     }
 }
--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -9,14 +9,21 @@ import java.time.LocalTime;
 @Data
 @NoArgsConstructor
 @Getter
-@AllArgsConstructor
 public class Reservation {
+
     private Long id;
     private String name;
     private LocalDate date;
     private LocalTime time;
 
-    public static Reservation toEntity(Reservation reservation,Long id){
+    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+        this.id = id;
+        this.name = Exception.validateNameIsNotNull(name);
+        this.date = Exception.validateDateIsNotNull(date);
+        this.time = Exception.validateTimeIsNotNull(time);
+    }
+
+    public static Reservation toEntity(Reservation reservation, Long id){
         return new Reservation(id,
                 reservation.name,
                 reservation.date,
