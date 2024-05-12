6a38973ef3629f6634312a98a4a6a255a971a713
DaYeong <day0_ng@naver.com>
2024-05-12 21:06:29 +0900

    feat: 예약추가 API구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -3,16 +3,17 @@ package roomescape.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import roomescape.domain.Reservation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ReservationController {
     private List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(0); //예약을 위한 ID
 
     @GetMapping("/reservation")
     public String reservation() {
@@ -21,11 +22,19 @@ public class ReservationController {
 
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> getReservations() {
-
         List<Reservation> reservationList = new ArrayList<>();
-        reservationList.add(new Reservation(1, "브라운", "2023-01-01", "10:00"));
-        reservationList.add(new Reservation(2, "브라운", "2023-01-02", "11:00"));
-
         return ResponseEntity.status(HttpStatus.OK).body(reservationList);
     }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation request) {
+        int id = (int) index.incrementAndGet();
+        Reservation reservation = new Reservation(id, request.getName(), request.getDate(), request.getTime());
+        reservations.add(reservation);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("Location", "/reservations/" + id)
+                .body(reservation);
+    }
+
+
 }
