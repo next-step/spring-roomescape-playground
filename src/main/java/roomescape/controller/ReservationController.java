26e97b2d85050c5a966544e0c7bc79941ccc118b
zhy2on <dhwlgus5940@gmail.com>
2024-05-13 06:04:54 +0900

    feat: 3단계 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,23 +1,20 @@
 package roomescape.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import roomescape.dto.ReservationDTO;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ReservationController {
     private List<ReservationDTO> reservations = new ArrayList<>();
-
-    public ReservationController() {
-        reservations.add(new ReservationDTO(1L, "안녕", "2024-05-13", "04:18"));
-        reservations.add(new ReservationDTO(2L, "하세요", "2024-05-14", "04:18"));
-        reservations.add(new ReservationDTO(3L, "감사해요", "2024-05-15", "04:18"));
-    }
+    private final AtomicLong index = new AtomicLong(1);
 
     @GetMapping("/reservation")
     public String Reservation(Model model) {
@@ -29,5 +26,22 @@ public class ReservationController {
     @ResponseBody
     public List<ReservationDTO> getReservations() {
         return reservations;
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<ReservationDTO> addReservation(@RequestBody ReservationDTO reservation) {
+        long id = index.getAndIncrement();
+        ReservationDTO newReservation = new ReservationDTO(id, reservation.name(), reservation.date(), reservation.time());
+        reservations.add(newReservation);
+
+        return ResponseEntity.created(URI.create("/reservations/" + id))
+                .body(newReservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> cancelReservation(@PathVariable Long id) {
+        reservations.removeIf(r -> r.id().equals(id));
+        index.decrementAndGet();
+        return ResponseEntity.noContent().build();
     }
 }
