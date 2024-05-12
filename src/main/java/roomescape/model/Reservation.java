fbdc5c159da3274dc60fc6c5f00a6951d65050cd
PlusUltraCode <kkd06144@naver.com>
2024-05-13 07:07:25 +0900

    feat: 1단계 화면 추가

---
+++ src/main/java/roomescape/model/Reservation.java
@@ -0,0 +1,25 @@
+package roomescape.model;
+
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+public class Reservation {
+    private Long id;
+    private String name;
+    private LocalDate date;
+    private LocalTime time;
+
+    public static Reservation toEntity(Reservation reservation,Long id){
+        return new Reservation(id,
+                reservation.name,
+                reservation.date,
+                reservation.time);
+    }
+}
