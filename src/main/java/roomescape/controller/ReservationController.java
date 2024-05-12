d9ba166062d61dd46feffa236ea1e76c8aacfadd
DaYeong <day0_ng@naver.com>
2024-05-13 00:03:45 +0900

    feat: 예약 삭제 API 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -6,14 +6,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import roomescape.domain.Reservation;
 
+import java.lang.reflect.Member;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ReservationController {
     private List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong index = new AtomicLong(0); //예약을 위한 ID
+    private final AtomicLong index = new AtomicLong(0);
 
     @GetMapping("/reservation")
     public String reservation() {
@@ -23,17 +26,32 @@ public class ReservationController {
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> getReservations() {
         List<Reservation> reservationList = new ArrayList<>();
-        return ResponseEntity.status(HttpStatus.OK).body(reservationList);
+        return ResponseEntity.ok(reservations);
     }
+
 
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> addReservation(@RequestBody Reservation request) {
-        int id = (int) index.incrementAndGet();
+        long id = index.incrementAndGet();
         Reservation reservation = new Reservation(id, request.getName(), request.getDate(), request.getTime());
         reservations.add(reservation);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .header("Location", "/reservations/" + id)
-                .body(reservation);
+
+        URI location = URI.create("/reservations/" + id);
+
+        return ResponseEntity.created(location).body(reservation);
+    }
+
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> cancelReservation(@PathVariable long id) {
+        Reservation reservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+
+        reservations.remove(reservation);
+
+        return ResponseEntity.noContent().build(); // 성공(204) 상태 코드 반환
     }
 
 
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -2,6 +2,7 @@ package roomescape;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -14,7 +15,6 @@ import static org.hamcrest.Matchers.is;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
-
     @Test
     void 일단계() {
         RestAssured.given().log().all()
@@ -34,40 +34,45 @@ public class MissionStepTest {
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(2));
+                .body("size()", is(0));
     }
 
-    @Test
-    void 삼단계() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
+    @Nested
+    class 삼단계 {
+        @Test
+        void 예약_추가_확인() {
+            Map<String, String> params = new HashMap<>();
+            params.put("name", "브라운");
+            params.put("date", "2023-08-05");
+            params.put("time", "15:40");
 
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(201)
-                .header("Location", "/reservations/1")
-                .body("id", is(1));
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/reservations")
+                    .then().log().all()
+                    .statusCode(201)
+                    .header("Location", "/reservations/1")
+                    .body("id", is(1));
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
+            RestAssured.given().log().all()
+                    .when().get("/reservations")
+                    .then().log().all()
+                    .statusCode(200)
+                    .body("size()", is(1));
 
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
-                .then().log().all()
-                .statusCode(204);
+        }
+        @Test
+        void 예약_취소_확인() {
+            RestAssured.given().log().all()
+                    .when().delete("/reservations/1")
+                    .then().log().all()
+                    .statusCode(204);
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
+            RestAssured.given().log().all()
+                    .when().get("/reservations")
+                    .then().log().all();
+        }
+
     }
 }
