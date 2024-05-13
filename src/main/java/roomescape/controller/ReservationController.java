f4dd3570b73e4d21c9f4ac6bdc6b32974d7165be
mangsuyo <mangsuyo3610@gmail.com>
2024-05-13 18:41:01 +0900

    refactor: @ResponseBody 제거

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import roomescape.dto.request.ReservationSaveRequest;
@@ -25,13 +24,11 @@ public class ReservationController {
 	}
 
 	@GetMapping("/reservations")
-	@ResponseBody
 	public ResponseEntity<List<ReservationResponse>> getReservations() {
 		return reservationService.getReservations();
 	}
 
 	@PostMapping("/reservations")
-	@ResponseBody
 	public ResponseEntity<ReservationResponse> saveReservation(@RequestBody ReservationSaveRequest request) {
 		return reservationService.saveReservation(request);
 	}
