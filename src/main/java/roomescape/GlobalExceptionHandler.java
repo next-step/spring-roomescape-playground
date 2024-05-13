1efd24f3db3e72d56c89d3ca60dec68e63c9e9ff
jieun4510@sju.ac.kr <jieun4510@sju.ac.kr>
2024-05-13 11:52:33 +0900

    feat: 객체가 존재하지 않는 경우 예외처리

---
+++ src/main/java/roomescape/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package roomescape;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity handleNoSuchElementException(NoSuchElementException e) {
+        return ResponseEntity.badRequest().build();
+    }
+
+
+}
