56c15c3c2709d9a106eed5f4080ddbe4bbb0873d
Sumin <new.sumyang@gmail.com>
2024-05-13 13:37:37 +0900

    Step2 reservation

--- src/main/java/roomescape/controller/HomeController.java
+++ src/main/java/roomescape/controller/HomeController.java
@@ -1,10 +1,11 @@
 package roomescape.controller;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import roomescape.model.MemberDTO;
 import roomescape.model.repository.MemberRepository;
 
@@ -13,9 +14,11 @@ import java.util.List;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
     private List<MemberDTO> reservations = new ArrayList<>();
+    private final MemberRepository memberRepository;
 
     @GetMapping("/")
     public String home() {
@@ -23,13 +26,22 @@ public class HomeController {
     }
 
     @GetMapping("/reservations")
-    public List<MemberDTO> reservationController() {
-        MemberRepository memberRepository = new MemberRepository();
-        memberRepository.MemberAdd(new MemberDTO(), "브라운", "2023-01-01", "10:00");
-        memberRepository.MemberAdd(new MemberDTO(), "브라운", "2023-02-01", "10:00");
+    public List<MemberDTO> reservationsController() {
         List<MemberDTO> reservations = memberRepository.findAll();
         log.info("reservations = {}", reservations);
         return reservations;
     }
 
+    @GetMapping("/reservation")
+    public String reservationController(Model model) {
+        List<MemberDTO> reservations = memberRepository.findAll();
+        model.addAttribute("members", reservations);
+        return "reservation";
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.MemberAdd(new MemberDTO(), "브라운", "2023-01-01", "10:00");
+        memberRepository.MemberAdd(new MemberDTO(), "브라운", "2023-02-01", "10:00");
+    }
 }
