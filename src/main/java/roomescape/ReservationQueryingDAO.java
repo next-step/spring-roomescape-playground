87ac220d2e8b0050b314d602aa2daa353b334162
seobbang <seobbangway@naver.com>
2024-05-14 15:52:54 +0900

    feat: step 7 - 예약 추가, 삭제 쿼리 생성

--- src/main/java/roomescape/ReservationQueryingDAO.java
+++ src/main/java/roomescape/ReservationQueryingDAO.java
@@ -2,8 +2,12 @@ package roomescape;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository
@@ -24,8 +28,29 @@ public class ReservationQueryingDAO {
         return reservation;
     };
 
+
+
     public List<Reservation> getReservations() {
         String sql = "SELECT id, name, date, time FROM reservation";
         return jdbcTemplate.query(sql, actorRowMapper);
+    }
+
+    public long createReservation(ReservationRequestDto reservation) {
+        String sql = "INSERT INTO reservation(name, date, time) VALUES (?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, reservation.getName());
+            ps.setString(2, reservation.getDate());
+            ps.setString(3, reservation.getTime());
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
+    }
+
+    public int deleteReservationById(long id) {
+        String sql = "DELETE FROM reservation WHERE id = ?";
+        return jdbcTemplate.update(sql, id);
     }
 }
