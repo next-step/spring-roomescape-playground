10984589aa24ba5917eb77f87e7339271edf5bdb
seobbang <seobbangway@naver.com>
2024-05-14 15:54:15 +0900

    feat: step 7 - 예약 추가, 삭제 쿼리를 이용해 기존 코드 수정

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -7,10 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @ControllerAdvice
 class ReservationExceptionHandler {
@@ -41,44 +38,38 @@ public class ReservationController {
     public void reservation () {
     }
 
-    private AtomicLong index = new AtomicLong(0);
-
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> read() {
+//        테스트 코드를 위해 기존 ResponseDto를 이용한 코드 주석 처리
 //        ResponseDto response = new ResponseDto(HttpStatus.OK.value(), "요청이 성공적으로 처리되었습니다.", queryingDAO.getReservations() );
 //        return ResponseEntity.ok().body(response);
         return ResponseEntity.ok(queryingDAO.getReservations());
     }
 
-//    @PostMapping("/reservations")
-//    public ResponseEntity<ResponseDto> create (@RequestBody ReservationRequestDto request) {
-//        String name = request.getName();
-//        String date = request.getDate();
-//        String time = request.getTime();
-//
-//        long id = index.incrementAndGet();
-//
-//        Reservation newReservation = new Reservation(id, name, date, time);
-//        reservations.add(newReservation);
-//
-//        URI location = URI.create("/reservations/" + id);
-//
-//        ResponseDto response = new ResponseDto(HttpStatus.CREATED.value(), "예약이 성공적으로 추가되었습니다.", newReservation);
-//        return ResponseEntity.created(location).body(response);
-//    }
-//
-//    @DeleteMapping("/reservations/{id}")
-//    public ResponseEntity<Void> delete (@PathVariable Long id) {
-//        Iterator<Reservation> iterator = reservations.iterator();
-//
-//        while(iterator.hasNext()) {
-//            Reservation reservation = iterator.next();
-//            if(reservation.getId() == id) {
-//                iterator.remove();
-//                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//            }
-//        }
-//
-//        throw new NotFoundReservationException("Reservation with id " + id + " not found." );
-//    }
+    @PostMapping("/reservations")
+    public ResponseEntity<ResponseDto> create (@RequestBody ReservationRequestDto request) {
+        String name = request.getName();
+        String date = request.getDate();
+        String time = request.getTime();
+
+        ReservationRequestDto newReservation = new ReservationRequestDto(name, date, time);
+
+        long id = queryingDAO.createReservation(newReservation);
+
+        URI location = URI.create("/reservations/" + id);
+
+        ResponseDto response = new ResponseDto(HttpStatus.CREATED.value(), "예약이 성공적으로 추가되었습니다.", newReservation);
+        return ResponseEntity.created(location).body(response);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete (@PathVariable Long id) {
+        int rowsAffected = queryingDAO.deleteReservationById(id);
+
+        if(rowsAffected > 0){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }else {
+            throw new NotFoundReservationException("Reservation with id " + id + " not found." );
+        }
+    }
 }
