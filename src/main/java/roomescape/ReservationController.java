0e02d9c325202299152ac35a6a229927775ceb5d
seobbang <seobbangway@naver.com>
2024-05-14 15:26:36 +0900

    feat: step 6 - 예약 조회 쿼리를 이용해 기존 코드 수정

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -4,10 +4,7 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
-import org.thymeleaf.util.StringUtils;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -34,48 +31,54 @@ class ReservationExceptionHandler {
 @Controller
 public class ReservationController {
 
+    private ReservationQueryingDAO queryingDAO;
+
+    public ReservationController(ReservationQueryingDAO queryingDAO) {
+        this.queryingDAO = queryingDAO;
+    }
+
     @GetMapping("/reservation")
     public void reservation () {
     }
 
-    private List<Reservation> reservations = new ArrayList<>();
     private AtomicLong index = new AtomicLong(0);
 
     @GetMapping("/reservations")
-    public ResponseEntity<ResponseDto> read() {
-        ResponseDto response = new ResponseDto(HttpStatus.OK.value(), "요청이 성공적으로 처리되었습니다.", reservations );
-        return ResponseEntity.ok().body(response);
+    public ResponseEntity<List<Reservation>> read() {
+//        ResponseDto response = new ResponseDto(HttpStatus.OK.value(), "요청이 성공적으로 처리되었습니다.", queryingDAO.getReservations() );
+//        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok(queryingDAO.getReservations());
     }
 
-    @PostMapping("/reservations")
-    public ResponseEntity<ResponseDto> create (@RequestBody ReservationRequestDto request) {
-        String name = request.getName();
-        String date = request.getDate();
-        String time = request.getTime();
-
-        long id = index.incrementAndGet();
-
-        Reservation newReservation = new Reservation(id, name, date, time);
-        reservations.add(newReservation);
-
-        URI location = URI.create("/reservations/" + id);
-
-        ResponseDto response = new ResponseDto(HttpStatus.CREATED.value(), "예약이 성공적으로 추가되었습니다.", newReservation);
-        return ResponseEntity.created(location).body(response);
-    }
-
-    @DeleteMapping("/reservations/{id}")
-    public ResponseEntity<Void> delete (@PathVariable Long id) {
-        Iterator<Reservation> iterator = reservations.iterator();
-
-        while(iterator.hasNext()) {
-            Reservation reservation = iterator.next();
-            if(reservation.getId() == id) {
-                iterator.remove();
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-        }
-
-        throw new NotFoundReservationException("Reservation with id " + id + " not found." );
-    }
+//    @PostMapping("/reservations")
+//    public ResponseEntity<ResponseDto> create (@RequestBody ReservationRequestDto request) {
+//        String name = request.getName();
+//        String date = request.getDate();
+//        String time = request.getTime();
+//
+//        long id = index.incrementAndGet();
+//
+//        Reservation newReservation = new Reservation(id, name, date, time);
+//        reservations.add(newReservation);
+//
+//        URI location = URI.create("/reservations/" + id);
+//
+//        ResponseDto response = new ResponseDto(HttpStatus.CREATED.value(), "예약이 성공적으로 추가되었습니다.", newReservation);
+//        return ResponseEntity.created(location).body(response);
+//    }
+//
+//    @DeleteMapping("/reservations/{id}")
+//    public ResponseEntity<Void> delete (@PathVariable Long id) {
+//        Iterator<Reservation> iterator = reservations.iterator();
+//
+//        while(iterator.hasNext()) {
+//            Reservation reservation = iterator.next();
+//            if(reservation.getId() == id) {
+//                iterator.remove();
+//                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//            }
+//        }
+//
+//        throw new NotFoundReservationException("Reservation with id " + id + " not found." );
+//    }
 }
