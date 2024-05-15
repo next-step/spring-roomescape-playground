ac2e5bf0093d780882184d997923bb61178a704e
juanxiu <2316007@ewhain.net>
2024-05-15 20:17:02 +0900

    feat 5,6,7단계

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,6 +2,10 @@ package roomescape.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
+import java.sql.PreparedStatement;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -46,9 +51,7 @@ public class ReservationController {
 
     //예약 추가
 
-
-    @PostMapping("/reservations")
-    public ResponseEntity<Reservation> create (@RequestBody Reservation request) {
+    public ResponseEntity<Reservation> create(@RequestBody Reservation request) {
         long id = index.incrementAndGet();
 
         Reservation newReservation = new Reservation(id, request.getName(), request.getDate(), request.getTime());
@@ -61,8 +64,8 @@ public class ReservationController {
 
     // 예약 삭제
 
-    @DeleteMapping("/reservations/{id}")
-    public ResponseEntity<Void> delete (@PathVariable Long id) {
+
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         if (reservations.stream().noneMatch(reservation -> reservation.getId().equals(id))) {
             return ResponseEntity.notFound().build();
         }
@@ -76,5 +79,80 @@ public class ReservationController {
         return ResponseEntity.noContent().build();
     }
 
+
+
+//데이터베이스 조회
+    private JdbcTemplate jdbcTemplate;
+
+    public ReservationController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<Reservation> reservationRowMapper = (resultSet, rowNum) -> {
+
+        Reservation reservation = new Reservation(
+                resultSet.getLong("id"),
+                resultSet.getString("name"),
+                resultSet.getString("time"),
+                resultSet.getString("date")
+        );
+        return reservation;
+    };
+
+    public List<Reservation> findReservation(){
+        String sql = "SELECT id, name, date, time FROM reservation";
+        List<Reservation> reservationList = jdbcTemplate.query(sql, reservationRowMapper);
+        return reservationList;
+    }
+
+
+
+// 데이터베이스 수정, 삭제
+
+    @PostMapping("/reservations") // @PostMapping("/reservations"): 1. HTTP 메서드 POST이고, 2.괄호 안에 URL = /reservations 일 때 요청 처리를 해라!! 근데 HTTP와 URL이 둘 다 같은 데 두 개 이상 있으면 에러!
+    @ResponseBody
+    public ResponseEntity<Reservation> newReservation(@RequestBody Reservation reservation){
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        String sql = "insert into reservation (name, date, time) values (?, ?, ?)";
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(
+                    sql,
+                    new String[]{"id"}
+            );
+            ps.setString(1, reservation.getName());
+            ps.setString(2, reservation.getDate());
+            ps.setString(3, reservation.getTime());
+            return ps;
+
+        }, keyHolder);
+        Long id = keyHolder.getKey().longValue();
+        Reservation added = new Reservation(id, reservation.getName(), reservation.getDate(), reservation.getName());
+        return ResponseEntity.created(URI.create("/reservations/" + id)).body(added);
+    }
+
+
+    @DeleteMapping("/reservations/{id}") // 여기도 마찬가지. HTTP 메서드가 Delete 이고, URL이 /reservations/{id} 일 때 요청 처리를 해라.
+    public ResponseEntity<Void> deleteReservation(@PathVariable long id) throws Exception {
+
+        // 요청으로 들어온 id의 reservation이 없는 경우 기존 로직은 예외를 던진다.
+        String query = "SELECT COUNT(*) FROM reservation WHERE id = ?";
+        int count = jdbcTemplate.queryForObject(query, Integer.class, id);
+        if (count == 0) {
+            throw new Exception("해당 id가 없습니다.");
+        }
+
+        String sql = "delete from reservation where id = ?";
+        jdbcTemplate.update(sql, id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
+
+    
+    
+    
+    
 
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -2,15 +2,19 @@ package roomescape;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.controller.ReservationController;
+import roomescape.domain.Reservation;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -103,6 +107,64 @@ public class MissionStepTest{
             throw new RuntimeException(e);
         }
     }
+
+
+
+
+
+    @Test
+    void 육단계() {
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05", "15:40");
+
+        ReservationController reservationController = new ReservationController(jdbcTemplate);
+        List<Reservation> reservationList = reservationController.findReservation();
+
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+
+        assertThat(reservationList.size()).isEqualTo(count); //가져온 목록의 크기와 데이터베이스에 있는 예약의 수를 비교하여 일치하는지 확인
+    }
+    /*
+    void 육단계() {
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05", "15:40");
+
+        List<Reservation> reservationList = RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200).extract()
+                .jsonPath().getList(".", Reservation.class);
+
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+
+        assertThat(reservationList.size()).isEqualTo(count);
+     */
+
+    @Test
+    void 칠단계() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201)
+                .header("Location", "/reservations/1");
+
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+        assertThat(count).isEqualTo(1);
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(204);
+
+        Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+        assertThat(countAfterDelete).isEqualTo(0);
+    }
+
 
 }
 
