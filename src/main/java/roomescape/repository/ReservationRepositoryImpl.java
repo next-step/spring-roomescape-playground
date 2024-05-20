e7517800959e85370496843677d6b42f8345b5ac
nyeroni <nir2y@naver.com>
2024-05-20 23:25:31 +0900

    feat: save 추가

--- src/main/java/roomescape/repository/ReservationRepositoryImpl.java
+++ src/main/java/roomescape/repository/ReservationRepositoryImpl.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface ReservationRepositoryImpl {
 
     List<Reservation> findAll();
+    Reservation save(Reservation reservation);
     Reservation findById(Long id);
     void deleteById(Long id);
 }
