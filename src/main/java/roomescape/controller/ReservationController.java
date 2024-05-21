bb448c29289f7ba18f9001127326adfa551e43e6
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-05-22 08:18:49 +0900

    [Spring MVC] 조태현 미션 제출합니다. (#226)
    * chore: 스프링 스타터 웹 모듈 추가
    
    * chore: 타임리프 모듈 추가
    
    * 1단계 구현 완료
    
    * 2단계 구현 완료
    
    * 3단계 예약 추가 API 구현 완료
    
    * 3단계 예약 삭제 API 구현 완료
    
    * 4단계 예외 처리 구현 완료
    
    * 4단계 @ControllerAdvice 적용
    
    * refactor: @ResponseBody 제거
    
    - ResponseEntity 반환 시 @ResponseBody 어노테이션 제거
    
    * refactor: 코드리뷰 반영
    - RequestValidator에서 수행하던 DTO 검증 로직을 DTO 클래스 내에 위치시킴
    
    * refactor: 코드리뷰 반영
    - Repository 객체 구현
    - 삭제 요청 시 ID 검증도 Repository에서 진행

---
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -0,0 +1,76 @@
+package roomescape.controller;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import roomescape.dto.ReservationRequestDto;
+import roomescape.dto.ReservationResponseDto;
+import roomescape.mapper.DtoMapper;
+import roomescape.model.Reservation;
+import roomescape.repository.ReservationRepository;
+
+@Controller
+public class ReservationController {
+
+    private final ReservationRepository repository;
+
+    private final AtomicLong idMaker = new AtomicLong();
+
+    public ReservationController(final ReservationRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/reservation")
+    public String getReservation() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<ReservationResponseDto> getReservations() {
+        return repository.findAll().stream()
+                .map(DtoMapper::convertToDTO)
+                .toList();
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<ReservationResponseDto> saveReservation(@RequestBody ReservationRequestDto dto) {
+        final Reservation reservation = new Reservation(
+                idMaker.incrementAndGet(),
+                dto.getName(),
+                dto.getDate(),
+                dto.getTime());
+        repository.save(reservation);
+        final ReservationResponseDto responseDto = DtoMapper.convertToDTO(reservation);
+        return ResponseEntity
+                .created(URI.create("/reservations/" + responseDto.getId()))
+                .body(responseDto);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // 테스트 데이터 삽입용 메서드
+    private List<Reservation> createReservations() {
+
+        return List.of(
+                new Reservation(idMaker.incrementAndGet(), "해쉬", LocalDate.now(), LocalTime.now()),
+                new Reservation(idMaker.incrementAndGet(), "브라운", LocalDate.now(), LocalTime.now()),
+                new Reservation(idMaker.incrementAndGet(), "버거", LocalDate.now(), LocalTime.now())
+        );
+    }
+
+}
---
+++ src/main/java/roomescape/exception/ReservationException.java
@@ -0,0 +1,15 @@
+package roomescape.exception;
+
+public class ReservationException extends RuntimeException {
+
+    private final ErrorMessage errorMessage;
+
+    public ReservationException(ErrorMessage message) {
+        this.errorMessage = message;
+    }
+
+    public ErrorMessage getErrorMessage() {
+        return errorMessage;
+    }
+
+}
