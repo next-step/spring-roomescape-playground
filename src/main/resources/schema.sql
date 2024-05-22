24a1195f7932f6471bbf65e5684ecce144245f7c
mereorfish <sungwon326@naver.com>
2024-05-22 23:07:36 +0900

    refactor: 6단계 7단계 테스트 통과하도록 수정 Reservation의 기본 생성자 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,7 @@ CREATE TABLE reservation
     time    VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
+INSERT INTO reservation VALUES (1, '윤성원', '2024-05-22', '10:14');
+INSERT INTO reservation VALUES (2, '윤성원', '2024-05-23', '10:15');
+INSERT INTO reservation VALUES (3, '윤성원', '2024-05-24', '10:16');
