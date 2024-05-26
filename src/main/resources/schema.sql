22d4beb86cf93649d1415e3fcb743f43e932017f
kokodak <kokodakadokok@gmail.com>
2024-05-26 22:07:40 +0900

    chore: 스키마 Sql 업데이트

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,13 @@
 CREATE TABLE reservation
 (
-    id   BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    reservation_date_time TIMESTAMP NOT NULL
+    id                    BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name                  VARCHAR(255) NOT NULL,
+    reservation_date_time TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE time
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    time VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
