df683da69dbd3afb7b413e1813ada26ca344905e
whxogus215 <whxogus215@naver.com>
2024-05-26 22:33:28 +0900

    test: 9단계 테스트 코드 추가

--- src/test/java/roomescape/CoreStepTest.java
+++ src/test/java/roomescape/CoreStepTest.java
@@ -14,10 +14,14 @@ import org.springframework.test.annotation.DirtiesContext;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class CoreStepTest {
 
+    private static final int TEST_DATA_COUNT = 3;
+
     @Test
     void 팔단계() {
         Map<String, String> params = new HashMap<>();
         params.put("time", "10:00");
+
+        final int newId = TEST_DATA_COUNT + 1;
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -25,18 +29,34 @@ public class CoreStepTest {
                 .when().post("/times")
                 .then().log().all()
                 .statusCode(201)
-                .header("Location", "/times/1");
+                .header("Location", "/times/" + newId);
 
         RestAssured.given().log().all()
                 .when().get("/times")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(1));
+                .body("size()", is(TEST_DATA_COUNT + 1));
 
         RestAssured.given().log().all()
-                .when().delete("/times/1")
+                .when().delete("/times/" + newId)
                 .then().log().all()
                 .statusCode(204);
     }
+
+    @Test
+    void 구단계() {
+        Map<String, String> reservation = new HashMap<>();
+        reservation.put("name", "브라운");
+        reservation.put("date", "2023-08-05");
+        reservation.put("time", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservation)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(400);
+    }
+
 
 }
