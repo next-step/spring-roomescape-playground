8ae08618f9f76b3c966db0ab9a10669098f7cfaf
DaYeong <day0_ng@naver.com>
2024-05-28 00:41:53 +0900

    fix

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,65 +1,51 @@
 package roomescape.domain;
 
-import roomescape.exception.InvalidRequestException;
+import java.sql.Time;
+import java.time.LocalDate;
 
 public class Reservation {
-    private long id;
+    private Long id;
     private String name;
-    private String date;
-    private String time;
+    private LocalDate date;
+    private Time time;
 
-    private final String INVALID_NAME_REQUEST_MESSAGE = "이름 정보가 공백입니다.";
-    private final String INVALID_DATE_REQUEST_MESSAGE = "날짜 정보가 공백입니다.";
-    private final String INVALID_TIME_REQUEST_MESSAGE = "시간 정보가 공백입니다.";
-
-    public Reservation(long id, String name, String date, String time) {
+    public Reservation(Long id, String name, LocalDate date, Time time) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
     }
 
-    public long getId() {
+    // Getters and setters
+    public Long getId() {
         return id;
     }
 
-    public String getDate() {
-        return date;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getTime() {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
         return time;
     }
 
-    public void Validate(){
-        nameValidate();
-        dateValidate();
-        timeValidate();
-    }
-
-    public void nameValidate() {
-        if (isBlank(name)) {
-            throw new InvalidRequestException(INVALID_NAME_REQUEST_MESSAGE);
-        }
-    }
-
-    public void dateValidate() {
-        if (isBlank(date)) {
-            throw new InvalidRequestException(INVALID_DATE_REQUEST_MESSAGE);
-        }
-    }
-
-    public void timeValidate() {
-        if (isBlank(time)) {
-            throw new InvalidRequestException(INVALID_TIME_REQUEST_MESSAGE);
-        }
-    }
-
-    private boolean isBlank(String str) {
-        return str == null || str.trim().isEmpty();
+    public void setTime(Time time) {
+        this.time = time;
     }
 }
