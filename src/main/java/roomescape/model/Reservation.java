403eca56540b87f5b0c9ad6dafcd85ad6657d135
Sumin <new.sumyang@gmail.com>
2024-05-27 15:09:15 +0900

    schema와 Reservation time 수정

--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -3,6 +3,8 @@ package roomescape.model;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+import java.sql.Time;
+
 @Data
 public class Reservation {
     int id;
@@ -14,12 +16,12 @@ public class Reservation {
     String date;
 
     @NotBlank
-    String time;
+    Time time;
 
     public Reservation(){
     }
 
-    public Reservation(String name, String date, String time) {
+    public Reservation(String name, String date, Time time) {
         this.name = name;
         this.date = date;
         this.time = time;
