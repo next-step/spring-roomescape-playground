c33472a2c0704e3632c3270bc6c7563a29b2f554
byjo <buyongjo@gmail.com>
2024-05-27 13:05:30 +0900

    Revert "[Spring JDBC] 윤성원 미션 제출합니다. (#238)"
    This reverts commit 14339c453249962ae58f733b0776a311faea273a.

--- src/main/java/roomescape/domain/TimeFormatter.java
+++
@@ -1,8 +0,0 @@
-package roomescape.domain;
-
-import java.time.format.DateTimeFormatter;
-
-public class TimeFormatter {
-    public static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    public static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-}
