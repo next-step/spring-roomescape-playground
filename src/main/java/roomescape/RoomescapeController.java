86dcc4d85847d8c33e50c4c22f41163e28c8c468
Junyeong-An <2117ab@gmail.com>
2024-06-27 21:23:24 +0900

    2단계 - 예약 조회

---
+++ src/main/java/roomescape/RoomescapeController.java
@@ -0,0 +1,33 @@
+package roomescape;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class RoomescapeController {
+
+    private List<Reservation> reservations = new ArrayList<>();
+
+    @GetMapping("/reservation")
+    public String reservation() {
+
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public ResponseEntity<List<Reservation>> reservations() {
+        reservations.add(new Reservation(1, "브라운", "2023-01-01", "10:00"));
+        reservations.add(new Reservation(2, "브라운", "2021-01-02", "11:00"));
+        reservations.add(new Reservation(3, "브라운", "2021-01-03", "12:00"));
+
+        return new ResponseEntity<>(reservations, HttpStatus.OK);
+    }
+
+}
