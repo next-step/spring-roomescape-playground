18bcf4d1f7190b0d58409e9e3db36d2c3baccec4
김혜준 <alice5855@naver.com>
2024-07-01 14:14:28 +0900

    3단계

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,5 +1,7 @@
 package roomescape;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -17,9 +19,9 @@ public class ReservationController {
     private final AtomicLong index = new AtomicLong(1);
 
     public ReservationController() {
-        reservations.add(new Reservation(index.getAndIncrement(), "김혜준", "2023-01-01", "10:00"));
-        reservations.add(new Reservation(index.getAndIncrement(), "김혜준", "2023-01-02", "11:00"));
-        reservations.add(new Reservation(index.getAndIncrement(), "김혜준", "2023-01-03", "12:00"));
+//        reservations.add(new Reservation(index.getAndIncrement(), "김혜준", "2023-01-01", "10:00"));
+//        reservations.add(new Reservation(index.getAndIncrement(), "김혜준", "2023-01-02", "11:00"));
+//        reservations.add(new Reservation(index.getAndIncrement(), "김혜준", "2023-01-03", "12:00"));
     }
 
     // reservation HTML 연결
@@ -31,9 +33,34 @@ public class ReservationController {
     @GetMapping("/reservations")
     @ResponseBody
     public ResponseEntity<List<Reservation>> reservations(){
-        return ResponseEntity.ok(reservations);
+        return new ResponseEntity<>(reservations, HttpStatus.OK);
     }
 
 
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> create(@RequestBody Reservation reservation){
+        Reservation newReservation = new Reservation(index.getAndIncrement(), reservation.getName(), reservation.getDate(), reservation.getTime());
+        reservations.add(newReservation);
+        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId()))
+                .body(newReservation);
+    }
+
+
+    @DeleteMapping("/reservations/{id}")
+    @ResponseBody
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        Reservation reservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+        reservations.remove(reservation);
+
+        return ResponseEntity.noContent().build();
+    }
+
+//    @ExceptionHandler(NotFoundReservationException.class)
+//    public ResponseEntity<String> handleException(NotFoundReservationException e) {
+//        return ResponseEntity.badRequest().build();
+//    }
 }
 
