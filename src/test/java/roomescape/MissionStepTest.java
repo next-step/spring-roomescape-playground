2869a35d8f88106aaff322bc74a20990344c93fc
junseoplee <junseoplee@soongsil.ac.kr>
2024-08-06 17:52:34 +0900

    test: 5단계 - 데이터베이스 적용하기 테스트 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,15 +1,20 @@
 package roomescape;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.http.HttpHeaders;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -94,4 +99,19 @@ public class MissionStepTest {
                .then().log().all()
                .statusCode(HttpStatus.BAD_REQUEST.value());
   }
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  @Test
+  void 오단계() {
+    try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+      assertThat(connection).isNotNull();
+      assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+      assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
 }
