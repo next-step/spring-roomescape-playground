542d28e53aa30eb3fef981917f23c7500671f088
gaxunsxo <seoge111493@gmail.com>
2024-08-12 01:30:32 +0900

    refactor: 필수 인자 검증 로직 분리

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -4,9 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import roomescape.dto.Reservation;
+import roomescape.domain.Reservation;
 import roomescape.exception.NotFoundReservationException;
 
 import java.net.URI;
@@ -18,41 +17,37 @@ import java.util.concurrent.atomic.AtomicLong;
 @Controller
 public class ReservationController {
 
-    // 임시 예약 목록
     private List<Reservation> reservations = new ArrayList<>();
     private final AtomicLong index = new AtomicLong(1);
 
-    // 예약 페이지 응답
     @GetMapping("/reservation")
     public String showReservationPage() {
         // template/reservation.html을 반환
         return "reservation";
     }
 
-    // 예약 추가
+    private boolean isValidReservation(Reservation reservation) {
+        return reservation.getName() != null && !reservation.getName().isEmpty() &&
+                reservation.getDate() != null && !reservation.getDate().isEmpty() &&
+                reservation.getTime() != null && !reservation.getTime().isEmpty();
+    }
+
     @PostMapping("/reservations")
     @ResponseBody
-    public ResponseEntity<Reservation> create(@RequestBody Reservation reservation) {
-        // TODO: 예약 정보를 받아 예약을 추가한다.
-        // 필수 인자 검증
-        if (reservation.getName() == null || reservation.getName().isEmpty() ||
-                reservation.getDate() == null || reservation.getDate().isEmpty() ||
-                reservation.getTime() == null || reservation.getTime().isEmpty()) {
-            return ResponseEntity.badRequest().build();
-        }
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
+       if (!isValidReservation(reservation)) {
+           return ResponseEntity.badRequest().build();
+       }
 
         Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
         reservations.add(newReservation);
         return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId()))
-                .contentType(MediaType.APPLICATION_JSON)
                 .body(newReservation);
     }
 
-    // 예약 취소
     @DeleteMapping("/reservations/{id}")
     @ResponseBody
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        // TODO: url 상의 id 정보를 받아 예약을 취소(삭제)한다.
         Reservation reservation = reservations.stream()
                 .filter(it -> Objects.equals(it.getId(), id))
                 .findFirst()
@@ -63,7 +58,6 @@ public class ReservationController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    // 예약 목록 조회 API
     @GetMapping("/reservations")
     @ResponseBody
     public ResponseEntity<List<Reservation>> getReservations() {
