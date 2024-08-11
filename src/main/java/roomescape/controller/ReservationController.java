6502b3435dafd83c8ca3c93cd7e0a99a588560d7
gaxunsxo <seoge111493@gmail.com>
2024-08-12 02:02:52 +0900

    feat: 예약 추가/삭제 API 로직을 데이터베이스를 활용하도록 수정

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -17,9 +17,6 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ReservationController {
-
-    private List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong index = new AtomicLong(1);
     private final ReservationService reservationService;
 
     @Autowired
@@ -32,36 +29,28 @@ public class ReservationController {
         return "reservation";
     }
 
-    private boolean isValidReservation(Reservation reservation) {
-        return reservation.getName() != null && !reservation.getName().isEmpty() &&
-                reservation.getDate() != null && !reservation.getDate().isEmpty() &&
-                reservation.getTime() != null && !reservation.getTime().isEmpty();
-    }
-
     @PostMapping("/reservations")
     @ResponseBody
     public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
-       if (!isValidReservation(reservation)) {
+       if (!reservationService.isValidReservation(reservation)) {
            return ResponseEntity.badRequest().build();
        }
 
-        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
-        reservations.add(newReservation);
-        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId()))
+       Reservation newReservation = reservationService.createReservation(reservation);
+
+       return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId()))
                 .body(newReservation);
     }
 
     @DeleteMapping("/reservations/{id}")
     @ResponseBody
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        Reservation reservation = reservations.stream()
-                .filter(it -> Objects.equals(it.getId(), id))
-                .findFirst()
-                .orElseThrow(() -> new NotFoundReservationException("Reservation not found for ID: " + id));
-
-        reservations.remove(reservation);
-
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    public ResponseEntity<?> deleteReservation(@PathVariable Long id) {
+        boolean isDeleted = reservationService.deleteReservation(id);
+        if (isDeleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @GetMapping("/reservations")
