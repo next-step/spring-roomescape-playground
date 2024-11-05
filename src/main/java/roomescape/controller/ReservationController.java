ff0013cb61786f5d5e4eddc99a68d7aef46fad92
jermany17 <leejm5050@naver.com>
2024-11-05 11:08:40 +0900

    3단계 - 예약 추가 / 취소

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,32 +1,61 @@
 package roomescape.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import roomescape.model.Reservation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ReservationController {
 
     private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
 
-    public ReservationController() {
-        reservations.add(new Reservation(1L, "브라운", "2023-01-01", "10:00"));
-        reservations.add(new Reservation(2L, "브라운", "2023-01-02", "11:00"));
-        reservations.add(new Reservation(3L, "브라운", "2023-01-03", "12:00"));
-    }
-
+    // 예약 관리 페이지
     @GetMapping("/reservation")
     public String reservationPage() {
         return "reservation";
     }
 
+    // 예약 조회
     @GetMapping("/reservations")
     @ResponseBody
     public List<Reservation> getReservations() {
         return reservations;
+    }
+
+    // 예약 추가
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation newReservation) {
+        long id = index.getAndIncrement();
+        newReservation.setId(id);
+        reservations.add(newReservation);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/reservations/" + id);
+
+        return new ResponseEntity<>(newReservation, headers, HttpStatus.CREATED);
+    }
+
+    // 예약 삭제
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+        Optional<Reservation> reservationToDelete = reservations.stream()
+                .filter(reservation -> reservation.getId().equals(id))
+                .findFirst();
+
+        if (reservationToDelete.isPresent()) {
+            reservations.remove(reservationToDelete.get());
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
