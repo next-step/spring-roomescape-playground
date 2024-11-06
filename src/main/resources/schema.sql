8dd0e333360f6fe18cc0ad89a04a7427623071bc
sinji <tobeabestns@naver.com>
2024-11-06 18:28:56 +0900

    docs(schema.sql): sql 파일의 주석 표시 변경(// -> --)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE RESERVATION
 (
     id   BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
-    date VARCHAR(10) NOT NULL, // yyyy-mm-dd -> 10 글자
-    time VARCHAR(5)  NOT NULL // hh-mm -> 5 글자
+    date VARCHAR(10) NOT NULL, -- yyyy-mm-dd -> 10 글자
+    time VARCHAR(5)  NOT NULL -- hh-mm -> 5 글자
 );
