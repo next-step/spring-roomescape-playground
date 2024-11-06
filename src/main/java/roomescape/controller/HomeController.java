4698441037642277f9972d95f1c55707d432d452
sinji <tobeabestns@naver.com>
2024-11-06 20:59:08 +0900

    refactor(HomeController): 예약 관련 Get 메서드 이동(->ReservationController)

--- src/main/java/roomescape/controller/HomeController.java
+++ src/main/java/roomescape/controller/HomeController.java
@@ -2,7 +2,6 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import roomescape.model.Reservation;
 
 @Controller
 public class HomeController {
@@ -10,10 +9,5 @@ public class HomeController {
     @GetMapping("/home")
     public String home() {
         return "home";
-    }
-
-    @GetMapping("/reservation")
-    public String reservation() {
-        return "reservation";
     }
 }
