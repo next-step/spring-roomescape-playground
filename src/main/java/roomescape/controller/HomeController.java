e2e626d2183dc4e8e9efba2a425713daa479a255
sinji <tobeabestns@naver.com>
2024-11-06 18:30:12 +0900

    refactor(HomeController): '예약관리' 페이지 추가

--- src/main/java/roomescape/controller/HomeController.java
+++ src/main/java/roomescape/controller/HomeController.java
@@ -2,6 +2,7 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import roomescape.model.Reservation;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,10 @@ public class HomeController {
     @GetMapping("/home")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
     }
 }
