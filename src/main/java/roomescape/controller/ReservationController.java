f80c2d19fd03d89389a3c9cc7ae66398678270a8
sinji <tobeabestns@naver.com>
2024-11-13 19:20:27 +0900

    refactor(ReservationController): [7단계] 데이터 CRUD 방식 변경(List 에 저장 -> 쿼리문 사용)

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,5 +1,8 @@
 package roomescape.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import roomescape.exception.InvalidReservationParameterException;
 import roomescape.exception.NotFoundReservationException;
 import org.springframework.http.HttpStatus;
@@ -8,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import roomescape.model.Reservation;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -16,8 +21,8 @@ import java.util.concurrent.atomic.AtomicLong;
 @Controller
 public class ReservationController {
 
-    private List<Reservation> reservations = new ArrayList<>();
-    private AtomicLong index = new AtomicLong(1);
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @GetMapping("/reservation")
     public String reservationPage() {
@@ -27,36 +32,45 @@ public class ReservationController {
     @GetMapping("/reservations")
     @ResponseBody
     public List<Reservation> getReservations() {
-        return reservations;
+        String sql = "SELECT * FROM reservation";
+        return jdbcTemplate.query(sql, new ReservationRowMapper());
     }
 
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> addReservation(@RequestBody Map<String, String> params) {
-
-        Reservation reservation = new Reservation(params.get("name"), params.get("date"), params.get("time"));
-
-        if (reservation.getName() == null || reservation.getName().isEmpty() ||
-                reservation.getDate() == null || reservation.getDate().isEmpty() ||
-                reservation.getTime() == null || reservation.getTime().isEmpty()) {
+        if (params.get("name") == null || params.get("name").isEmpty() ||
+                params.get("date") == null || params.get("date").isEmpty() ||
+                params.get("time") == null || params.get("time").isEmpty()) {
             throw new InvalidReservationParameterException("예약 내용에 누락된 부분이 있습니다.");
         }
 
-        long id = index.getAndIncrement();
-        reservation.setId(id);
-        reservations.add(reservation);
+        Reservation reservation = new Reservation(params.get("name"), params.get("date"), params.get("time"));
+
+        String sql = "INSERT INTO reservation (name,date,time) VALUES (?,?,?)";
+        jdbcTemplate.update(sql, reservation.getName(), reservation.getDate(), reservation.getTime());
 
         return ResponseEntity.status(HttpStatus.CREATED)
-                .header("Location", "/reservations/" + reservation.getId())
+                .header("Location", "/reservations/")
                 .body(reservation);
     }
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
-        boolean isRemoved = reservations.removeIf(reservation -> reservation.getId().equals(id));
+        String sql = "DELETE FROM reservation WHERE id = ?";
+        int rowAffected = jdbcTemplate.update(sql, id);
 
-        if (!isRemoved) {
+        if (rowAffected == 0) {
             throw new NotFoundReservationException("삭제하려는 예약이 없습니다.");
         }
         return ResponseEntity.noContent().build();
+    }
+
+    private static class ReservationRowMapper implements RowMapper<Reservation> {
+        @Override
+        public Reservation mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Reservation reservation = new Reservation(rs.getString("name"), rs.getString("date"), rs.getString("time"));
+            reservation.setId(rs.getLong("id"));
+            return reservation;
+        }
     }
 }
