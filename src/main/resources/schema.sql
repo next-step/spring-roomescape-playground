f25be5697c3311846e5378b1b59bdbda6e4cc673
sinji <tobeabestns@naver.com>
2024-11-13 17:03:26 +0900

    refactor(schema.sql): [5단계] MVC 에서 생성한 Reservation 정보와 동기화

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,7 @@
-CREATE TABLE users (
-                       id BIGINT PRIMARY KEY AUTO_INCREMENT,
-                       name VARCHAR(50) NOT NULL,
-                       email VARCHAR(100) UNIQUE NOT NULL,
-                       password VARCHAR(255) NOT NULL
+CREATE TABLE RESERVATION
+(
+    id   BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    date VARCHAR(10) NOT NULL, // yyyy-mm-dd -> 10 글자
+    time VARCHAR(5)  NOT NULL // hh-mm -> 5 글자
 );
