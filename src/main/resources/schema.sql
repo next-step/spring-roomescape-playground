37ea51274331a66fd9ce6d1627be37632369b56d
sinji <tobeabestns@naver.com>
2024-11-13 16:43:10 +0900

    docs(schema.sql): [5단계] 테이블 스키마 정의(테이블명은 임의로 설정)

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+                       id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                       name VARCHAR(50) NOT NULL,
+                       email VARCHAR(100) UNIQUE NOT NULL,
+                       password VARCHAR(255) NOT NULL
+);
