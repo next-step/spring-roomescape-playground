801dd857faf58ace802370e307902117c04ed357
sinji <tobeabestns@naver.com>
2024-11-20 23:51:53 +0900

    refactor(GlobalExceptionHandler): ControllerAdvice 에 assignableTypes 추가

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -4,13 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import roomescape.controller.ReservationController;
 
-@ControllerAdvice
+@ControllerAdvice(assignableTypes = ReservationController.class)
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({NotFoundReservationException.class, InvalidReservationParameterException.class, IllegalArgumentException.class})
     public ResponseEntity<String> handleIllegalArgumentException(RuntimeException exception) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST) // status: 400
                 .body(exception.getMessage());
     }
 }
