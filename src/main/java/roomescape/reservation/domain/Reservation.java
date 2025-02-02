79f3e257017111bcb6b187497e7ebe3cc59759d0
heehunjung <heehunjung3658@gmail.com>
2025-02-02 23:39:01 +0900

    feat: Reservation 엔티티 작성
    더미 데이터 생성 메서드도 작성

---
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -0,0 +1,33 @@
+package roomescape.reservation.domain;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reservation {
+
+    private Long id;
+
+    private String name;
+
+    private LocalDate date;
+
+    private LocalTime time;
+
+    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public static List<Reservation> makeDummyData() {
+        List<Reservation> reservations = new ArrayList<>();
+        reservations.add(new Reservation(1L, "비토", LocalDate.of(2023, 1, 1), LocalTime.of(10, 1)));
+        reservations.add(new Reservation(2L, "곰곰", LocalDate.of(2023, 1, 2), LocalTime.of(10, 2)));
+        reservations.add(new Reservation(3L, "망고", LocalDate.of(2023, 1, 3), LocalTime.of(10, 3)));
+
+        return reservations;
+    }
+}
