7235556b787dfd5d9d84e667d000c398b82fcac8
JMJ <ghtntkdnsk@naver.com>
2025-02-02 22:26:44 +0900

    feat: 예약 페이지 로드 및 예약 목록 조회 기능 추가

--- src/main/java/roomescape/controller/RoomEscapeController.java
+++ src/main/java/roomescape/controller/RoomEscapeController.java
@@ -1,13 +1,38 @@
 package roomescape.controller;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import roomescape.domain.Reservation;
 
 @Controller
 public class RoomEscapeController {
+    private final List<Reservation> reservations = createReservations();
 
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String reservations() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    private List<Reservation> createReservations() {
+        return List.of(new Reservation(1, "파도", LocalDate.of(2025, 2, 2), LocalTime.of(9, 10)),
+                new Reservation(2, "달", LocalDate.of(2025, 1, 14), LocalTime.of(10, 10)),
+                new Reservation(2, "별", LocalDate.of(2025, 2, 1), LocalTime.of(10, 10)));
     }
 }
