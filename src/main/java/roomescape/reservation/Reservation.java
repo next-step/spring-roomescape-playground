3d1505018685cdeb003fa86058f1ca263eddb7bb
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-03 15:26:01 +0900

    feat : 2단계 테스트 추가

---
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -0,0 +1,15 @@
+package roomescape.dto;
+
+import java.security.Timestamp;
+import java.time.LocalTime;
+import java.util.Date;
+
+public class Reservation {
+
+    private int id;
+    private int user_name;
+    private Date date;
+    private LocalTime time;
+    private Timestamp created_at;
+
+}
