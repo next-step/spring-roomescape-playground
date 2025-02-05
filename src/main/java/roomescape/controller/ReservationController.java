8102b7e337049d18a916abc8506f2e2133eeea57
Jiihyun <cwlgusc@gmail.com>
2025-02-05 18:31:20 +0900

    feat: 특정 예약 취소 API 생성 - 인메모리 List에서 reservationId를 지닌 객체 인덱스를 찾아 예약 취소 - 예약 생성 더미 데이터 메서드 주석 처리

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -3,10 +3,13 @@ package roomescape.controller;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.request.CreateReservationRequest;
 import roomescape.dto.response.ReservationResponse;
@@ -40,5 +43,11 @@ public class ReservationController {
     @GetMapping()
     public List<ReservationResponse> getReservations() {
         return reservationService.getReservations();
+    }
+
+    @DeleteMapping("/{reservationId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteReservation(@PathVariable Long reservationId) {
+        reservationService.deleteReservation(reservationId);
     }
 }
--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -5,15 +5,14 @@ import roomescape.dto.request.CreateReservationRequest;
 import roomescape.dto.response.ReservationResponse;
 import roomescape.entity.Reservation;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class ReservationService {
 
-    private List<Reservation> reservations = createReservations();
+    private List<Reservation> reservations = new ArrayList<>();
     private AtomicLong index = new AtomicLong(0);
 
     public ReservationResponse createReservation(final CreateReservationRequest request) {
@@ -29,11 +28,22 @@ public class ReservationService {
                 .toList();
     }
 
-    private List<Reservation> createReservations() {
-        return List.of(
-                new Reservation(1L, "브라운", LocalDate.of(2024, 2, 1), LocalTime.of(10, 0, 0)),
-                new Reservation(2L, "브라운", LocalDate.of(2024, 2, 2), LocalTime.of(11, 0, 0)),
-                new Reservation(3L, "브라운", LocalDate.of(2024, 2, 3), LocalTime.of(12, 0, 0))
-        );
+//    private List<Reservation> createReservations() {
+//        return List.of(
+//                new Reservation(1L, "브라운", LocalDate.of(2024, 2, 1), LocalTime.of(10, 0, 0)),
+//                new Reservation(2L, "브라운", LocalDate.of(2024, 2, 2), LocalTime.of(11, 0, 0)),
+//                new Reservation(3L, "브라운", LocalDate.of(2024, 2, 3), LocalTime.of(12, 0, 0))
+//        );
+//    }
+
+    public void deleteReservation(final Long reservationId) {
+        int index = 0;
+        for (Reservation reservation : reservations) {
+            if (reservation.getId().equals(reservationId)) {
+                break;
+            }
+            index++;
+        }
+        reservations.remove(index);
     }
 }
