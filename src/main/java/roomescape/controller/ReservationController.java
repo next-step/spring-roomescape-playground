9daf3ecc216cfcd5f5919a6d3c6569d412ae8443
Jiihyun <cwlgusc@gmail.com>
2025-02-05 16:44:06 +0900

    refactor: 클래스 레벨에 공통 URI 추가

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,6 +2,7 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import roomescape.dto.response.ReservationResponse;
 import roomescape.service.ReservationService;
@@ -9,6 +10,7 @@ import roomescape.service.ReservationService;
 import java.util.List;
 
 @Controller
+@RequestMapping("/reservations")
 public class ReservationController {
 
     private final ReservationService reservationService;
@@ -18,7 +20,7 @@ public class ReservationController {
     }
 
     @ResponseBody
-    @GetMapping(value = "/reservations")
+    @GetMapping()
     public List<ReservationResponse> getReservations() {
         return reservationService.getReservations();
     }
