54f353f6766a1cb10e319fde127ac87bbdebcb70
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-06 13:51:30 +0900

    feat: GlobalExceptionHandler 기능 추가

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,4 +1,18 @@
 package roomescape.exception;
 
+import java.util.NoSuchElementException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundReservationException.class)
+    public ResponseEntity<String> NotFoundReservationException(NotFoundReservationException ex) {
+        return new ResponseEntity<>("Not Found Reservation", HttpStatus.BAD_REQUEST);
+    }
+
+
 }
