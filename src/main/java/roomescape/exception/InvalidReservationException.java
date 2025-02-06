60e87deb2d4ed97bbf3901b44d0b3268b9918567
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-06 13:59:21 +0900

    feat : 유효하지 않은 인자를 입력받을때의 예외 추가

--- src/main/java/roomescape/exception/InvalidReservationException.java
+++ src/main/java/roomescape/exception/InvalidReservationException.java
@@ -1,7 +1,7 @@
 package roomescape.exception;
 
-public class InvalidReservationException extends RuntimeException {
-  public InvalidReservationException(String message) {
-    super(message);
-  }
+public class InvalidReservationException extends IllegalArgumentException {
+    public InvalidReservationException(String message) {
+        super(message);
+    }
 }
