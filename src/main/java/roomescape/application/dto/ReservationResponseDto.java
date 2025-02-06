40a83bd29a994c6c50150aad973b92c121e65d10
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-02-06 18:58:48 +0900

    refactor: date time format annotation add

--- src/main/java/roomescape/application/dto/ReservationResponseDto.java
+++ src/main/java/roomescape/application/dto/ReservationResponseDto.java
@@ -1,14 +1,18 @@
 package roomescape.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import lombok.Builder;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Builder
 public record ReservationResponseDto(
         Long id,
         String name,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
         LocalDate date,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm", timezone = "Asia/Seoul")
         LocalTime time
 ) {
 }
