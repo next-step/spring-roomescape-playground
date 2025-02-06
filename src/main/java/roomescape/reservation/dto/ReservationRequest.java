04a4d0b0aaa165d0718c529e37f122e60c9800b7
heehunjung <heehunjung3658@gmail.com>
2025-02-06 23:14:36 +0900

    feat: requestDto 검증 로직 추가

--- src/main/java/roomescape/reservation/dto/ReservationRequest.java
+++ src/main/java/roomescape/reservation/dto/ReservationRequest.java
@@ -6,6 +6,10 @@ import roomescape.reservation.domain.Reservation;
 
 public record ReservationRequest(String name, LocalDate date, LocalTime time) {
 
+    public boolean isValid() {
+        return name != null && date != null && time != null;
+    }
+
     public static Reservation toReservation(final ReservationRequest reservationRequest, Long id) {
         return new Reservation(id, reservationRequest.name(), reservationRequest.date(), reservationRequest.time());
     }
