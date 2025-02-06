a924f542d5821b1a64ee02dfba30dc6351005307
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-06 14:00:03 +0900

    feat: 삭제처리를 할때 인자가 없는 경우, 런타임 에러를 발생시키는 예외 추가

--- src/main/java/roomescape/exception/NotFoundReservationException.java
+++ src/main/java/roomescape/exception/NotFoundReservationException.java
@@ -1,4 +1,7 @@
 package roomescape.exception;
 
-public class NotFoundException {
+public class NotFoundReservationException extends RuntimeException {
+    public NotFoundReservationException(String message) {
+        super(message);
+    }
 }
