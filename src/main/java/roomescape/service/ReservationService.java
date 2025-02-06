18226d786ba1f25c4961008a4e635d21abcc2128
Jiihyun <cwlgusc@gmail.com>
2025-02-06 22:31:39 +0900

    chore: 필요없는 주석 제거 - 더미데이터 생성하는 메서드 제거

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -29,14 +29,6 @@ public class ReservationService {
                 .toList();
     }
 
-//    private List<Reservation> createReservations() {
-//        return List.of(
-//                new Reservation(1L, "브라운", LocalDate.of(2024, 2, 1), LocalTime.of(10, 0, 0)),
-//                new Reservation(2L, "브라운", LocalDate.of(2024, 2, 2), LocalTime.of(11, 0, 0)),
-//                new Reservation(3L, "브라운", LocalDate.of(2024, 2, 3), LocalTime.of(12, 0, 0))
-//        );
-//    }
-
     public void deleteReservation(final Long reservationId) {
         int index = getIndexOrThrow(reservationId);
         reservations.remove(index);
