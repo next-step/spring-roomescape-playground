f68740f8787bf7eb9b2f7b7e7e4cea9002e51d73
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-06 14:00:34 +0900

    feat : 예외처리 validator 클래스 분리

--- src/main/java/roomescape/validator/ReservationValidator.java
+++ src/main/java/roomescape/validator/ReservationValidator.java
@@ -1,4 +1,28 @@
 package roomescape.validator;
 
+import roomescape.entity.Reservation;
+import roomescape.exception.InvalidReservationException;
+import roomescape.exception.NotFoundReservationException;
+
 public class ReservationValidator {
+
+    public static void validate(Reservation reservation) {
+        if (reservation.getName() == null || reservation.getName().trim().isEmpty()) {
+            throw new InvalidReservationException("Name is required");
+        }
+
+        if (reservation.getDate() == null) {
+            throw new NotFoundReservationException("Date is required");
+        }
+
+        if (reservation.getTime() == null) {
+            throw new NotFoundReservationException("Time is required");
+        }
+    }
+
+    public static void deleteValidate(boolean removed, int id){
+        if (!removed) {
+            throw new NotFoundReservationException("Reservation with ID" + id + "Not Found");
+        }
+    }
 }
