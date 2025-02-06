7aad6c64209d5b4ac9c4e8723093b33b08af5af7
Jiihyun <cwlgusc@gmail.com>
2025-02-07 00:16:59 +0900

    test: 예약 삭제 테스트 추가

---
+++ src/test/java/roomescape/service/ReservationServiceTest.java
@@ -0,0 +1,44 @@
+package roomescape.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import roomescape.dto.request.CreateReservationRequest;
+import roomescape.global.exception.BadRequestException;
+import roomescape.global.exception.ExceptionMessage;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ReservationServiceTest {
+
+    private ReservationService reservationService;
+
+    @BeforeEach
+    void setUp() {
+        reservationService = new ReservationService();
+    }
+
+    @Test
+    void 예약을_삭제할_수_있다() {
+        // given
+        CreateReservationRequest request = new CreateReservationRequest(
+                "김철수",
+                LocalDate.of(2025, 2, 6),
+                LocalTime.of(12, 30));
+        reservationService.createReservation(request);
+        // when & then
+        assertThatCode(() -> reservationService.deleteReservation(1L))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 존재하지_않는_예약을_삭제하는_경우_오류_발생() {
+        assertThatThrownBy(() -> reservationService.deleteReservation(1L))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining(
+                        ExceptionMessage.RESERVATION_NOT_EXISTS.getMessage());
+    }
+}
