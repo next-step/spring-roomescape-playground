ceff785e97d2a36b6e43245f233bd1b009c6f1a5
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-07 16:58:59 +0900

    refactor : merge 부분 수정

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -17,16 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.Reservation;
 import roomescape.exception.NotFoundReservationException;
 import roomescape.validator.ReservationValidator;
-import org.springframework.ui.Model;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import roomescape.reservation.Reservation;
 
 @RestController
 @RequestMapping("/reservations")
