916cdbba78ed58b99f4a280b45b50126eca10ed8
JMJ <ghtntkdnsk@naver.com>
2025-02-08 02:16:08 +0900

    refactoring: 유지보수를 위해 반복되는 경로를 RequestMapping을 이용하여 묶음

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,13 +2,12 @@ package roomescape.controller;
 
 import java.util.List;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
@@ -17,6 +16,7 @@ import roomescape.dto.response.ReservationResponse;
 import roomescape.service.ReservationService;
 
 @RestController
+@RequestMapping("/reservations")
 public class ReservationController {
     private final ReservationService reservationService;
 
@@ -24,18 +24,18 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
-    @GetMapping("/reservations")
+    @GetMapping()
     public List<Reservation> getReservations() {
         return reservationService.showReservations();
     }
 
-    @PostMapping("/reservations")
+    @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public ReservationResponse createReservation(@RequestBody ReservationCreateRequest request) {
         return reservationService.reserve(request);
     }
 
-    @DeleteMapping("/reservations/{userId}")
+    @DeleteMapping("/{userId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void cancelReservation(@PathVariable Long userId) {
         reservationService.cancelReservation(userId);
