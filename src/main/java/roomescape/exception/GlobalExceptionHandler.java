5350a7174944ee8a2d5266bbad4731ccbee254da
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-07 16:45:39 +0900

    refactor : NotFoundReservationException 제거 및 http 코드 수정

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package roomescape.exception;
 
-import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,9 +8,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(NotFoundReservationException.class)
-    public ResponseEntity<String> NotFoundReservationException(NotFoundReservationException ex) {
-        return new ResponseEntity<>("Not Found Reservation", HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(InvalidException.class)
+    public ResponseEntity<String> invalidException(InvalidException ex) {
+        return new ResponseEntity<>("Invalid value entered", HttpStatus.BAD_REQUEST);
     }
 
 
