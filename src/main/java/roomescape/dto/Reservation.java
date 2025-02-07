4f29fd30e5383597de6761898d5557b95153f5c2
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-07 16:44:17 +0900

    refactor : Entity -> DTO 이름 변경

--- src/main/java/roomescape/dto/Reservation.java
+++ src/main/java/roomescape/dto/Reservation.java
@@ -1,5 +1,6 @@
-package roomescape.entity;
+package roomescape.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -8,6 +9,7 @@ public class Reservation {
     private final long id;
     private final String name;
     private final LocalDate date;
+    @JsonFormat(pattern = "HH:mm")
     private final LocalTime time;
 
     public Reservation(long id, String name, LocalDate date, LocalTime time) {
