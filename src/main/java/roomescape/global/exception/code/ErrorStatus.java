ff3c4e7f3fbf27a6af39057af42e29df5ae7867c
heehunjung <heehunjung3658@gmail.com>
2025-02-08 16:37:02 +0900

    refactor: 예외 코드 변경

--- src/main/java/roomescape/global/exception/code/ErrorStatus.java
+++ src/main/java/roomescape/global/exception/code/ErrorStatus.java
@@ -6,7 +6,7 @@ public enum ErrorStatus {
 
     INVALID_REQUEST_RESERVATION_INFO(HttpStatus.BAD_REQUEST, "잘못된 예약 정보입니다"),
     INVALID_REQUEST_RESERVATION_ID(HttpStatus.BAD_REQUEST, "예약 아이디를 잘못 입력하였습니다. (0보다 큰 양수)"),
-    RESERVATION_NOT_FOUND(HttpStatus.BAD_REQUEST, "예약 정보를 찾지못했습니다. %s");
+    RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "예약 정보를 찾지못했습니다. %s");
 
     private final HttpStatus httpStatus;
     private final String message;
