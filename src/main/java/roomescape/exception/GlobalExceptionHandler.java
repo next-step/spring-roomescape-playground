cad6cb9fba75352461387751a6bafb57394c0674
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-08 17:53:26 +0900

    refactor : GlobalExceptionHandler @RestControllerAdvice 어노테이션 추가

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -2,15 +2,22 @@ package roomescape.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    //404 Not Found 처리
     @ExceptionHandler(InvalidException.class)
     public ResponseEntity<String> invalidException(InvalidException ex) {
         return new ResponseEntity<>("Invalid value entered", HttpStatus.BAD_REQUEST);
+    }
+
+    //
+    @ExceptionHandler(NotFoundReservationException.class)
+    public ResponseEntity<String> notFoundReservation(NotFoundReservationException ex){
+        return new ResponseEntity<>("The reservation isn't exist", HttpStatus.NOT_FOUND);
     }
 
 
