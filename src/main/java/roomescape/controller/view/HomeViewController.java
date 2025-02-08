a28e8978fd4fd12e0435a750a84e5ed62e282445
Jiihyun <cwlgusc@gmail.com>
2025-02-08 18:07:55 +0900

    refactor: 페이지 컨트롤러 도메인 분리

--- src/main/java/roomescape/controller/view/HomeViewController.java
+++ src/main/java/roomescape/controller/view/HomeViewController.java
@@ -1,18 +1,13 @@
-package roomescape.controller;
+package roomescape.controller.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller()
-public class PageController {
+public class HomeViewController {
 
     @GetMapping("/")
     public String readHomePage() {
         return "home";
-    }
-
-    @GetMapping("/reservation")
-    public String readReservationPage() {
-        return "reservation";
     }
 }
