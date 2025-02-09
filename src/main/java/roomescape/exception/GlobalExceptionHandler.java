39b0ecc858e04f48abd608d66aae4fb702c6f706
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-09 12:39:46 +0900

    refactor : 예외처리 Http 응답번호 수정

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    
     @ExceptionHandler(InvalidException.class)
     public ResponseEntity<Map<String, String>> invalidException(InvalidException ex) {
         Map<String, String> response = new HashMap<>();
@@ -22,10 +21,9 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundReservationException.class)
     public ResponseEntity<Map<String, String>> notFoundReservation(NotFoundReservationException ex) {
         Map<String, String> response = new HashMap<>();
-        response.put("error","Not Found");
+        response.put("error", "Bad Request");
         response.put("message", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
-
 
 }
