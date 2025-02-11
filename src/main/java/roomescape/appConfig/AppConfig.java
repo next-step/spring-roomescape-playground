245fb1f13a584c0fd5cda5d0b65b14d5f3a4c0bb
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-11 22:17:50 +0900

    feat : JDBC 빈 등록

--- src/main/java/roomescape/appConfig/AppConfig.java
+++ src/main/java/roomescape/appConfig/AppConfig.java
@@ -1,4 +1,26 @@
 package roomescape.appConfig;
 
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import roomescape.dao.ReservationDAO;
+import roomescape.dao.ReservationImplDAO;
+import roomescape.entity.Reservation;
+
+@Configuration
 public class AppConfig {
+
+    private final DataSource dataSource;
+
+    public AppConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource);
+    }
+
 }
