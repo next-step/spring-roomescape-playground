3dcc0f629dcb8fd551c670ae80116cfb26fe28a0
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-11 22:17:31 +0900

    feat :DAO 추가

--- src/main/java/roomescape/dao/ReservationDAO.java
+++ src/main/java/roomescape/dao/ReservationDAO.java
@@ -1,4 +1,17 @@
 package roomescape.dao;
 
+import java.util.List;
+import roomescape.entity.Reservation;
+
 public interface ReservationDAO {
+
+    void save(Reservation reservation);
+
+    List<Reservation> findAll();
+
+    void update(Reservation reservation);
+
+    void delete(long id);
+
+    int count();
 }
--- src/main/java/roomescape/dao/ReservationImplDAO.java
+++ src/main/java/roomescape/dao/ReservationImplDAO.java
@@ -1,4 +1,51 @@
 package roomescape.dao;
 
-public class ReservationImplDAO {
+import java.util.List;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import roomescape.entity.Reservation;
+
+public class ReservationImplDAO implements ReservationDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationImplDAO(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public void save(Reservation reservation) {
+        String sql = "INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, reservation.getName(), reservation.getDate(), reservation.getTime());
+    }
+
+    @Override
+    public List<Reservation> findAll() {
+        String sql = "SELECT id, name, date, time FROM reservation";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Reservation.class));
+    }
+
+    @Override
+    public void update(Reservation reservation) {
+        String sql = "UPDATE reservation SET name = ?, date = ?, time = ? WHERE id = ?";
+        jdbcTemplate.update(sql, reservation.getName(), reservation.getDate(), reservation.getTime(),
+                reservation.getId());
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+
+    @Override
+    public int count() {
+        String sql = "SELECT count(*) FROM reservation";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
+
+        if (count == null) {
+            return 0;
+        }
+
+        return count;
+    }
 }
