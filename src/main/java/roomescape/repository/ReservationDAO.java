2e6ec9ca30927b665f1af31f915eb77f56c4f0f7
JMJ <ghtntkdnsk@naver.com>
2025-02-12 15:29:55 +0900

    fix: 생성자 주입 방식에 맞게 JdbcTemplate을 불변 선언으로 변경

--- src/main/java/roomescape/repository/ReservationDAO.java
+++ src/main/java/roomescape/repository/ReservationDAO.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class ReservationDAO {
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     public ReservationDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
