f8914e1ee91c7c9575fcb551dc6aa16c7808e94d
heehunjung <heehunjung3658@gmail.com>
2025-02-13 05:04:17 +0900

    chore: 필요없는 import 삭제

--- src/test/java/roomescape/domain/reservation/repository/ReservationRepositoryTest.java
+++ src/test/java/roomescape/domain/reservation/repository/ReservationRepositoryTest.java
@@ -1,11 +1,8 @@
 package roomescape.domain.reservation.repository;
 
-import groovy.util.logging.Slf4j;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
