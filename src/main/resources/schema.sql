c625b9de07961ab06b943a148d44778085f3dbc8
heehunjung <heehunjung3658@gmail.com>
2025-02-12 19:25:47 +0900

    feat: if not exist 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,4 @@
-CREATE TABLE reservation
-(
+CREATE TABLE IF NOT EXISTS reservation (
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
     date    VARCHAR(255) NOT NULL,
