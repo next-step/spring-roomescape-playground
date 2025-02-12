525f902b4b069d0dbc199577988a24d636d812a5
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-02-12 16:55:03 +0900

    refactor: 사용하지 않는 임포트 삭제

--- src/main/java/roomescape/repository/reservation/ReservationDatabaseRepository.java
+++ src/main/java/roomescape/repository/reservation/ReservationDatabaseRepository.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
