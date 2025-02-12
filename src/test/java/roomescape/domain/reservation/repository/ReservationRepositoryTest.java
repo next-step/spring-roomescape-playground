4782f44d4680f8e412ac0b26443850ca593e0edb
heehunjung <heehunjung3658@gmail.com>
2025-02-13 05:02:34 +0900

    refactor: ReservationRepositoryTest-> 단위테스트로 변경

--- src/test/java/roomescape/domain/reservation/repository/ReservationRepositoryTest.java
+++ src/test/java/roomescape/domain/reservation/repository/ReservationRepositoryTest.java
@@ -1,32 +1,37 @@
 package roomescape.domain.reservation.repository;
 
+import groovy.util.logging.Slf4j;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import roomescape.domain.reservation.domain.Reservation;
 
-@SpringBootTest
+@JdbcTest
 class ReservationRepositoryTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    private ReservationRepository reservationRepository;
-
     @Test
     void getReservations_테스트() {
-        List<Reservation> reservations = reservationRepository.getReservations();
-        int beforeCount = reservations.size();
+        ReservationRepository reservationRepository = new ReservationRepository(jdbcTemplate);
 
         jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05",
-                "15:40");
-        reservations = reservationRepository.getReservations();
-        int afterCount = reservations.size();
+                "15:41");
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "커찬", "2024-08-05",
+                "15:42");
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "망고", "2025-08-05",
+                "15:43");
+        List<Reservation> reservations = reservationRepository.getReservations();
 
-        Assertions.assertThat(beforeCount + 1).isEqualTo(afterCount);
+        Assertions.assertThat(3).isEqualTo(reservations.size());
+        Assertions.assertThat(reservations)
+                .extracting(Reservation::getName)
+                .containsExactly("브라운", "커찬", "망고");
     }
 }
