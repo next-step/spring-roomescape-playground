da7b2fe07979303a07feac527aa81670be5028ef
Jiihyun <cwlgusc@gmail.com>
2025-02-13 19:38:03 +0900

    refactor: 불필요한 어노테이션 제거

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -1,11 +1,9 @@
 package roomescape.repository;
 
-import org.springframework.stereotype.Repository;
 import roomescape.entity.Reservation;
 
 import java.util.List;
 
-@Repository
 public interface ReservationRepository {
 
     List<Reservation> findAll();
