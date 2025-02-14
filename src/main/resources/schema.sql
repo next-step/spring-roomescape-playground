d24d016d9133b9b83b096836a1c7ba7f39f60cdf
Jiihyun <cwlgusc@gmail.com>
2025-02-15 01:09:06 +0900

    refactor: Reservation테이블 예약 시간 및 날짜 타입 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS reservation
 (
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     customer_name    VARCHAR(255) NOT NULL,
-    reservation_date    VARCHAR(255) NOT NULL,
-    reservation_time    VARCHAR(255) NOT NULL,
+    reservation_date    DATE NOT NULL,
+    reservation_time    TIME NOT NULL,
     PRIMARY KEY (id)
 );
