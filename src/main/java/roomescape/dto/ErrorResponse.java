dead844b18612d78fec4f3cabcf188fbd8501346
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-16 00:20:50 +0900

    refactor : 내부 객체 이름 수정

--- src/main/java/roomescape/dto/ErrorResponse.java
+++ src/main/java/roomescape/dto/ErrorResponse.java
@@ -3,15 +3,19 @@ package roomescape.dto;
 public class ErrorResponse {
 
     private final String exceptionType;
+    
     private final String message;
 
     public ErrorResponse(String exceptionType, String message) {
+
         this.exceptionType = exceptionType;
         this.message = message;
     }
 
     public String getException() {
+
         return exceptionType;
+
     }
 
     public String getMessage() {
