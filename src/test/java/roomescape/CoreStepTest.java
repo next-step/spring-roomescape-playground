5e028f6fa9dd25e9f2e0e3240c83fdb549b81808
JoungMinJu <alswn9938@gmail.com>
2025-02-17 19:52:18 +0900

    step10

--- src/test/java/roomescape/CoreStepTest.java
+++ src/test/java/roomescape/CoreStepTest.java
@@ -1,18 +1,26 @@
 package roomescape;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.controller.ReservationController;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class CoreStepTest {
+
+    @Autowired
+    private ReservationController reservationController;
 
     @Test
     void 팔단계() {
@@ -53,5 +61,20 @@ public class CoreStepTest {
             .then().log().all()
             .statusCode(400);
     }
+
+    @Test
+    void 십단계() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
+    }
+
 
 }
