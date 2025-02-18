1af220a8cab4f346c1a4ea9d93dfd58a77170197
Jiihyun <cwlgusc@gmail.com>
2025-02-18 16:38:20 +0900

    test: 예약 삭제 테스트 주석 제거

--- src/test/java/roomescape/service/ReservationServiceTest.java
+++ src/test/java/roomescape/service/ReservationServiceTest.java
@@ -5,11 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.dto.request.CreateReservationRequest;
 import roomescape.dto.response.ReservationResponse;
+import roomescape.global.exception.BadRequestException;
+import roomescape.global.exception.ExceptionMessage;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -24,31 +31,31 @@ class ReservationServiceTest {
     @Test
     void 총_예약건수를_조회할_수_있다() {
         // given
-        jdbcTemplate.update("INSERT INTO reservation (customer_name, reservation_date, reservation_time) VALUES (?, ?, ?)", "예약생성못해쿼리투입", "2025-02-12", "15:00");
+        jdbcTemplate.update("INSERT INTO reservation (customer_name, date, time) VALUES (?, ?, ?)", "예약생성못해쿼리투입", "2025-02-12", "15:00");
         // when
         List<ReservationResponse> reservations = reservationService.getReservations();
         // then
         assertThat(reservations.size() == 1).isTrue();
     }
 
-//    @Test
-//    void 예약을_삭제할_수_있다() {
-//        // given
-//        CreateReservationRequest request = new CreateReservationRequest(
-//                "김철수",
-//                LocalDate.of(2025, 2, 6),
-//                LocalTime.of(12, 30));
-//        reservationService.createReservation(request);
-//        // when & then
-//        assertThatCode(() -> reservationService.deleteReservation(1))
-//                .doesNotThrowAnyException();
-//    }
-//
-//    @Test
-//    void 존재하지_않는_예약을_삭제하는_경우_오류_발생() {
-//        assertThatThrownBy(() -> reservationService.deleteReservation(1))
-//                .isInstanceOf(BadRequestException.class)
-//                .hasMessageContaining(
-//                        ExceptionMessage.RESERVATION_NOT_EXISTS.getMessage());
-//    }
+    @Test
+    void 예약을_삭제할_수_있다() {
+        // given
+        CreateReservationRequest request = new CreateReservationRequest(
+                "김철수",
+                LocalDate.of(2025, 2, 6),
+                LocalTime.of(12, 30));
+        ReservationResponse reservation = reservationService.createReservation(request);
+        // when & then
+        assertThatCode(() -> reservationService.deleteReservation(reservation.id()))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 존재하지_않는_예약을_삭제하는_경우_오류_발생() {
+        assertThatThrownBy(() -> reservationService.deleteReservation(1))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining(
+                        ExceptionMessage.RESERVATION_NOT_EXISTS.getMessage());
+    }
 }
