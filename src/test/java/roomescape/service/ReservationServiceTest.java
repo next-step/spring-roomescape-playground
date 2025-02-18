32e477b5af7fa3c9a22373b0470b261bd99e2921
Jiihyun <cwlgusc@gmail.com>
2025-02-18 22:09:21 +0900

    test: 예약 생성 성공, 동시간대 예약 실패 테스트 추가

--- src/test/java/roomescape/service/ReservationServiceTest.java
+++ src/test/java/roomescape/service/ReservationServiceTest.java
@@ -1,5 +1,6 @@
 package roomescape.service;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +18,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -28,34 +30,75 @@ class ReservationServiceTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @AfterEach
+    void tearDown() {
+        jdbcTemplate.update("TRUNCATE TABLE RESERVATION");
+    }
+
+    @Test
+    void 예약을_생성할_수_있다() {
+        // given
+        CreateReservationRequest request = createReservationRequest("김철수", 2025, 2, 18, 13, 0);
+        // when
+        ReservationResponse response = reservationService.createReservation(request);
+        // then
+        assertAll(
+                () -> assertThat(response.name()).isEqualTo(request.name()),
+                () -> assertThat(response.date()).isEqualTo(request.date()),
+                () -> assertThat(response.time()).isEqualTo(request.time())
+        );
+    }
+
+    @Test
+    void 동시간대에_예약이_존재한다면_예외가_발생한다() {
+        // given
+        CreateReservationRequest request1 = createReservationRequest("김철수", 2025, 2, 18, 13, 0);
+        reservationService.createReservation(request1);
+        CreateReservationRequest request2 = createReservationRequest("김영희", 2025, 2, 18, 13, 0);
+        // when & then
+        assertThatThrownBy(() -> reservationService.createReservation(request2))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessage(
+                        ExceptionMessage.RESERVATION_ALREADY_EXISTS.getMessage());
+    }
+
     @Test
     void 총_예약건수를_조회할_수_있다() {
         // given
-        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "예약생성못해쿼리투입", "2025-02-12", "15:00");
+        CreateReservationRequest request1 = createReservationRequest("김철수", 2025, 2, 18, 13, 0);
+        CreateReservationRequest request2 = createReservationRequest("김영희", 2025, 2, 18, 14, 0);
+        reservationService.createReservation(request1);
+        reservationService.createReservation(request2);
         // when
-        List<ReservationResponse> reservations = reservationService.getReservations();
+        List<ReservationResponse> reservationResponses = reservationService.getReservations();
         // then
-        assertThat(reservations.size() == 1).isTrue();
+        assertThat(reservationResponses.size() == 2).isTrue();
     }
 
     @Test
     void 예약을_삭제할_수_있다() {
         // given
-        CreateReservationRequest request = new CreateReservationRequest(
-                "김철수",
-                LocalDate.of(2025, 2, 6),
-                LocalTime.of(12, 30));
-        ReservationResponse reservation = reservationService.createReservation(request);
+        CreateReservationRequest request = createReservationRequest("김철수", 2025, 2, 18, 13, 0);
+        ReservationResponse response = reservationService.createReservation(request);
         // when & then
-        assertThatCode(() -> reservationService.deleteReservation(reservation.id()))
+        assertThatCode(() -> reservationService.deleteReservation(response.id()))
                 .doesNotThrowAnyException();
     }
 
     @Test
-    void 존재하지_않는_예약을_삭제하는_경우_오류_발생() {
+    void 존재하지_않는_예약을_삭제하는_경우_예외가_발생한다() {
         assertThatThrownBy(() -> reservationService.deleteReservation(1))
                 .isInstanceOf(BadRequestException.class)
-                .hasMessageContaining(
+                .hasMessage(
                         ExceptionMessage.RESERVATION_NOT_EXISTS.getMessage());
+    }
+
+    private CreateReservationRequest createReservationRequest(String name,
+                                                              int year, int month, int dayOfMonth,
+                                                              int hour, int minute) {
+        return new CreateReservationRequest(
+                name,
+                LocalDate.of(year, month, dayOfMonth),
+                LocalTime.of(hour, minute));
     }
 }
