e2d81627fa49e5bed5d7713c6421873966c8056d
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-20 08:40:40 +0900

    refactor : 클래스 첫 줄 개행 수정

--- src/main/java/roomescape/dao/ReservationRowMapper.java
+++ src/main/java/roomescape/dao/ReservationRowMapper.java
@@ -6,13 +6,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ReservationRowMapper implements RowMapper<Reservation> {
+
     @Override
     public Reservation mapRow(ResultSet rs, int rowNum) throws SQLException {
+
         return new Reservation(
                 rs.getInt("id"),
                 rs.getString("name"),
                 rs.getDate("date").toLocalDate(),
                 rs.getTime("time").toLocalTime()
         );
+
     }
 }
