b4740dacc80a40f64be989d3ceab7611478ae357
Jiihyun <cwlgusc@gmail.com>
2025-02-19 13:54:07 +0900

    test: reservation 테스트 추가 - 시간 정각 단위 예외 테스트 - 과거 시간 예약 예외 테스트

--- src/test/java/roomescape/domain/ReservationTest.java
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -7,49 +7,80 @@ import org.junit.jupiter.params.provider.ValueSource;
 import roomescape.global.exception.BadRequestException;
 import roomescape.global.exception.ExceptionMessage;
 
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZoneId;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ReservationTest {
 
+    private static final Clock FIXED_CLOCK = Clock.fixed(Instant.parse("2025-02-17T10:00:00Z"), ZoneId.of("UTC"));
+
     @Test
     void 예약을_생성할_수_있다() {
-        assertThatCode(() -> new Reservation("김철수", LocalDate.now(), LocalTime.now()))
+        assertThatCode(() -> new Reservation("김철수", LocalDate.of(2025, 2, 19), LocalTime.of(13, 0)))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 현재보다_과거_시간대의_예약을_생성하면_true를_반환한다() {
+        // given
+        Reservation reservation = new Reservation("김철수", LocalDate.of(2025, 2, 17), LocalTime.of(9, 0));
+        // when &  then
+        assertThat(reservation.isExpired(FIXED_CLOCK)).isTrue();
+    }
+
+    @Test
+    void 현재_이후_시간대의_예약을_생성하면_false를_반환한다() {
+        // given
+        Reservation reservation = new Reservation("김철수", LocalDate.of(2025, 2, 17), LocalTime.of(11, 0));
+        // when &  then
+        assertThat(reservation.isExpired(FIXED_CLOCK)).isFalse();
     }
 
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = {" ", "김", "아야어여오요우유으이이"})
     void 이름_길이가_2보다_작거나_19보다_크면_오류_발생(String name) {
-        assertThatThrownBy(() -> new Reservation(name, LocalDate.now(), LocalTime.MIN))
+        assertThatThrownBy(() -> new Reservation(name, LocalDate.of(2025, 2, 19), LocalTime.of(13, 0)))
                 .isInstanceOf(BadRequestException.class)
-                .hasMessageContaining(
+                .hasMessage(
                         ExceptionMessage.INVALID_NAME.getMessage());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"kim", "나자바!", "철수 Kim"})
     void 이름에_한글_외_다른_문자_넣을시_오류_발생(String name) {
-        assertThatThrownBy(() -> new Reservation(name, LocalDate.now(), LocalTime.MIN))
+        assertThatThrownBy(() -> new Reservation(name, LocalDate.of(2025, 2, 19), LocalTime.of(13, 0)))
                 .isInstanceOf(BadRequestException.class)
-                .hasMessageContaining(
+                .hasMessage(
                         ExceptionMessage.INVALID_NAME.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 30, 55})
+    void 시간이_정각_단위가_아니면_예외가_발생한다(int time) {
+        assertThatThrownBy(() -> new Reservation("김철수", LocalDate.of(2025, 2, 19), LocalTime.of(13, time)))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessage(
+                        ExceptionMessage.INVALID_TIME.getMessage());
     }
 
     @Test
     void 필수_인자_입력하지_않을시_오류_발생() {
         assertAll(
-                () -> assertThatThrownBy(() -> new Reservation("김철수", null, LocalTime.MIN))
+                () -> assertThatThrownBy(() -> new Reservation("김철수", null, LocalTime.of(13, 0)))
                         .isInstanceOf(BadRequestException.class)
-                        .hasMessageContaining(ExceptionMessage.INVALID_DATE.getMessage()),
+                        .hasMessage(ExceptionMessage.INVALID_DATE.getMessage()),
 
-                () -> assertThatThrownBy(() -> new Reservation("김철수", LocalDate.now(), null))
+                () -> assertThatThrownBy(() -> new Reservation("김철수", LocalDate.of(2025, 2, 19), null))
                         .isInstanceOf(BadRequestException.class)
-                        .hasMessageContaining(ExceptionMessage.INVALID_TIME.getMessage()));
+                        .hasMessage(ExceptionMessage.INVALID_TIME.getMessage()));
     }
 }
