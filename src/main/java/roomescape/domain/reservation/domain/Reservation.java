af38ffa1b30a3f7d7afad3ee4970de7b5de152cd
heehun <slrkqk2@naver.com>
2025-02-20 16:24:14 +0900

    refactor: 공통된 기준으로 final 키워드 추가

--- src/main/java/roomescape/domain/reservation/domain/Reservation.java
+++ src/main/java/roomescape/domain/reservation/domain/Reservation.java
@@ -7,11 +7,11 @@ public class Reservation {
 
     private final Long id;
 
-    private String name;
+    private final String name;
 
-    private LocalDate date;
+    private final LocalDate date;
 
-    private LocalTime time;
+    private final LocalTime time;
 
     public Reservation(final Long id, final String name, final LocalDate date, final LocalTime time) {
         this.id = id;
