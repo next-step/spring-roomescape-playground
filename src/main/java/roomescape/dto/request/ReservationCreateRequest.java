6daef6fec29163cbd8b5fe864050b0a0e21ae2f8
JMJ <ghtntkdnsk@naver.com>
2025-02-23 13:15:34 +0900

    refactor: DTO를 역할에 더 적합한 record로 변경

--- src/main/java/roomescape/dto/request/ReservationCreateRequest.java
+++ src/main/java/roomescape/dto/request/ReservationCreateRequest.java
@@ -3,20 +3,6 @@ package roomescape.dto.request;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class ReservationCreateRequest {
-    private String name;
-    private LocalDate date;
-    private LocalTime time;
+public record ReservationCreateRequest(String name, LocalDate date, LocalTime time) {
 
-    public String getName() {
-        return name;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
 }
