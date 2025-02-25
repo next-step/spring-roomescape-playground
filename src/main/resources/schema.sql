1d7c767239487ba4c11cdf0d2055d1c7364aa52e
JMJ <ghtntkdnsk@naver.com>
2025-02-26 01:40:15 +0900

    fix: 외래키 지정을 통해 reservation 테이블과 time 테이블의 관계를 재설정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,8 +3,9 @@ CREATE TABLE reservation
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
     date    DATE NOT NULL,
-    time    TIME NOT NULL,
-    PRIMARY KEY (id)
+    time_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES time(id)
 );
 
 CREATE TABLE time
