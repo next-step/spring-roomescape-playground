6806bb2277824ad4417b72139bf1bcf1a69283a6
Jiihyun <cwlgusc@gmail.com>
2025-02-26 15:13:00 +0900

    fix: sql 테이블 생성 순서 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,16 @@
-CREATE TABLE IF NOT EXISTS reservation
-(
-    id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(30) NOT NULL,
-    `date`    DATE NOT NULL,
-    `time`    TIME NOT NULL,
-    PRIMARY KEY (id)
-);
-
 CREATE TABLE IF NOT EXISTS time
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     time VARCHAR(10) NOT NULL,
     PRIMARY KEY (id)
+    );
+
+CREATE TABLE IF NOT EXISTS reservation
+(
+    id      BIGINT       NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(30) NOT NULL,
+    `date`    DATE NOT NULL,
+    time_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES time(id)
 );
