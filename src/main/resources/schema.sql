a0ad718cf61522bb9257275341e74089d6d837a1
heehunjung <heehunjung3658@gmail.com>
2025-02-26 22:20:52 +0900

    refactor: 요구사항에 따른 스키마 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE time
+CREATE TABLE reservationTime
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     time TIME NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE reservation
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     date DATE NOT NULL,
-    time_id BIGINT,                           // TODO: 수정
+    time_id BIGINT,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES time(id) // TODO: 추가
+    FOREIGN KEY (time_id) REFERENCES reservationTime(id)
 );
