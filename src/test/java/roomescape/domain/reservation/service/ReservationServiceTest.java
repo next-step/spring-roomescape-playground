f52537c0f9496a6d113a35c2b749ba1e5b7f6bf4
heehunjung <heehunjung3658@gmail.com>
2025-02-27 15:40:10 +0900

    test: service test 작성

---
+++ src/test/java/roomescape/domain/reservation/service/ReservationServiceTest.java
@@ -0,0 +1,129 @@
+package roomescape.domain.reservation.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import roomescape.domain.reservation.dto.ReservationRequest;
+import roomescape.domain.reservation.dto.ReservationResponse;
+import roomescape.domain.reservation.repository.ReservationRepository;
+import roomescape.domain.reservationTime.repository.ReservationTimeRepository;
+import roomescape.global.exception.RoomescapeBadRequestException;
+import roomescape.global.exception.RoomescapeServerException;
+
+@JdbcTest
+class ReservationServiceTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private DataSource dataSource;
+
+    private ReservationRepository reservationRepository;
+    private ReservationTimeRepository reservationTimeRepository;
+    private ReservationService reservationService;
+
+    @BeforeEach
+    void setUp() {
+        reservationRepository = new ReservationRepository(jdbcTemplate, dataSource);
+        reservationTimeRepository = new ReservationTimeRepository(jdbcTemplate, dataSource);
+        reservationService = new ReservationService(reservationRepository, reservationTimeRepository);
+    }
+
+    @Test
+    void getReservationResponses_테스트() {
+        jdbcTemplate.update(
+                "INSERT INTO reservationTime (id, time) VALUES (?, ?), (?, ?), (?, ?)",
+                0L, LocalTime.of(11, 0),
+                1L, LocalTime.of(12, 0),
+                2L, LocalTime.of(13, 0)
+        );
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)", "브라운", "2023-08-05", 0L);
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)", "커찬", "2024-08-05", 1L);
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)", "망고", "2025-08-05", 2L);
+
+        List<ReservationResponse> result = reservationService.getReservationResponses();
+
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result).extracting(ReservationResponse::name)
+                .contains("브라운", "커찬", "망고");
+    }
+
+    @Test
+    void createReservation_테스트() {
+        jdbcTemplate.update("INSERT INTO reservationTime (id, time) VALUES (?, ?)", 0L, LocalTime.of(11, 0));
+        ReservationRequest request = new ReservationRequest("망고", LocalDate.of(2025, 10, 11), 0L);
+
+        ReservationResponse result = reservationService.createReservation(request);
+        Map<String, Object> stringObjectMap = jdbcTemplate.queryForMap("SELECT * FROM reservation WHERE id = ?",
+                result.id());
+        String name = (String) stringObjectMap.get("name");
+        LocalDate date = ((java.sql.Date) stringObjectMap.get("date")).toLocalDate();
+        Long timeId = (Long) stringObjectMap.get("time_id");
+
+        assertThat(name).isEqualTo(request.name());
+        assertThat(date).isEqualTo(request.date());
+        assertThat(timeId).isEqualTo(request.time());
+    }
+
+    @DisplayName("과거 시간에 예약하면 오류가 발생")
+    @Test
+    void beforeDateError_테스트() {
+        jdbcTemplate.update("INSERT INTO reservationTime (id, time) VALUES (?, ?)", 0L, LocalTime.of(11, 0));
+        ReservationRequest request = new ReservationRequest("망고", LocalDate.of(2024, 10, 11), 0L);
+
+        assertThatThrownBy(() -> reservationService.createReservation(request))
+                .isInstanceOf(RoomescapeBadRequestException.class);
+    }
+
+    @DisplayName("당일 예약인 경우 현재 시각보다 빠른 시간에 예약하면 오류가 발생")
+    @Test
+    void beforeTimeError_테스트() {
+        jdbcTemplate.update("INSERT INTO reservationTime (id, time) VALUES (?, ?)", 0L,
+                LocalTime.now().minusMinutes(10));
+        ReservationRequest request = new ReservationRequest("망고", LocalDate.now(), 0L);
+
+        assertThatThrownBy(() -> reservationService.createReservation(request))
+                .isInstanceOf(RoomescapeBadRequestException.class);
+    }
+
+    @Test
+    void deleteReservation_테스트() {
+        Long id = deleteTestSetup();
+
+        reservationService.deleteReservation(id);
+        Integer afterSize = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+
+        assertThat(afterSize).isEqualTo(0);
+    }
+
+    @DisplayName("잘못된 예약 아이디를 입력하면 예외")
+    @Test
+    void deletedExceptionById_테스트() {
+        Long id = deleteTestSetup();
+
+        assertThatCode(() -> reservationService.deleteReservation(id))
+                .doesNotThrowAnyException();
+        assertThatThrownBy(() -> reservationService.deleteReservation(-1L))
+                .isInstanceOf(RoomescapeBadRequestException.class);
+        assertThatThrownBy(() -> reservationService.deleteReservation(1L))
+                .isInstanceOf(RoomescapeServerException.class);
+    }
+
+    private Long deleteTestSetup() {
+        jdbcTemplate.update("INSERT INTO reservationTime (id, time) VALUES (?, ?)", 0L, LocalTime.of(11, 0));
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)", "브라운", "2023-08-05", 0L);
+
+        return jdbcTemplate.queryForObject("SELECT id FROM reservation WHERE name = ?", Long.class, "브라운");
+    }
+}
---
+++ src/test/java/roomescape/domain/reservationTime/service/ReservationTimeServiceTest.java
@@ -0,0 +1,100 @@
+package roomescape.domain.reservationTime.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalTime;
+import java.util.List;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import roomescape.domain.reservationTime.dto.ReservationTimeRequest;
+import roomescape.domain.reservationTime.dto.ReservationTimeResponse;
+import roomescape.domain.reservationTime.repository.ReservationTimeRepository;
+import roomescape.global.exception.RoomescapeBadRequestException;
+import roomescape.global.exception.RoomescapeServerException;
+
+@JdbcTest
+class ReservationTimeServiceTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private DataSource dataSource;
+
+    private ReservationTimeRepository reservationTimeRepository;
+    private ReservationTimeService reservationTimeService;
+
+    @BeforeEach
+    void setUp() {
+        reservationTimeRepository = new ReservationTimeRepository(jdbcTemplate, dataSource);
+        reservationTimeService = new ReservationTimeService(reservationTimeRepository);
+    }
+
+    @Test
+    void createReservationTime_테스트() {
+        ReservationTimeRequest request = new ReservationTimeRequest(LocalTime.of(11, 00));
+
+        ReservationTimeResponse reservationTime = reservationTimeService.createReservationTime(request);
+
+        assertThat(reservationTime.time()).isEqualTo(LocalTime.of(11, 00));
+        assertThat(reservationTime.id()).isNotNull();
+    }
+
+    @DisplayName("조회 결과를 오름차순으로 보여준다.")
+    @Test
+    void getReservationTimes_테스트() {
+        LocalTime time = LocalTime.of(11, 00);
+        LocalTime time2 = LocalTime.of(12, 00);
+        LocalTime time3 = LocalTime.of(13, 00);
+        jdbcTemplate.update(
+                "INSERT INTO reservationTime (id, time) VALUES (?, ?), (?, ?), (?, ?)",
+                0L, time,
+                1L, time2,
+                2L, time3
+        );
+
+        List<ReservationTimeResponse> reservationTimes = reservationTimeService.getReservationTimes();
+
+        assertThat(reservationTimes.size()).isEqualTo(3);
+        assertThat(reservationTimes)
+                .extracting(ReservationTimeResponse::time)
+                .containsExactly(time, time2, time3);
+    }
+
+    @Test
+    void deleteReservationTime_테스트() {
+        Long id = deleteTestSetup();
+
+        reservationTimeService.deleteReservationTime(id);
+        Integer afterSize = jdbcTemplate.queryForObject("SELECT count(1) from reservationTime", Integer.class);
+
+        assertThat(afterSize).isEqualTo(0);
+    }
+
+    @DisplayName("잘못된 예약 아이디를 입력하면 예외")
+    @Test
+    void deletedExceptionById_테스트() {
+        Long id = deleteTestSetup();
+
+        assertThatCode(() -> reservationTimeService.deleteReservationTime(id))
+                .doesNotThrowAnyException();
+        assertThatThrownBy(() -> reservationTimeService.deleteReservationTime(-1L))
+                .isInstanceOf(RoomescapeBadRequestException.class);
+        assertThatThrownBy(() -> reservationTimeService.deleteReservationTime(1L))
+                .isInstanceOf(RoomescapeServerException.class);
+    }
+
+    private Long deleteTestSetup() {
+        LocalTime time = LocalTime.of(11, 00);
+        jdbcTemplate.update("INSERT INTO reservationTime (id, time) VALUES (?, ?)", 0L, time);
+
+        return jdbcTemplate.queryForObject("SELECT id FROM reservationTime WHERE time = ?", Long.class, time);
+    }
+}
