e1f9ee1abac4678f9314012b00e8411d2ed73f28
heehunjung <heehunjung3658@gmail.com>
2025-02-27 15:44:32 +0900

    refactor: 사용하지 않는 import 및 class 삭제

--- src/main/java/roomescape/global/exception/advice/CustomExceptionHandler.java
+++ src/main/java/roomescape/global/exception/advice/CustomExceptionHandler.java
@@ -2,7 +2,6 @@ package roomescape.global.exception.advice;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
