ad42708006578065d175f955cb91ae3d34799748
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-02-28 01:21:21 +0900

    feat : AppConfig 빈 등록 및 스키마 테이블 쿼리 수정
    지금 커밋으로 적용해야 데이터베이스 정상작동이 되기 때문에 기준점으로 삼아야 함

--- src/main/java/roomescape/config/AppConfig.java
+++ src/main/java/roomescape/config/AppConfig.java
@@ -1,4 +1,30 @@
 package roomescape.config;
 
+
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import roomescape.dao.reservation.ReservationRowMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import roomescape.entity.Reservation;
+
+@Configuration
 public class AppConfig {
+
+    private final DataSource dataSource;
+
+    public AppConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public RowMapper<Reservation> reservationRowMapper() {
+        return new ReservationRowMapper(jdbcTemplate());
+    }
 }
