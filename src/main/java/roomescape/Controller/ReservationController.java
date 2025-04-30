e9215d53bb9d58b5103da2dbd0907db939b7ca31
jeonseohee9 <seohuij354@gmail.com>
2025-04-30 15:48:21 +0900

    리팩터링

--- src/main/java/roomescape/Controller/ReservationController.java
+++ src/main/java/roomescape/Controller/ReservationController.java
@@ -5,8 +5,6 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.Domain.Reservation;
 
