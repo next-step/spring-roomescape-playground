40bf7b46a54c10dc07b554f59822013af977f33c
jeonseohee9 <seohuij354@gmail.com>
2025-05-08 00:44:01 +0900

    리팩터링

--- src/main/java/roomescape/Exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/Exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package roomescape.Exception;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,7 +10,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({InvalidReservationException.class, NotFoundReservationException.class})
-    public ResponseEntity<Void> handleBadRequest(RuntimeException e) {
-        return ResponseEntity.badRequest().build();
-    }
+    public ResponseEntity<Map<String, String>> handleBadRequest(RuntimeException e) {
+        Map<String, String> error = new HashMap<>();
+        error.put("message", e.getMessage());
+        return ResponseEntity.badRequest().body(error);    }
 }
--- src/main/java/roomescape/Service/ReservationService.java
+++ src/main/java/roomescape/Service/ReservationService.java
@@ -1,4 +1,4 @@
-package roomescape;
+package roomescape.Service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
