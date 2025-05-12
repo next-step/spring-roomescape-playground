82b89044ac3361598429d271c3551df41653778d
jeonseohee9 <seohuij354@gmail.com>
2025-05-12 10:40:47 +0900

    예외 구체적 명시

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package roomescape.exception;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({InvalidReservationException.class, NotFoundReservationException.class})
-    public ResponseEntity<Map<String, String>> handleBadRequest(RuntimeException e) {
+    @ExceptionHandler(InvalidReservationException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidReservation(InvalidReservationException e) {
         Map<String, String> error = new HashMap<>();
-        error.put("message", e.getMessage());
-        return ResponseEntity.badRequest().body(error);    }
+        error.put("error", e.getMessage());
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(NotFoundReservationException.class)
+    public ResponseEntity<Map<String, String>> handleNotFoundReservation(NotFoundReservationException e) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 }
