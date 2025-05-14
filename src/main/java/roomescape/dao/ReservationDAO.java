944254cec87d9ec3c961524ed07b6ff37d1f05ed
jeonseohee9 <seohuij354@gmail.com>
2025-05-14 14:12:45 +0900

    스프링jdbc변환

--- src/main/java/roomescape/dao/ReservationDAO.java
+++ src/main/java/roomescape/dao/ReservationDAO.java
@@ -1,121 +1,55 @@
 package roomescape.dao;
 
-import jakarta.annotation.PostConstruct;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 import roomescape.domain.Reservation;
 
-@Component
+@Repository
 public class ReservationDAO {
 
-    private static final String URL = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1";
-    private static final String USERNAME = "sa";
-    private static final String PASSWORD = "";
+    private final JdbcTemplate jdbcTemplate;
 
-    public Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (final SQLException e) {
-            throw new RuntimeException("DB 연결 실패: " + e.getMessage(), e);
-        }
+    public ReservationDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
+    public Reservation addReservation(final Reservation reservation) {
+        final var query = "INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)";
+        jdbcTemplate.update(query,
+                reservation.getName(),
+                reservation.getDate().toString(),
+                reservation.getTime().toString());
 
-    @PostConstruct
-    public void createTable() {
-
-        final var query = "CREATE TABLE IF NOT EXISTS RESERVATION (" +
-                "ID BIGINT PRIMARY KEY, " +
-                "NAME VARCHAR(50), " +
-                "DATE VARCHAR(20), " +
-                "TIME VARCHAR(20))";
-
-        try (
-                final var connection = getConnection();
-        ) {
-            connection.createStatement().execute(query);
-        } catch (final SQLException e) {
-            throw new RuntimeException(e);
-        }
+        final var fetchSql = "SELECT * FROM reservation ORDER BY id DESC LIMIT 1";
+        return jdbcTemplate.queryForObject(fetchSql, reservationRowMapper);
     }
 
-    public void addReservation(final Reservation reservation) {
-        final var query = "INSERT INTO RESERVATION VALUES(?, ?, ?, ?)";
-        try (
-                final var connection = getConnection();
-                final var preparedStatement = connection.prepareStatement(query);
-        ) {
-            preparedStatement.setLong(1, reservation.getId());
-            preparedStatement.setString(2, reservation.getName());
-            preparedStatement.setString(3, reservation.getDate().toString());
-            preparedStatement.setString(4, reservation.getTime().toString());
-            preparedStatement.executeUpdate();
-        } catch (final SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public Reservation findReservation(final int id) {
-        final var query = "SELECT * FROM RESERVATION WHERE ID = ?";
-        try (
-                final var connection = getConnection();
-                final var preparedStatement = connection.prepareStatement(query);
-        ) {
-            preparedStatement.setLong(1, id);
-
-            final var resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                LocalDate date = LocalDate.parse(resultSet.getString("DATE"), DateTimeFormatter.ISO_DATE);
-                LocalTime time = LocalTime.parse(resultSet.getString("TIME"), DateTimeFormatter.ISO_TIME);
-
-                System.out.println(date + " " + time);
-                return new Reservation(
-                        resultSet.getInt("ID"),
-                        resultSet.getString("NAME"),
-                        date, time
-                );
-            }
-
-
-        } catch (final SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return null;
+    public Optional<Reservation> findByID(final int id) {
+        final var query = "SELECT * FROM reservation WHERE id = ?";
+        List<Reservation> results = jdbcTemplate.query(query, reservationRowMapper, id);
+        return results.stream().findFirst();
     }
 
     public void deleteReservation(final int id) {
-        final var query = "DELETE FROM RESERVATION WHERE ID = ?";
-        try (
-                final var connection = getConnection();
-                final var preparedStatement = connection.prepareStatement(query)
-        ) {
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-        } catch (final SQLException e) {
-            throw new RuntimeException(e);
-        }
+        final var query = "DELETE FROM reservation WHERE id = ?";
+        jdbcTemplate.update(query, id);
     }
 
-    public void resetTable() {
-        try (var connection = getConnection()) {
-            var stmt = connection.createStatement();
-            stmt.execute("DROP TABLE IF EXISTS RESERVATION");
-            stmt.execute("CREATE TABLE RESERVATION (" +
-                    "ID BIGINT PRIMARY KEY, " +
-                    "NAME VARCHAR(50), " +
-                    "DATE VARCHAR(20), " +
-                    "TIME VARCHAR(20))");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public List<Reservation> findAll() {
+        final var query = "SELECT * FROM reservation";
+        return jdbcTemplate.query(query, reservationRowMapper);
     }
 
+    private final RowMapper<Reservation> reservationRowMapper = (resultSet, rowNum) -> new Reservation(
+            resultSet.getInt("id"),
+            resultSet.getString("name"),
+            LocalDate.parse(resultSet.getString("date"), DateTimeFormatter.ISO_DATE),
+            LocalTime.parse(resultSet.getString("time"), DateTimeFormatter.ISO_TIME)
+    );
 }
--- src/test/java/roomescape/DAOTest.java
+++ src/test/java/roomescape/DAOTest.java
@@ -1,54 +1,86 @@
 package roomescape;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import io.restassured.RestAssured;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import roomescape.dao.ReservationDAO;
 import roomescape.domain.Reservation;
 
+@SpringBootTest
 public class DAOTest {
 
-    private final ReservationDAO reservationDAO = new ReservationDAO();
+    @Autowired
+    private ReservationDAO reservationDAO;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @BeforeEach
     void setUp() {
-        reservationDAO.resetTable();
+        reservationDAO.findAll().forEach(reservation ->
+                reservationDAO.deleteReservation(reservation.getId()));
     }
 
     @Test
-    public void connection() {
-        try (final var connection = reservationDAO.getConnection()) {
+    void addReservation() {
+        Reservation reservation = new Reservation(1, "전서희", LocalDate.parse("2026-05-12"), LocalTime.parse("19:00"));
+        reservationDAO.addReservation(reservation);
+        assertThat(reservationDAO.findAll()).isNotEmpty();
+    }
+
+    @Test
+    void findReservation() {
+        Reservation reservation = new Reservation(1, "전서희", LocalDate.parse("2026-05-12"), LocalTime.parse("19:00"));
+        reservationDAO.addReservation(reservation);
+
+        Optional<Reservation> found = reservationDAO.findByID(1);
+        assertThat(found).isPresent();
+        assertThat(found.get()).isEqualTo(reservation);
+    }
+
+    @Test
+    void deleteReservation() {
+        Reservation reservation = new Reservation(1, "전서희", LocalDate.parse("2026-05-12"), LocalTime.parse("19:00"));
+        reservationDAO.addReservation(reservation);
+        reservationDAO.deleteReservation(1);
+
+        Optional<Reservation> result = reservationDAO.findByID(1);
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void 오단계() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
             assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Test
-    void addReservation() {
-        final var reservation = new Reservation(1, "전서희", LocalDate.parse("2026-05-12"), LocalTime.parse("19:00"));
-        reservationDAO.addReservation(reservation);
-    }
+    void 육단계() {
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05", "15:40");
 
-    @Test
-    void findReservation() {
-        final var reservation = new Reservation(1, "전서희", LocalDate.parse("2026-05-12"), LocalTime.parse("19:00"));
-        reservationDAO.addReservation(reservation);
-        final var reservation2 = reservationDAO.findReservation(reservation.getId());
-        assertThat(reservation2).isEqualTo(new Reservation(1, "전서희", LocalDate.parse("2026-05-12"), LocalTime.parse("19:00")));
-    }
+        List<Reservation> reservations = RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200).extract()
+                .jsonPath().getList(".", Reservation.class);
 
-    @Test
-    void deleteReservation() {
-        final var reservation = new Reservation(1, "전서희", LocalDate.parse("2026-05-12"), LocalTime.parse("19:00"));
-        reservationDAO.addReservation(reservation);
-        reservationDAO.deleteReservation(reservation.getId());
-        final var result = reservationDAO.findReservation(reservation.getId());
-        assertThat(result).isNull();
-    }
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
 
+        assertThat(reservations.size()).isEqualTo(count);
+    }
 }
