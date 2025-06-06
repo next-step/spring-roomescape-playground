972ba89accd78868c4ac2e91b3e7567ad6a242f0
siyoung <wfs0502@gmail.com>
2025-06-06 15:09:56 +0900

    feat: 2단계 - 예약 조회

---
+++ src/main/java/roomescape/ReservationController.java
@@ -0,0 +1,30 @@
+package roomescape;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ReservationController {
+
+    private List<Reservation> reservations = new ArrayList<>();
+
+    public ReservationController() {
+        reservations.add(new Reservation(1L, "브라운", "2023-01-01", "10:00"));
+        reservations.add(new Reservation(2L, "브라운", "2023-01-02", "11:00"));
+        reservations.add(new Reservation(3L, "브라운", "2023-01-03", "12:00"));
+    }
+
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> reservations() {
+        return reservations;
+    }
+}
