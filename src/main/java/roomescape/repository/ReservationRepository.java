913a87ed305fd059e668e5c41132aa28bfc73b7b
heejung <mishel7@naver.com>
2025-06-10 00:12:20 +0900

    fix : KeyHolder 적용

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -2,8 +2,12 @@ package roomescape.repository;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.Reservation;
+
+import java.sql.PreparedStatement;
 import java.util.List;
 
 @Repository
@@ -25,9 +29,17 @@ public class ReservationRepository {
 
     public Reservation save(Reservation reservation) {
         String sql = "INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)";
-        jdbcTemplate.update(sql, reservation.getName(), reservation.getDate(), reservation.getTime());
+        KeyHolder keyHolder = new GeneratedKeyHolder();
 
-        Long id = jdbcTemplate.queryForObject("SELECT MAX(id) FROM reservation", Long.class);
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, reservation.getName());
+            ps.setString(2, reservation.getDate());
+            ps.setString(3, reservation.getTime());
+            return ps;
+        }, keyHolder);
+
+        Long id = keyHolder.getKey().longValue();
         return new Reservation(id, reservation.getName(), reservation.getDate(), reservation.getTime());
     }
 
