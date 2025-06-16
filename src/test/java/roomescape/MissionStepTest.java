9cb4a3505b3ae944afec1ccadcb96da309eaaf9a
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-16 19:25:38 +0900

    test: 어드민 페이지 및 예약 조회 페이지 테스트 구현

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,19 +1,38 @@
 package roomescape;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
     @Test
-    void 일단계() {
+    @DisplayName("GET / 요청 시 어드민 메인 페이지를 반환한다")
+    void shouldReturnAdminHomePage() {
         RestAssured.given().log().all()
                 .when().get("/")
                 .then().log().all()
                 .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("GET /reservation 요청 시 예약 페이지 HTML을, /reservations 요청 시 예약 리스트 JSON을 반환한다")
+    void shouldReturnReservationPageAndReservationList() {
+        RestAssured.given().log().all()
+                .when().get("/reservation")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(3)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
     }
 }
