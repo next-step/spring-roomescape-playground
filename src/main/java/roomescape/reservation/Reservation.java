01a3919aed3a51a47bb6beabe0bc00c174beaa2d
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-16 19:24:44 +0900

    feat: Reservation class 구현

---
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -0,0 +1,31 @@
+package roomescape.reservation;
+
+public class Reservation {
+    private final int id;
+    private final String name;
+    private final String date;
+    private final String time;
+
+    public Reservation(int id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
