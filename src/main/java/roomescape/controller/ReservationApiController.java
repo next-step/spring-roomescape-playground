0cc5369e88f9a78468ac0a1b745c4e9d1ddbaf43
qkrcodus <qkrcodus0722@gmail.com>
2025-06-17 21:54:30 +0900

    feat : 3단계 예약 추가 구현 완료

--- src/main/java/roomescape/controller/ReservationApiController.java
+++ src/main/java/roomescape/controller/ReservationApiController.java
@@ -1,18 +1,48 @@
 package roomescape.controller;
 
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.domain.Reservation;
+import roomescape.dto.ReservationRequest;
 import roomescape.dto.ReservationResponse;
 
 @RestController
 public class ReservationApiController {
+    private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(1);
+
+    @PostMapping("/reservations")
+    public ResponseEntity<ReservationResponse> createReservation(@RequestBody ReservationRequest reservationRequest) {
+        Reservation reservation = new Reservation(
+                index.getAndIncrement(),
+                reservationRequest.getName(),
+                reservationRequest.getDate(),
+                reservationRequest.getTime()
+        );
+        reservations.add(reservation);
+
+        ReservationResponse reservationResponse = new ReservationResponse(
+                reservation.getId(),
+                reservation.getName(),
+                reservation.getDate(),
+                reservation.getTime()
+        );
+
+        return ResponseEntity
+                .created(URI.create("/reservations/" + reservation.getId()))
+                .body(reservationResponse);
+    }
+
     @GetMapping("/reservations")
     public List<ReservationResponse> getReservations() {
-        return List.of(
-                new ReservationResponse(1L, "햄버거", "2025-06-17", "10:00"),
-                new ReservationResponse(2L, "감자튀김", "2025-06-17", "11:00"),
-                new ReservationResponse(3L, "콜라", "2025-06-17", "12:00")
-        );
+        return reservations.stream().map(r -> new ReservationResponse(r.getId(), r.getName(), r.getDate(), r.getTime()))
+                .toList();
     }
 }
