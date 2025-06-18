91bd75b7788c4f58666646257b4b7c789eeab55c
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-18 10:26:07 +0900

    refactor: record로 변환

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -1,31 +1,7 @@
 package roomescape.reservation;
 
-public class Reservation {
-    private final int id;
-    private final String name;
-    private final String date;
-    private final String time;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-    public Reservation(int id, String name, String date, String time) {
-        this.id = id;
-        this.name = name;
-        this.date = date;
-        this.time = time;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public String getTime() {
-        return time;
-    }
+public record Reservation(Long id, String name, LocalDate date, LocalTime time) {
 }
