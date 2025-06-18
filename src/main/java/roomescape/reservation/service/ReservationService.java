d64dae25615b4b765694b449284944ae095fdade
SanghyunLee <lgw9736@naver.com>
2025-06-18 21:22:07 +0900

    refactor: 예약 자료형 변경
    - List -> Map

--- src/main/java/roomescape/reservation/service/ReservationService.java
+++ src/main/java/roomescape/reservation/service/ReservationService.java
@@ -1,7 +1,8 @@
 package roomescape.reservation.service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,11 @@ import roomescape.reservation.exception.NotFoundReservationException;
 
 @Service
 public class ReservationService {
-    private final List<Reservation> reservations = new ArrayList<>();
+    private final Map<Long, Reservation> reservations = new ConcurrentHashMap<>();
     private final AtomicLong index = new AtomicLong(1);
 
     public List<ReservationResponse> getReservations() {
-        return reservations.stream()
+        return reservations.values().stream()
                 .map(this::toResponseDTO)
                 .collect(Collectors.toList());
     }
@@ -28,17 +29,15 @@ public class ReservationService {
 
         Long id = index.getAndIncrement();
         Reservation newReservation = new Reservation(id, request.name(), request.date(), request.time());
-        reservations.add(newReservation);
+        reservations.put(id, newReservation);
         return toResponseDTO(newReservation);
     }
 
     public void deleteReservation(Long id) {
-        Reservation target = reservations.stream()
-                .filter(reservation -> reservation.getId().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new NotFoundReservationException("해당 ID의 예약이 존재하지 않습니다: "));
-
-        reservations.remove(target);
+        Reservation target = reservations.remove(id);
+        if (target == null) {
+            throw new NotFoundReservationException("해당 ID의 예약이 존재하지 않습니다: ");
+        }
     }
 
     private ReservationResponse toResponseDTO(Reservation reservation) {
@@ -51,7 +50,7 @@ public class ReservationService {
     }
 
     private boolean isDuplicate(ReservationRequest request) {
-        return reservations.stream().anyMatch(reservation ->
+        return reservations.values().stream().anyMatch(reservation ->
                 reservation.getDate().equals(request.date()) &&
                         reservation.getTime().equals(request.time())
         );
