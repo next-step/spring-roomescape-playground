7e9c7eed96c5894aea7ab56a88a9da818d39cfb9
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-18 10:26:37 +0900

    refactor: 임시 객체 타입 변경

--- src/main/java/roomescape/reservation/ReservationApiController.java
+++ src/main/java/roomescape/reservation/ReservationApiController.java
@@ -4,6 +4,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +15,9 @@ public class ReservationApiController {
 
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> read() {
-        reservations.add(new Reservation(1, "브라운","2023-01-01","10:00"));
-        reservations.add(new Reservation(2, "브라운","2023-01-02","11:00"));
-        reservations.add(new Reservation(3, "브라운","2023-01-03","12:00"));
+        reservations.add(new Reservation(1L, "브라운", LocalDate.now(), LocalTime.now()));
+        reservations.add(new Reservation(2L, "브라운",LocalDate.now(), LocalTime.now()));
+        reservations.add(new Reservation(3L, "브라운",LocalDate.now(), LocalTime.now()));
 
         return ResponseEntity.ok().body(reservations);
     }
