75e724cd37b0cd3ecba770e3ef56811ab2070da7
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-19 18:28:19 +0900

    feat: 검증 메소드 추가

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -1,10 +1,19 @@
 package roomescape.reservation;
 
+import roomescape.exception.BadRequestException;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record Reservation(Long id, String name, LocalDate date, LocalTime time) {
     public static Reservation toEntity(Reservation reservation, Long id) {
         return new Reservation (id, reservation.name, reservation.date, reservation.time);
+    }
+
+    public void validateRequiredFields() {
+        if (this.name() == null || this.name().isBlank() ||
+                this.date() == null || this.time() == null) {
+            throw new BadRequestException("예약 요청에 누락된 값이 있습니다.");
+        }
     }
 }
