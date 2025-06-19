aa837ed32b113fabb72862df1e8a4d55e1433e1f
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-19 17:49:09 +0900

    feat: 예약 추가/취소 메소드 구현

--- src/main/java/roomescape/reservation/ReservationApiController.java
+++ src/main/java/roomescape/reservation/ReservationApiController.java
@@ -1,24 +1,46 @@
 package roomescape.reservation;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class ReservationApiController {
-    private List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(1);
+    private final List<Reservation> reservations = new ArrayList<>();
 
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> read() {
-        reservations.add(new Reservation(1L, "브라운", LocalDate.now(), LocalTime.now()));
-        reservations.add(new Reservation(2L, "브라운",LocalDate.now(), LocalTime.now()));
-        reservations.add(new Reservation(3L, "브라운",LocalDate.now(), LocalTime.now()));
+        reservations.add(new Reservation(index.getAndIncrement(), "브라운", LocalDate.now(), LocalTime.now()));
+        reservations.add(new Reservation(index.getAndIncrement(), "브라운",LocalDate.now(), LocalTime.now()));
+        reservations.add(new Reservation(index.getAndIncrement(), "브라운",LocalDate.now(), LocalTime.now()));
 
         return ResponseEntity.ok().body(reservations);
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Void> create(@RequestBody Reservation reservation) {
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        reservations.add(newReservation);
+
+        return ResponseEntity.created(URI.create("/reservations/" + newReservation.id())).build();
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(reservationItem -> Objects.equals(reservationItem.id(), id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+        reservations.remove(reservation);
+
+        return ResponseEntity.noContent().build();
     }
 }
