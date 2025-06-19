719d296fed97ac934aac81aeb75740c3cd6b63ee
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-19 18:07:01 +0900

    feat: ReservationResponse 구현

---
+++ src/main/java/roomescape/reservation/dto/ReservationResponse.java
@@ -0,0 +1,25 @@
+package roomescape.reservation.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import roomescape.reservation.Reservation;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record ReservationResponse(
+        Long id,
+        String name,
+        LocalDate date,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
+        LocalTime time
+) {
+    public static ReservationResponse from(Reservation reservation) {
+        return new ReservationResponse(
+                reservation.id(),
+                reservation.name(),
+                reservation.date(),
+                reservation.time()
+        );
+    }
+}
