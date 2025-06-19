cfd6835ef0c0f09b1f89015ed14e698283d3f1f1
minji <mmm307955@gmail.com>
2025-06-19 20:49:13 +0900

    refactor: InvalidRequestException 클래스 예외 메시지 추가

--- src/main/java/roomescape/exception/InvalidRequestException.java
+++ src/main/java/roomescape/exception/InvalidRequestException.java
@@ -2,6 +2,6 @@ package roomescape.exception;
 
 public class InvalidRequestException extends IllegalArgumentException {
     public InvalidRequestException(String message) {
-        super(message);
+        super("비어있는 입력이 있습니다: " + message);
     }
 }
