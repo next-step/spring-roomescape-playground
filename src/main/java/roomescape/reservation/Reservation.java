c40a3948b8bba84e3abc1e26ec8441e5ed2e3eab
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-19 17:48:48 +0900

    feat: toEntity 정적메소드 구현

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -4,4 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record Reservation(Long id, String name, LocalDate date, LocalTime time) {
+    public static Reservation toEntity(Reservation reservation, Long id) {
+        return new Reservation (id, reservation.name, reservation.date, reservation.time);
+    }
 }
