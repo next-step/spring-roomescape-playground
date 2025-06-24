806f55d37902457fc6cc8238fd02bc5373966e70
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-24 20:42:38 +0900

    feat: ReservationService 구현

---
+++ src/main/java/roomescape/reservation/service/ReservationService.java
@@ -0,0 +1,28 @@
+package roomescape.reservation.service;
+
+import org.springframework.stereotype.Service;
+import roomescape.reservation.domain.Reservation;
+import roomescape.reservation.repository.ReservationRepository;
+
+import java.util.List;
+
+@Service
+public class ReservationService {
+    private final ReservationRepository reservationRepository;
+
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
+    public List<Reservation> findAll() {
+        return reservationRepository.findAll();
+    }
+
+    public Reservation save(Reservation reservation) {
+        return reservationRepository.save(reservation);
+    }
+
+    public void deleteById(Long id) {
+        reservationRepository.deleteById(id);
+    }
+}
