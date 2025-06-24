c21e78733f2c6cb41c48da1f3746b064f426dfc9
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-24 20:31:28 +0900

    feat: 정적 메소드 추가

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -6,9 +6,13 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record Reservation(Long id, String name, LocalDate date, LocalTime time) {
+    //memory 용
     public static Reservation of(ReservationRequest request, Long id) {
         return new Reservation (id, request.name(), request.date(), request.time());
     }
 
-
+    //DB 용
+    public static Reservation of(ReservationRequest request) {
+        return new Reservation(null, request.name(), request.date(), request.time());
+    }
 }
