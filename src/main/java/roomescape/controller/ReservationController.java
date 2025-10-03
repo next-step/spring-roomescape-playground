bb1e64cce96c1f3f56697aae435861d348a623cc
corjqnrl <coqnrl@ewha.ac.kr>
2025-10-03 22:37:22 +0900

    2단계 - 예약 조회

---
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -0,0 +1,38 @@
+package roomescape.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import roomescape.model.Reservation;
+
+@Controller
+public class ReservationController {
+  private List<Reservation> reservations = new ArrayList<>();
+
+  @GetMapping("/reservation")
+  public String reservation() {
+    return "reservation";
+  }
+
+  @GetMapping("/reservations")
+  @ResponseBody
+  public List<Reservation> getReservation() {
+    reservations.add(new Reservation(
+      1L, 
+      "브라운", 
+      "2023-01-01", 
+      "10:00"
+    ));
+    
+    reservations.add(new Reservation(
+      2L, 
+      "브라운", 
+      "2023-01-02", 
+      "11:00"
+    ));
+
+    return reservations;
+  }
+}
