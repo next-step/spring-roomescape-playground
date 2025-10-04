a509ea2cc1fe91a6a4a896d9f945ed48ce329642
Siwon <trxye.522@gmail.com>
2025-10-04 23:37:38 +0900

    2단계 - 예약 조회

--- src/main/java/roomescape/HomeController.java
+++ src/main/java/roomescape/HomeController.java
@@ -2,7 +2,6 @@
 package roomescape;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
