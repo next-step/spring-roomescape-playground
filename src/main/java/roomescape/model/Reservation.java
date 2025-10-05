4bd639c715b7655b271821e0e491b81aae045ced
corjqnrl <coqnrl@ewha.ac.kr>
2025-10-05 19:41:39 +0900

    type 변경

--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -6,7 +6,7 @@ public class Reservation {
     private String date;
     private String time;
 
-    public Reservation(long id, String name, String date, String time) {
+    public Reservation(Long id, String name, String date, String time) {
         this.id = id;
         this.name = name;
         this.date = date;
