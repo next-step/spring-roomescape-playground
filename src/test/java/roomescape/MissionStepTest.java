e1d4c6802a4c6e10f7827c113cedcd5d4a627a08
us2xince <kstar1029@ewhain.net>
2025-11-15 02:17:46 +0900

    <1단계:홈화면>

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,9 +1,15 @@
 package roomescape;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -16,4 +22,6 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200);
     }
+
+
 }
