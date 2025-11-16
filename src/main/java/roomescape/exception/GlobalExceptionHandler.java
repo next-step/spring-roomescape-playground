e817519ee29613df7d3905245b2534c1c453ce36
김태우 <162796810+tae-wooo@users.noreply.github.com>
2025-11-16 19:18:16 +0900

    feat: 예외 JSON 응답 통일 및 DTO/날짜 파싱 예외 처리 추가

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -2,23 +2,42 @@ package roomescape.exception;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import roomescape.dto.ErrorResponse;
 
-@ControllerAdvice
+import java.time.format.DateTimeParseException;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidReservationArgumentException.class)
-    public ResponseEntity<String> handlerInvalidReservationArgumentException(InvalidReservationArgumentException e) {
+    public ResponseEntity<ErrorResponse> handlerInvalidReservationArgumentException(InvalidReservationArgumentException e) {
         return ResponseEntity
                 .badRequest()
-                .body(e.getMessage());
+                .body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(NotFoundReservationException.class)
-    public ResponseEntity<String> handleNotFoundReservationException(NotFoundReservationException e) {
+    public ResponseEntity<ErrorResponse> handlerNotFoundReservationException(NotFoundReservationException e) {
         return ResponseEntity
                 .badRequest()
-                .body(e.getMessage());
+                .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse(message));
+    }
+
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<ErrorResponse> handleDateTimeParseException(DateTimeParseException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse("날짜 또는 시간이 기본형식이 아닙니다. ex) 2025-11-16, 18:23"));
     }
 }
