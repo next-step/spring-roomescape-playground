f9935e5d0d2464113eeee69a10b0ece25fd5b4b4
nonactress <tjgsuwls1007@gmail.com>
2025-11-16 14:18:31 +0900

    fix(dto) : local date, local time 으로 변경

--- src/main/java/roomescape/dto/ReservationCreateRequest.java
+++ src/main/java/roomescape/dto/ReservationCreateRequest.java
@@ -2,6 +2,9 @@ package roomescape.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 
 public record ReservationCreateRequest(
 
@@ -9,10 +12,10 @@ public record ReservationCreateRequest(
         String name,
 
         @NotBlank(message = "날짜는 필수 항목입니다.")
-        String date,
+        LocalDate date,
 
         @NotBlank(message = "시간은 필수 항목입니다.")
-        String time
+        LocalTime time
 ) {
 
 }
