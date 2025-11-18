efff5e44588ea95e4bb2f0e34dda08214df7d46c
nonactress <tjgsuwls1007@gmail.com>
2025-11-18 19:40:13 +0900

    refactor : 레포지토리에 맞게 서비스 로직 리팩토링

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,54 +1,29 @@
 package roomescape.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import roomescape.model.Reservation;
-
-import java.util.ArrayList;
+import roomescape.repository.ReservationRepository;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong; // 1. ID 생성을 위해 추가
 
 @Service
 public class ReservationService {
+    private final ReservationRepository reservationRepository;
 
-    private final List<Reservation> reservations = new ArrayList<>();
-
-    private final AtomicLong counter = new AtomicLong();
-
-    public ReservationService() {
-        reservations.add(new Reservation(counter.incrementAndGet(), "브라운", "2025-01-01", "10:00"));
-        reservations.add(new Reservation(counter.incrementAndGet(), "코니", "2025-01-02", "11:00"));
+    @Autowired
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
     }
 
     public List<Reservation> getAllReservations() {
-        return reservations;
+        return reservationRepository.findAll();
     }
 
     public Reservation addReservation(Reservation newReservation) {
-        Reservation savedReservation = new Reservation(
-                counter.incrementAndGet(), // 새 ID 발급
-                newReservation.getName(),
-                newReservation.getDate(),
-                newReservation.getTime()
-        );
-        reservations.add(savedReservation);
-        return savedReservation;
+        return reservationRepository.save(newReservation);
     }
-
-
 
     public void deleteReservation(Long id) {
-        boolean exists = reservations.stream()
-                .anyMatch(reservation -> reservation.getId().equals(id));
-
-        if (!exists) {
-            throw new IllegalArgumentException("존재하지 않는 예약 ID입니다: " + id);
-        }
-
-        reservations.removeIf(reservation -> reservation.getId().equals(id));
-    }
-
-    public void clear() {
-        reservations.clear();
-        counter.set(0L);      //테스트를 위한 코드 입니다.
+        reservationRepository.deleteById(id);
     }
 }
