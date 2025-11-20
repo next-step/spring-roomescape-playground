913fe55c8160165f040e1341f5fe57dab4919172
김태우 <162796810+tae-wooo@users.noreply.github.com>
2025-11-20 13:31:44 +0900

    refactor: DB 스키마에 맞춰 Reservation의 date/time 타입을 String으로 변경

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -8,14 +8,18 @@ import java.time.LocalTime;
 public class Reservation {
     private final Long id;
     private final String name;
-    private final LocalDate date;
-    private final LocalTime time;
+    private final String date;
+    private final String time;
 
-    private Reservation(Long id, String name, LocalDate date, LocalTime time) {
+    private Reservation(Long id, String name, String date, String time) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
+    }
+
+    public static Reservation newReservationFromDb(Long id, String name, String date, String time) {
+        return new Reservation(id, name, date, time);
     }
 
     public static Reservation createReservation(Long id, String name, String stringDate, String stringTime) {
@@ -29,7 +33,7 @@ public class Reservation {
         if (date.isEqual(LocalDate.now()) && time.isBefore(LocalTime.now())) {
             throw new InvalidReservationArgumentException("예약 시간은 현재 시간 이후여야 합니다.");
         }
-        return new Reservation(id, name, date, time);
+        return new Reservation(id, name, stringDate, stringTime);
     }
 
 
@@ -41,11 +45,11 @@ public class Reservation {
         return name;
     }
 
-    public LocalDate getDate() {
+    public String getDate() {
         return date;
     }
 
-    public LocalTime getTime() {
+    public String getTime() {
         return time;
     }
 }
