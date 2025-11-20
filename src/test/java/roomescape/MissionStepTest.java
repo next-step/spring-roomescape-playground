75bd872e33032b4132463eb1b6446cb7f79b4fd6
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-11-20 13:23:58 +0900

    test: 테스트코드 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -99,16 +99,32 @@ public class MissionStepTest {
     }
 
     @Autowired
-private JdbcTemplate jdbcTemplate;
+    private JdbcTemplate jdbcTemplate;
+
 
         @Test
         void 오단계() {
-        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
-                assertThat(connection).isNotNull();
-                assertThat(connection.getCatalog()).isEqualTo("DATABASE");
-                assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
-        } catch (SQLException e) {
-                throw new RuntimeException(e);
+                try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+                        assertThat(connection).isNotNull();
+                        assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+                        assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+                } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                }
         }
+
+        @Test
+        void 육단계() {
+            jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05", "15:40");
+        
+            List<Reservation> reservations = RestAssured.given().log().all()
+                    .when().get("/reservations")
+                    .then().log().all()
+                    .statusCode(200).extract()
+                    .jsonPath().getList(".", Reservation.class);
+        
+            Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+        
+            assertThat(reservations.size()).isEqualTo(count);
         }
 }
