4be4b67e9c2a96980970b5edff56503509d2bb88
us2xince <kstar1029@ewhain.net>
2025-11-29 16:13:33 +0900

    <10단계:계층화 리팩터링>

--- src/main/java/roomescape/controller/ReservationAPIController.java
+++ src/main/java/roomescape/controller/ReservationAPIController.java
@@ -1,5 +1,6 @@
 package roomescape.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,7 @@ import java.util.Map;
 public class ReservationAPIController {
     private final ReservationRepository reservationRepository;
 
+    @Autowired
     public ReservationAPIController(ReservationRepository reservationRepository) {
         this.reservationRepository = reservationRepository;
     }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import roomescape.controller.ReservationController;
 import roomescape.model.Reservation;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -194,4 +196,20 @@ public class MissionStepTest {
             .statusCode(400);
     }
 
+    @Autowired
+    private ReservationController reservationController;
+
+    @Test
+    void 십단계() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
+    }
 }
