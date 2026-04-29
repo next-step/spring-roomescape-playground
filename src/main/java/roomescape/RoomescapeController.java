5f2993ba0a47683f050c7882c7dd21932d42ca6e
pce0303 <ivychoen@gmail.com>
2026-04-29 14:30:00 +0900

    feat: 2단계 구현 Co-authored-by: 손희창 a01073111145@gmail.com

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -1,13 +1,39 @@
 package roomescape;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RoomescapeController {
     @GetMapping("/")
-    public String showHome() {
+    public String showHomePage() {
         return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String showReservationPage() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> showReservations() {
+        showReservationPage();
+        List<Reservation> reservationList = insertReservation();
+        return reservationList;
+    }
+
+    private List<Reservation> insertReservation() {
+        List<Reservation> reservations = new ArrayList<>();
+        reservations.add(new Reservation(1L, "a", LocalDate.now(), LocalTime.now()));
+        reservations.add(new Reservation(2L, "b", LocalDate.now(), LocalTime.now()));
+        reservations.add(new Reservation(3L, "c", LocalDate.now(), LocalTime.now()));
+        return reservations;
     }
 }
