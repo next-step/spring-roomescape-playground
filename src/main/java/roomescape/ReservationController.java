a6ccf2f4bafabec0761bc1366c19b8fa56e2dbb9
haeun92e0 <gracekim8729@gmail.com>
2026-05-03 22:19:48 +0900

    Feat: 예약 관리 페이지 연결 및 CRUD API 구현

---
+++ src/main/java/roomescape/ReservationController.java
@@ -0,0 +1,56 @@
+package roomescape;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import roomescape.Reservation;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller //웹 요청을 받는 담당자
+public class ReservationController {
+    private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(0);
+
+    @GetMapping("/")
+    //"/"주소로 get을 요청하면 아래 메서드 실행
+    public String home() {
+        return "home";
+    }//화면 보여주기
+
+    @GetMapping("/reservation")
+    public String reservationPage() {
+        return "reservation";
+    }//예약관리 페이지를 보여줌
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> getReservations() {//예약 목록을 조회
+        return reservations; //현재 저장된 예약 리스트 전체를 응답으로 보냄
+    }
+
+    @PostMapping("/reservations")
+    @ResponseBody
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation request) {//reservation객체로 바꿔서 받음
+        Reservation reservation = new Reservation(//새 예약 객체 만듦
+                index.incrementAndGet(), //id를 1 증가시키고 값을 가져옴
+                request.getName(),
+                request.getDate(),
+                request.getTime()
+        );
+        reservations.add(reservation);
+        return ResponseEntity
+                .created(URI.create("/reservations/" + reservation.getId())) //http 상태코드 201을 응답하고, 새로 만든 예약 위치를 /reservations/1같은 주소로 알려줌
+                .body(reservation);//응답 본문에 새로 생성된 예약 객체를 담아서 보냄
+    }
+
+    @DeleteMapping("/reservations/{id}") //삭제 요청
+    @ResponseBody
+    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {//주소에 있는 id값을 꺼내서 변수 id에 넣음
+        reservations.removeIf(reservation -> reservation.getId().equals(id));
+        return ResponseEntity.noContent().build(); //삭제 성공 후 응답 보내기. 204 no content 상태 코드를 반환
+    }
+}
