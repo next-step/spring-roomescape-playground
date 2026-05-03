e069e8c9163d602c73bdb2e9ad1d102ca0aaf464
TheDeveloperKwon <akongwon@gmail.com>
2026-05-03 23:33:28 +0900

    feat: 미션 테스트 전부 통과 완료

---
+++ src/main/java/roomescape/domain/Reservation.java
@@ -0,0 +1,36 @@
+package roomescape.domain;
+
+public class Reservation {
+    private final Long id;
+    private final String name;
+    private final String date;
+    private final String time;
+
+    public Reservation(Long id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public static Reservation toEntity(Reservation reservation, Long id) {
+        return new Reservation(id, reservation.name, reservation.date, reservation.time);
+    }
+
+}
---
+++ src/main/java/roomescape/exception/NotFoundReservationException.java
@@ -0,0 +1,7 @@
+package roomescape.exception;
+
+public class NotFoundReservationException extends RuntimeException {
+    public NotFoundReservationException() {
+        super("예약을 찾을 수 없습니다.");
+    }
+}
