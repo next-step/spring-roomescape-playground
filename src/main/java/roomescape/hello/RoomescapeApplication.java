3bc545dd81eb8b9a6298c930fc697d8c62efefb7
mgim9316-a11y <mgim9316@gmail.com>
2026-05-03 23:26:21 +0900

    feat: application 작성

--- src/main/java/roomescape/hello/RoomescapeApplication.java
+++ src/main/java/roomescape/hello/RoomescapeApplication.java
@@ -1,4 +1,4 @@
-package roomescape;
+package roomescape.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,5 +8,4 @@ public class RoomescapeApplication {
     public static void main(String[] args) {
         SpringApplication.run(RoomescapeApplication.class, args);
     }
-
 }
