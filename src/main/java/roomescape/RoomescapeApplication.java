5ca8ad06d2d201465e080c9eff1721a3e102b4bd
haeun92e0 <gracekim8729@gmail.com>
2026-05-03 22:19:39 +0900

    Chore: 스프링 부트 애플리케이션 메인 클래스 설정

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -2,7 +2,7 @@ package roomescape;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//방탈출 예약 관리 시스템을 구동하기 위한 엔진 스위치
 @SpringBootApplication
 public class RoomescapeApplication {
     public static void main(String[] args) {
