1b0f8c123f7549e4c94b4b772519e768b9b1f0b0
HEECHANG SON <73211854+mikeylili@users.noreply.github.com>
2026-05-04 19:51:05 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

---
+++ src/main/java/roomescape/Reservation.java
@@ -0,0 +1,37 @@
+package roomescape;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+    private Long id;
+    private String name;
+    private String date;
+    private String time;
+
+    public Reservation(Long id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public static Reservation toEntity(Reservation reservation, Long id) {
+        return new Reservation(id, reservation.name, reservation.date, reservation.time);
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
---
+++ src/main/java/roomescape/RoomescapeController.java
@@ -0,0 +1,56 @@
+package roomescape;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RoomescapeController {
+
+    private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
+
+    @GetMapping("/")
+    public String showHomePage() {
+        return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String showReservationPage() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation) {
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        reservations.add(newReservation);
+        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public  ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+        reservations.remove(reservation);
+
+        return ResponseEntity.noContent().build();
+    }
+}
