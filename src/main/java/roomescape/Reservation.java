5f20660b26ee40b57da594130791b8d5cf13aaa7
Leech <leechan7120@g.skku.edu>
2026-05-04 20:08:25 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

---
+++ src/main/java/roomescape/Reservation.java
@@ -0,0 +1,94 @@
+package roomescape;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Reservation {
+
+    private int reservationId;
+    private String nameOfUser;
+    @JsonIgnore
+    private LocalDateTime dateTime;;
+
+    @JsonCreator
+    public Reservation(
+            @JsonProperty("name") String name,
+            @JsonProperty("date") LocalDate date,
+            @JsonProperty("time") LocalTime time
+    ) {
+        LocalDateTime dt = LocalDateTime.of(date, time);
+
+        checkValidName(name);
+        checkValidDateTime(dt);
+
+        this.nameOfUser = name;
+        this.dateTime = dt;
+        this.reservationId = 0;
+    }
+
+    public Reservation(int id, String name, LocalDateTime dateTime) {
+        this.reservationId = id;
+        this.nameOfUser = name;
+        this.dateTime = dateTime;
+    }
+
+    public static Reservation createNewReservation(int id, String name, LocalDateTime dateTime){
+        checkValidId(id);
+        checkValidName(name);
+        checkValidDateTime(dateTime);
+        return new Reservation(id, name, dateTime);
+    }
+
+    @JsonProperty("date")
+    public LocalDate getDate() {
+        return dateTime.toLocalDate();
+    }
+
+    @JsonProperty("time")
+    public LocalTime getTime() {
+        return dateTime.toLocalTime();
+    }
+
+    public int getId() {
+        return reservationId;
+    }
+
+    public String getName() {
+        return nameOfUser;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+    
+    public boolean isSameTime(LocalDateTime date) {
+        return this.dateTime.equals(date);
+    }
+
+    private static void checkValidId(int id){
+        if (id <= 0){
+            throw new IllegalArgumentException("id는 자연수여야 합니다.");
+        }
+    }
+
+    private static void checkValidName(String nameOfUser){
+        if (nameOfUser == null || nameOfUser.isBlank()){
+            throw new IllegalArgumentException("이름이 비었습니다.");
+        }
+    }
+    
+    private static void checkValidDateTime(LocalDateTime dateTime){
+        if (dateTime == null){
+            throw new IllegalArgumentException("날짜가 비었습니다.");
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        if (dateTime.isBefore(now)){
+            throw new IllegalArgumentException("이미 지난 날짜입니다.");
+        }
+    }
+}
---
+++ src/main/java/roomescape/RoomescapeController.java
@@ -0,0 +1,67 @@
+package roomescape;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class RoomescapeController {
+
+    private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicInteger index = new AtomicInteger(0);
+
+    @GetMapping("/")
+    public String showPage(){
+        return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String showReservationPage() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public ResponseEntity<List<Reservation>> showAllReservations() {
+        return ResponseEntity.ok(reservations);
+    }
+
+    @PostMapping("/reservations")
+    @ResponseBody
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation request) {
+
+        boolean isDuplicate = reservations.stream()
+                .anyMatch(res -> res.isSameTime(request.getDateTime()));
+
+        if (isDuplicate) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+        Reservation reservationOfClient = Reservation.createNewReservation(
+                index.incrementAndGet(),
+                request.getName(),
+                request.getDateTime()
+        );
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/reservations/" + reservationOfClient.getId());
+        reservations.add(reservationOfClient);
+        return ResponseEntity.status(HttpStatus.CREATED).headers(headers).body(reservationOfClient);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteReservation(@PathVariable int id) {
+        reservations.removeIf(res -> res.getId() == id);
+    }
+}
