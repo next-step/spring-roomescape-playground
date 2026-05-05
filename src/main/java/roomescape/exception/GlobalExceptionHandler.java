7e804dd7e9852cb77dbcc3a19d712011c4a89bd6
pce0303 <ivychoen@gmail.com>
2026-05-05 23:31:42 +0900

    feat: GlobalExceptionHandler 추가

---
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package roomescape.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequest(BadRequestException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(e.getMessage());
+    }
+}
