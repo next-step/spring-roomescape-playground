b6e6aa1f557f0a1552e085de02d5512f0dd34290
mikeylili <a01073111145@gmail.com>
2026-05-06 23:41:34 +0900

    feat: 4단계 구현

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -5,14 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import roomescape.exception.NotFoundReservationException;
 
 @Controller
 public class RoomescapeController {
@@ -38,6 +41,9 @@ public class RoomescapeController {
 
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation) {
+        if (reservation.getDate().isBlank() || reservation.getTime().isBlank() || reservation.getName().isBlank()) {
+            throw new NotFoundReservationException("date, time은 필수");
+        }
         Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
         reservations.add(newReservation);
         return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
@@ -48,9 +54,14 @@ public class RoomescapeController {
         Reservation reservation = reservations.stream()
                 .filter(it -> Objects.equals(it.getId(), id))
                 .findFirst()
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(NotFoundReservationException::new);
         reservations.remove(reservation);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(NotFoundReservationException.class)
+    public ResponseEntity<String> handleException(NotFoundReservationException e) {
+        return ResponseEntity.badRequest().build();
     }
 }
---
+++ src/main/java/roomescape/exception/NotFoundReservationException.java
@@ -0,0 +1,12 @@
+package roomescape.exception;
+
+public class NotFoundReservationException extends RuntimeException {
+
+    public NotFoundReservationException() {
+        super("예약을 찾을 수 없습니다.");
+    }
+
+    public NotFoundReservationException(String message) {
+        super(message);
+    }
+}
