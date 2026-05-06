f3cdea43823666c37530373671cc3df2adac1877
mikeylili <a01073111145@gmail.com>
2026-05-07 00:58:50 +0900

    feat: 날짜시간 중복, 과거 예약 예외 처리 추가

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -1,24 +1,48 @@
 package roomescape;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import roomescape.exception.InvalidReservationException;
+import roomescape.exception.NotFoundReservationException;
 
 public class Reservation {
     private Long id;
     private String name;
-    private String date;
-    private String time;
+    private LocalDate date;
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime time;
 
-    public Reservation(Long id, String name, String date, String time) {
+    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+        validate(name, date, time);
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
     }
 
-    public static Reservation toEntity(Reservation reservation, Long id) {
-        return new Reservation(id, reservation.name, reservation.date, reservation.time);
+    private void validate(String name, LocalDate date, LocalTime time) {
+        if (name == null || name.isBlank() || date == null || time == null) {
+            throw new NotFoundReservationException("필수 값이 누락되었습니다.");
+        }
+
+        if (date.isBefore(LocalDate.now())) {
+            throw new InvalidReservationException("과거 날짜로 예약할 수 없습니다.");
+        }
+
+        if (date.isEqual(LocalDate.now()) && time.isBefore(LocalTime.now())) {
+            throw new InvalidReservationException("이미 지난 시간입니다.");
+        }
     }
+
+    public static Reservation toEntity(Reservation reservation, Long id) {
+        return new Reservation(id, reservation.getName(), reservation.getDate(), reservation.getTime());
+    }
+
+    public boolean isSameTime(LocalDate date, LocalTime time) {
+        return this.date.equals(date) && this.time.equals(time);
+    }
+
     public Long getId() {
         return id;
     }
@@ -27,11 +51,11 @@ public class Reservation {
         return name;
     }
 
-    public String getDate() {
+    public LocalDate getDate() {
         return date;
     }
 
-    public String getTime() {
+    public LocalTime getTime() {
         return time;
     }
 }
