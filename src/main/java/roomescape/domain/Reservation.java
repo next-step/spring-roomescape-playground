e32269ffd78dc478979a82a511bcff0ae430567b
TheDeveloperKwon <akongwon@gmail.com>
2026-05-06 16:10:54 +0900

    feat: 빌더 패턴 추가. 라이브러리를 사용한 getter 생성

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,8 +1,13 @@
 package roomescape.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Getter
+@Builder
 public class Reservation {
     private final Long id;
     private final String name;
@@ -15,26 +20,4 @@ public class Reservation {
         this.date = date;
         this.time = time;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
-
-    // static 말고 그냥 파라미터 없이
-    public Reservation toEntity(Long id) {
-        return new Reservation(id, this.name, this.date, this.time);
-    }
-
 }
