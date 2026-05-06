8543e280f717aabb588cf101572d395a1584926e
Hojun Yang <38321374+hojunYang@users.noreply.github.com>
2026-05-06 12:41:08 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -22,14 +22,14 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200);
     }
-    
+
     @Test
     void 이단계() {
         RestAssured.given().log().all()
                 .when().get("/reservation")
                 .then().log().all()
                 .statusCode(200);
-        
+
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
@@ -42,7 +42,7 @@ public class MissionStepTest {
         params.put("name", "브라운");
         params.put("date", "2023-08-05");
         params.put("time", "15:40");
-        
+
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(params)
@@ -51,18 +51,18 @@ public class MissionStepTest {
                 .statusCode(201)
                 .header("Location", "/reservations/1")
                 .body("id", is(1));
-        
+
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
-        
+
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
                 .statusCode(204);
-        
+
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
