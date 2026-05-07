5f0ffbdc90a6db912d2f4562cbedac388eb91947
mgim9316-a11y <mgim9316@gmail.com>
2026-05-07 14:46:41 +0900

    refactor: 필요 없는 기능 삭제

--- src/main/java/roomescape/hello/GreetingController.java
+++ src/main/java/roomescape/hello/GreetingController.java
@@ -9,15 +9,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GreetingController {
 
-    private static final String TEMPLATE = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @ResponseBody
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
-    }
-
     @GetMapping("/")
     public String home() {
         // src/main/resources/templates/home.html 을 찾아 렌더링함
