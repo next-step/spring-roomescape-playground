90cc93d0d95f4e91703bb07234ce2d77ca75b138
monsaumon <monsaumon@naver.com>
2026-05-08 03:41:59 +0900

    refactor: toLocalDateTime 로직 개선

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -1,8 +1,9 @@
 package roomescape.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalTime;
 import roomescape.domain.Reservation;
 
 public record ReservationRequest(@NotBlank(message = "이름이 비어 있을 수 없습니다.") String name,
@@ -13,8 +14,9 @@ public record ReservationRequest(@NotBlank(message = "이름이 비어 있을 �
     }
 
     private LocalDateTime toLocalDateTime(String date, String time) {
-        String dateTimeString = date + " " + time;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        return LocalDateTime.parse(dateTimeString, formatter);
+        LocalDate localDate = LocalDate.parse(date);
+        LocalTime localTime = LocalTime.parse(time);
+
+        return LocalDateTime.of(localDate, localTime);
     }
 }
