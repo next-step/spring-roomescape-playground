9f6fe448714f5862f458d162f27b2b4e375b920e
haeun92e0 <gracekim8729@gmail.com>
2026-05-07 21:57:17 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

---
+++ src/main/java/roomescape/Reservation.java
@@ -0,0 +1,34 @@
+package roomescape;
+
+public class Reservation {
+    private Long id;
+    private String name;
+    private String date;
+    private String time;
+
+    public Reservation(Long id, String name, String date, String time){
+        this.id=id;
+        this.name=name;
+        this.date=date;
+        this.time=time;
+    }
+
+    //getter 메서드들
+    public Long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public String getTime(){
+        return time;
+    }
+}
+
+
---
+++ src/main/java/roomescape/ViewController.java
@@ -0,0 +1,27 @@
+package roomescape;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import roomescape.Reservation;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller //웹 요청을 받는 담당자
+public class ViewController {
+
+    @GetMapping("/")
+    //"/"주소로 get을 요청하면 아래 메서드 실행
+    public String home() {
+        return "home";
+    }//화면 보여주기
+
+    @GetMapping("/reservation")
+    public String reservationPage() {
+        return "reservation";
+    }//예약관리 페이지를 보여줌
+
+}
