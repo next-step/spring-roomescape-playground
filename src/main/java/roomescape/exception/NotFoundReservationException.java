132b4925eed901ea7e265fdb2805678fc42d1687
mikeylili <a01073111145@gmail.com>
2026-05-09 02:06:00 +0900

    feat: NotFoundReservationException에서 메시지를 받는 메서드 삭제

--- src/main/java/roomescape/exception/NotFoundReservationException.java
+++ src/main/java/roomescape/exception/NotFoundReservationException.java
@@ -5,8 +5,4 @@ public class NotFoundReservationException extends RuntimeException {
     public NotFoundReservationException() {
         super("예약을 찾을 수 없습니다.");
     }
-
-    public NotFoundReservationException(String message) {
-        super(message);
-    }
 }
