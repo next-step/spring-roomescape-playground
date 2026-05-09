a2d582d201fc81c2a79688dafb544e5f960bde61
chaehyunL <chaehyun1012@gmail.com>
2026-05-09 14:05:20 +0900

    feat: 예외처리 로직 생성

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -6,11 +6,25 @@ public class Reservation {
     private final String date;
     private final String time;
 
-    public Reservation(Long id, String name, String date, String time) {
+    public
+    Reservation(Long id, String name, String date, String time) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
+        validateReservation();
+    }
+
+    private void validateReservation(){
+        if(name == null||name.isBlank()){
+            throw new InvalidReservationException("ERROR: 이름을 작성하여야 합니다.");
+        }
+        if(date == null||date.isBlank()){
+            throw new InvalidReservationException("ERROR: 날짜를 작성하여야 합니다.");
+        }
+        if(time == null||time.isBlank()){
+            throw new InvalidReservationException("ERROR: 이름을 작성하여야 합니다.");
+        }
     }
 
     public Long getId() {
