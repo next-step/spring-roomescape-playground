068151a8ff2b3eefdda74572b1845655cdf379e1
TheDeveloperKwon <akongwon@gmail.com>
2026-05-09 15:31:50 +0900

    chore: 경고 제거를 위한 @NoArgsConstructor 추가

--- src/main/java/roomescape/dto/request/ReservationCreateRequest.java
+++ src/main/java/roomescape/dto/request/ReservationCreateRequest.java
@@ -3,12 +3,14 @@ package roomescape.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import roomescape.domain.Reservation;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Getter
+@NoArgsConstructor
 public class ReservationCreateRequest {
     @NotBlank(message = "이름은 필수 입력값입니다.")
     private String name;
