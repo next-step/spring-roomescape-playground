9b9beafa9c48806be3a19a661380a9d5dd9aded0
mgim9316-a11y <mgim9316@gmail.com>
2026-05-10 21:22:37 +0900

    refactor: 애노테이션수정

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,13 +1,12 @@
 package roomescape;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
 
-@Controller
+@RestController
 public class ReservationController {
 
     private final ReservationService reservationService;
@@ -21,7 +20,6 @@ public class ReservationController {
         return "reservation";
     }
 
-    @ResponseBody
     @GetMapping("/reservations")
     public List<Reservation> getReservations() {
         return reservationService.getAllReservations();
