4bea8dd82a0edee98435353bc4cdd5ff0fcd28fb
mgim9316-a11y <mgim9316@gmail.com>
2026-05-10 21:45:33 +0900

    feat: dto 사용

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,12 +1,16 @@
-package roomescape;
+package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import roomescape.dto.ReservationRequest;
+import roomescape.service.ReservationService;
+import roomescape.domain.Reservation;
 
 import java.net.URI;
 import java.util.List;
 
-@RestController
+@Controller
 public class ReservationController {
 
     private final ReservationService reservationService;
@@ -15,19 +19,22 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
-    @GetMapping("/reservation")
-    public String reservationPage() {
-        return "reservation";
+    @GetMapping("/")
+    public String home() {
+        return "home";
     }
 
+
+
+    @ResponseBody
     @GetMapping("/reservations")
     public List<Reservation> getReservations() {
         return reservationService.getAllReservations();
     }
 
     @PostMapping("/reservations")
-    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
-        Reservation newReservation = reservationService.createReservation(reservation);
+    public ResponseEntity<Reservation> createReservation(@RequestBody ReservationRequest request) {
+        Reservation newReservation = reservationService.createReservation(request);
         return ResponseEntity
                 .created(URI.create("/reservations/" + newReservation.getId()))
                 .body(newReservation);
