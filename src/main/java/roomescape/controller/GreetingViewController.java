5d407f21aa80bacaff0e429967b2f5d950ad5627
mgim9316-a11y <mgim9316@gmail.com>
2026-05-10 21:54:45 +0900

    feat: viewController 생성

--- src/main/java/roomescape/controller/GreetingViewController.java
+++ src/main/java/roomescape/controller/GreetingViewController.java
@@ -1,10 +1,10 @@
-package roomescape.view;
+package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class GreetingView {
+public class GreetingViewController {
 
     @GetMapping("/")
     public String home() {
