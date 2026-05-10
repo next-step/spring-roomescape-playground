54910d309fc4a15c787de512ef7f25ba37b6aa9e
monsaumon <monsaumon@naver.com>
2026-05-10 22:50:43 +0900

    refactor: ReservationRequest의 날짜와 시간을 LocalDate, LocalTime으로 직접 받음

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -1,22 +1,26 @@
 package roomescape.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import roomescape.domain.Reservation;
 
-public record ReservationRequest(@NotBlank(message = "이름이 비어 있을 수 없습니다.") String name,
-                                 @NotBlank(message = "예약 날짜가 비어 있을 수 없습니다.") String date,
-                                 @NotBlank(message = "예약 시간이 비어 있을 수 없습니다.") String time) {
+public record ReservationRequest(
+        @NotBlank(message = "이름이 비어 있을 수 없습니다.")
+        String name,
+
+        @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd")
+        @NotNull(message = "예약 날짜가 비어 있을 수 없습니다.")
+        LocalDate date,
+
+        @JsonFormat(shape = Shape.STRING, pattern = "HH:mm")
+        @NotNull(message = "예약 시간이 비어 있을 수 없습니다.")
+        LocalTime time) {
     public Reservation toReservation(Long id) {
-        return new Reservation(id, name, toLocalDateTime(date, time));
-    }
-
-    private LocalDateTime toLocalDateTime(String date, String time) {
-        LocalDate localDate = LocalDate.parse(date);
-        LocalTime localTime = LocalTime.parse(time);
-
-        return LocalDateTime.of(localDate, localTime);
+        return new Reservation(id, name, LocalDateTime.of(date, time));
     }
 }
