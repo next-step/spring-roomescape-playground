bf17853985fd8b7468a2f4abb91c15373e311475
mgim9316-a11y <mgim9316@gmail.com>
2026-05-10 23:29:30 +0900

    feat: 검증 기능 추가

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -2,6 +2,7 @@ package roomescape.service;
 
 import org.springframework.stereotype.Service;
 import roomescape.domain.Reservation;
+import roomescape.domain.ReservationValidator;
 import roomescape.dto.ReservationRequest;
 
 import java.util.ArrayList;
@@ -13,12 +14,20 @@ public class ReservationService {
 
     private final List<Reservation> reservations = new ArrayList<>();
     private final AtomicLong index = new AtomicLong(0);
+    private final ReservationValidator validator;
+
+    public ReservationService(ReservationValidator validator) {
+        this.validator = validator;
+    }
 
     public List<Reservation> getAllReservations() {
         return reservations;
     }
 
     public Reservation createReservation(ReservationRequest request) {
+        validator.validateReservationTime(request);
+        validator.validateDuplicatedReservation(request, reservations);
+
         Reservation newReservation = new Reservation(
                 index.incrementAndGet(),
                 request.getName(),
@@ -30,6 +39,9 @@ public class ReservationService {
     }
 
     public void deleteReservation(Long id) {
-        reservations.removeIf(reservation -> reservation.getId().equals(id));
+        boolean isRemoved = reservations.removeIf(reservation -> reservation.getId().equals(id));
+        if (!isRemoved) {
+            throw new IllegalArgumentException("존재하지 않는 예약입니다.");
+        }
     }
 }
