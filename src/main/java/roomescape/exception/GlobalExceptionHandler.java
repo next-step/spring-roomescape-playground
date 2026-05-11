1a7b6a67c7e1ff14f1305aab222726fbf459844b
dahyn <k0d5h16@gmail.com>
2026-05-11 15:24:21 +0900

    refactor: 예외 응답 메시지 추가

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -3,12 +3,15 @@ package roomescape.exception;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import roomescape.dto.ErrorResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundReservationException.class)
-    public ResponseEntity<Void> handleNotFoundReservationException(NotFoundReservationException e) {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<ErrorResponse> handleNotFoundReservationException(NotFoundReservationException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse(e.getMessage()));
     }
 }
