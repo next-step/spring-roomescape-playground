b381b71ab8902d67d796fc8f4ae74fcd7bfd12c5
dahyn <k0d5h16@gmail.com>
2026-05-11 15:16:42 +0900

    refactor: 예외 핸들러 생성

---
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package roomescape.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundReservationException.class)
+    public ResponseEntity<Void> handleNotFoundReservationException(NotFoundReservationException e) {
+        return ResponseEntity.badRequest().build();
+    }
+}
