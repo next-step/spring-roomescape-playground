282ac21493af4ec81b0fd1e4361dee26e9f5821d
chaehyunL <chaehyun1012@gmail.com>
2026-05-12 22:20:26 +0900

    refector: 이름 Home->View로 변경

--- src/main/java/roomescape/controller/ViewController.java
+++ src/main/java/roomescape/controller/ViewController.java
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class ViewController {
 
     @GetMapping("/")
     public String home() {
         return "home";
     }
+
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
+    }
+
 
 }
