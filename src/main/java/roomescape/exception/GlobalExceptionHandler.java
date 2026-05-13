3601ae5bfe7790811c03e687ae2b3b027810ee8d
htdufhc-bob <htdufhc@gmail.com>
2026-05-13 10:22:55 +0900

    refactor: unchecked와 checked exception 모두 처리하도록 메서드 수정

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -38,8 +38,8 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(Exception e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResponse("서버 내부 오류가 발생했습니다."));
     }
