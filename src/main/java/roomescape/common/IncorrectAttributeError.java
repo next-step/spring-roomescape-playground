348adbc282ad60b960ba80077a49f5f09db15348
Cappucciyes <ltkproper@gmail.com>
2026-05-14 04:21:20 +0900

    refactor: updated IncorrectAttributeError

--- src/main/java/roomescape/common/IncorrectAttributeError.java
+++ src/main/java/roomescape/common/IncorrectAttributeError.java
@@ -1,24 +1,26 @@
 package roomescape.common;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class IncorrectAttributeError {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleIncorrectAttributes(MethodArgumentNotValidException exception) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleIncorrectAttributes(MethodArgumentNotValidException exception) {
         Map<String, String> errors = new HashMap<>();
 
         for (FieldError error : exception.getBindingResult().getFieldErrors()) {
             errors.put(error.getField(), error.getDefaultMessage());
         }
 
-        return ResponseEntity.badRequest().body(errors);
+        return errors;
     }
 }
