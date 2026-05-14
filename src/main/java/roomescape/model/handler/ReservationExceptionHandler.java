c2e95dafe089614ad7a2ed3445b2f24678a914b6
Cappucciyes <ltkproper@gmail.com>
2026-05-14 12:53:59 +0900

    refactor : Renamed DomainExceptionHandler.java into ReservationExceptionHandler.java

--- src/main/java/roomescape/model/handler/ReservationExceptionHandler.java
+++ src/main/java/roomescape/model/handler/ReservationExceptionHandler.java
@@ -1,18 +1,13 @@
 package roomescape.model.handler;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.model.errors.ReservationNotFoundException;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestControllerAdvice
-public class DomainExceptionHandler {
+public class ReservationExceptionHandler {
     @ExceptionHandler(ReservationNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleReservationNotFoundError(ReservationNotFoundException exception) {
