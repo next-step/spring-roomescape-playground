1db68cd761eaca22f647ed7303b0f900d9f5d24f
htdufhc-bob <htdufhc@gmail.com>
2026-05-14 20:48:04 +0900

    chore: 사용하지 않는 import문 제거

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,6 +1,5 @@
 package roomescape.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import roomescape.domain.Reservation;
 import roomescape.domain.Reservations;
