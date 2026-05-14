374568ffbbe623210a5e50c37357fed8e40bed1e
TheDeveloperKwon <akongwon@gmail.com>
2026-05-14 11:50:53 +0900

    refactor: 불필요한 검증 로직 제거

--- src/main/java/roomescape/dto/response/ReservationCreateResponse.java
+++ src/main/java/roomescape/dto/response/ReservationCreateResponse.java
@@ -1,7 +1,5 @@
 package roomescape.dto.response;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import roomescape.domain.Reservation;
@@ -12,16 +10,9 @@ import java.time.LocalTime;
 @Getter
 @Builder
 public class ReservationCreateResponse {
-    @NotNull(message = "아이디가 설정되지 않았습니다.")
     private Long id;
-
-    @NotBlank(message = "이름은 필수 입력값입니다.")
     private String name;
-
-    @NotNull(message = "날짜은 필수 입력값입니다.")
     private LocalDate date;
-
-    @NotNull(message = "시간은 필수 입력값입니다.")
     private LocalTime time;
 
     public static ReservationCreateResponse from(Reservation reservation) {
--- src/main/java/roomescape/dto/response/ReservationGetResponse.java
+++ src/main/java/roomescape/dto/response/ReservationGetResponse.java
@@ -1,7 +1,5 @@
 package roomescape.dto.response;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,15 +9,8 @@ import java.time.LocalTime;
 @Getter
 @Builder
 public class ReservationGetResponse {
-    @NotNull(message = "아이디가 설정되지 않았습니다.")
     private Long id;
-
-    @NotBlank(message = "이름은 필수 입력값입니다.")
     private String name;
-
-    @NotNull(message = "날짜은 필수 입력값입니다.")
     private LocalDate date;
-
-    @NotNull(message = "시간은 필수 입력값입니다.")
     private LocalTime time;
 }
