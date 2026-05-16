e5bbb7243a082dcef3abcbba5310a3914efac642
chaehyunL <chaehyun1012@gmail.com>
2026-05-16 10:51:17 +0900

    refector: validate 검증 제거

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,7 +1,5 @@
 package roomescape.domain;
 
-import roomescape.exception.InvalidReservationException;
-
 public class Reservation {
     private final Long id;
     private final String name;
@@ -13,19 +11,6 @@ public class Reservation {
         this.name = name;
         this.date = date;
         this.time = time;
-        validateReservation();
-    }
-
-    private void validateReservation() {
-        if (name == null || name.isBlank()) {
-            throw new InvalidReservationException("ERROR: 이름을 작성하여야 합니다.");
-        }
-        if (date == null || date.isBlank()) {
-            throw new InvalidReservationException("ERROR: 날짜를 작성하여야 합니다.");
-        }
-        if (time == null || time.isBlank()) {
-            throw new InvalidReservationException("ERROR: 이름을 작성하여야 합니다.");
-        }
     }
 
     public Long getId() {
