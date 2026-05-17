fac4701a27d507d5016a467dcd62eba01259004f
MacBook Pro <hojun5104@gmail.com>
2026-05-17 23:18:45 +0900

    feat: DB size를 반영하여 validation 코드 추가

--- src/main/java/roomescape/reservation/dto/ReservationRequest.java
+++ src/main/java/roomescape/reservation/dto/ReservationRequest.java
@@ -2,6 +2,7 @@ package roomescape.reservation.dto;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import jakarta.validation.constraints.NotBlank;
@@ -9,6 +10,7 @@ import roomescape.reservation.domain.Reservation;
 
 public record ReservationRequest(
     @NotBlank(message = "이름은 비어 있을 수 없습니다.")
+    @Size(max = 255, message = "이름은 255자를 넘을 수 없습니다.")
     String name,
 
     @NotNull(message = "날짜는 필수입니다.")
