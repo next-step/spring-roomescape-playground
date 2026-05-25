ee3495660c2f452793a9a8d5080f673c9c4f0f5a
htdufhc-bob <htdufhc@gmail.com>
2026-05-25 16:45:46 +0900

    feat: 6, 7단계 구현

---
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -0,0 +1,69 @@
+package roomescape.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.domain.Reservation;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ReservationRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public List<Reservation> findAllReservation() {
+        return jdbcTemplate.query("SELECT * FROM reservation", reservationRowMapper());
+    }
+
+    public Optional<Reservation> findReservationWithId(Long id) {
+        return jdbcTemplate.query("SELECT * FROM reservation WHERE id = ?", reservationRowMapper(), id)
+                .stream().findFirst();
+    }
+
+    public Reservation insertReservation(Reservation reservation) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    "INSERT INTO reservation(name, date, time) VALUES (?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, reservation.getName());
+            ps.setString(2, reservation.getDate().toString());
+            ps.setString(3, reservation.getTime().toString());
+
+            return ps;
+        }, keyHolder);
+
+        Number generatedKey = keyHolder.getKey();
+        if (generatedKey == null) {
+            throw new IllegalArgumentException("");
+        }
+        Long key = generatedKey.longValue();
+
+        return Reservation.withId(reservation, key);
+    }
+
+    public void deleteReservation(Reservation reservation) {
+        jdbcTemplate.update("DELETE FROM reservation WHERE id = ?", reservation.getId());
+    }
+
+    private RowMapper<Reservation> reservationRowMapper() {
+        return (resultSet, rowNum) -> Reservation.of(
+                resultSet.getString("name"),
+                LocalDate.parse(resultSet.getString("date")),
+                LocalTime.parse(resultSet.getString("time"))
+        );
+    }
+}
--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -2,34 +2,37 @@ package roomescape.service;
 
 import org.springframework.stereotype.Service;
 import roomescape.domain.Reservation;
-import roomescape.domain.Reservations;
 import roomescape.dto.ReservationRequest;
 import roomescape.dto.ReservationResponse;
+import roomescape.exception.ReservationNotFoundException;
+import roomescape.repository.ReservationRepository;
 
 import java.util.List;
 
 @Service
 public class ReservationService {
 
-    private final Reservations reservations;
+    private final ReservationRepository reservationRepository;
 
-    public ReservationService(Reservations reservations) {
-        this.reservations = reservations;
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
     }
 
     public List<ReservationResponse> getAllReservations() {
-        return reservations.getReservations().stream()
+        return reservationRepository.findAllReservation().stream()
                 .map(ReservationResponse::from)
                 .toList();
     }
 
     public ReservationResponse createReservation(ReservationRequest request) {
         Reservation validatedReservation = Reservation.of(request.name(), request.date(), request.time());
-        Reservation savedReservation = reservations.addReservation(validatedReservation);
-        return ReservationResponse.from(savedReservation);
+        Reservation reservation = reservationRepository.insertReservation(validatedReservation);
+        return ReservationResponse.from(reservation);
     }
 
     public void deleteReservation(Long id) {
-        reservations.deleteReservationById(id);
+        Reservation reservation = reservationRepository.findReservationWithId(id)
+                        .orElseThrow(() -> new ReservationNotFoundException(id));
+        reservationRepository.deleteReservation(reservation);
     }
 }
