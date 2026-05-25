cc55c26752a69014b8adf531aedcfc34361cbf68
dahyn <k0d5h16@gmail.com>
2026-05-25 17:01:35 +0900

    feat: 7단계 예약 추가 및 삭제 DB 적용

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -1,9 +1,14 @@
 package roomescape.repository;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import roomescape.dto.ReservationRequest;
+import roomescape.exception.NotFoundReservationException;
 import roomescape.model.Reservation;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
 @Repository
@@ -32,5 +37,47 @@ public class ReservationRepository {
                                 resultSet.getString("time")
                         )
         );
+    }
+
+    public Reservation save(ReservationRequest request) {
+
+        String sql = """
+                INSERT INTO reservation(name, date, time)
+                VALUES (?, ?, ?)
+                """;
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement statement = connection.prepareStatement(sql, new String[]{"id"});
+            statement.setString(1, request.name());
+            statement.setString(2, request.date());
+            statement.setString(3, request.time());
+
+            return statement;
+        }, keyHolder);
+
+        Long id = keyHolder.getKey().longValue();
+
+        return new Reservation(
+                id,
+                request.name(),
+                request.date(),
+                request.time()
+        );
+    }
+
+    public void delete(Long id) {
+
+        String sql = """
+                DELETE FROM reservation
+                WHERE id = ?
+                """;
+
+        int affectedRows = jdbcTemplate.update(sql, id);
+
+        if (affectedRows == 0) {
+            throw new NotFoundReservationException();
+        }
     }
 }
--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -2,18 +2,13 @@ package roomescape.service;
 
 import org.springframework.stereotype.Service;
 import roomescape.dto.ReservationRequest;
-import roomescape.exception.NotFoundReservationException;
 import roomescape.model.Reservation;
 import roomescape.repository.ReservationRepository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class ReservationService {
-    private final List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong index = new AtomicLong(1);
     private final ReservationRepository reservationRepository;
 
     public ReservationService(ReservationRepository reservationRepository) {
@@ -25,24 +20,10 @@ public class ReservationService {
     }
 
     public Reservation createReservation(ReservationRequest reservationRequest) {
-        long id = index.getAndIncrement();
-
-        Reservation reservation = new Reservation(
-                id,
-                reservationRequest.name(),
-                reservationRequest.date(),
-                reservationRequest.time()
-        );
-
-        reservations.add(reservation);
-        return reservation;
+        return reservationRepository.save(reservationRequest);
     }
 
     public void deleteReservation(Long id) {
-        boolean removed = reservations.removeIf(reservation -> reservation.id().equals(id));
-
-        if (!removed) {
-            throw new NotFoundReservationException();
-        }
+        reservationRepository.delete(id);
     }
 }
