09ce71f2bd343c75b4b74f3566553a4712a9b8b1
chanh <leechan7120@skku.edu>
2026-05-26 11:07:00 +0900

    feat : validtime db 추가

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -19,6 +19,13 @@ public class RoomescapeApplication implements CommandLineRunner {
     public void run(String... strings) throws Exception {
         jdbcTemplate.execute("DROP TABLE Times IF EXISTS");
         jdbcTemplate.execute("DROP TABLE Reservations IF EXISTS");
+        jdbcTemplate.execute("DROP TABLE ValidTimes IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE ValidTimes(time TIME NOT NULL)");
+        jdbcTemplate.execute(
+                "INSERT INTO ValidTimes(time) " +
+                        "SELECT PARSEDATETIME(X || ':00:00', 'H:mm:ss') " +
+                        "FROM SYSTEM_RANGE(8, 22)"
+        );
         jdbcTemplate.execute(
                 "CREATE TABLE Reservations(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, date DATE NOT NULL)");
         jdbcTemplate.execute(
