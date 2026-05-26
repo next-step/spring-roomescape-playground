b33ca30d0bbb8f2a3216f290cf40d968e65a41a4
PARK JUNSEONG <jun0829park@gmail.com>
2026-05-27 00:23:11 +0900

    refactor: Reservation Service/Controller에 DTO 적용 및 Time 도메인 연동 로직 추가

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -5,14 +5,10 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/reservations")
 public class ReservationController {
 
     private static final Logger log = LoggerFactory.getLogger(ReservationController.class);
@@ -22,23 +18,22 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
-    @PostMapping("/reservations")
-    public ResponseEntity<Reservation> create(@RequestBody Reservation reservation) {
-        log.info("예약 생성 요청: name={}, date={}, time={}", reservation.getName(), reservation.getDate(), reservation.getTime());
-        Reservation newReservation = reservationService.createReservation(reservation);
-        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
+    @PostMapping
+    public ResponseEntity<Reservation.Response> create(@RequestBody Reservation.Request request) {
+        log.info("예약 생성 요청: name={}, date={}, timeId={}", request.getName(), request.getDate(), request.getTimeId());
+
+        Reservation.Response response = reservationService.createReservation(request);
+        return ResponseEntity.created(URI.create("/reservations/" + response.getId())).body(response);
     }
 
-    @GetMapping("/reservations")
+    @GetMapping
     public ResponseEntity<List<Reservation.Response>> readAll() {
         log.info("전체 예약 조회 요청");
-        List<Reservation.Response> responseList = reservationService.findAllReservations().stream()
-                .map(Reservation.Response::new)
-                .toList();
+        List<Reservation.Response> responseList = reservationService.findAllReservations();
         return ResponseEntity.ok(responseList);
     }
 
-    @DeleteMapping("/reservations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         log.info("예약 삭제 요청: id={}", id);
         reservationService.deleteReservation(id);
--- src/main/java/roomescape/ReservationService.java
+++ src/main/java/roomescape/ReservationService.java
@@ -1,40 +1,55 @@
 package roomescape;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import roomescape.exception.DuplicateReservationException;
+import roomescape.exception.InvalidReservationException;
 import roomescape.exception.NotFoundReservationException;
+import roomescape.exception.NotFoundTimeException;
 
 @Service
 public class ReservationService {
 
     private final ReservationDao reservationDao;
+    private final TimeDao timeDao;
 
-    public ReservationService(ReservationDao reservationDao) {
+    public ReservationService(ReservationDao reservationDao, TimeDao timeDao) {
         this.reservationDao = reservationDao;
+        this.timeDao = timeDao;
     }
 
-    public Reservation createReservation(Reservation reservation) {
-        LocalDate date = reservation.getDate();
-        LocalTime time = reservation.getTime();
-
-        if (reservationDao.existsByDateAndTime(date, time)) {
-            throw new DuplicateReservationException(date.toString(), time.toString());
+    public Reservation.Response createReservation(Reservation.Request request) {
+        if (request.getName() == null || request.getDate() == null || request.getTimeId() == null) {
+            throw new InvalidReservationException("필수 예약 정보가 누락되었습니다.");
+        }
+        if (request.getDate().isBefore(LocalDate.now())) {
+            throw new InvalidReservationException("과거 날짜로는 예약할 수 없습니다.");
         }
 
+        Time time = timeDao.findById(request.getTimeId());
+        if (time == null) {
+            throw new NotFoundTimeException(request.getTimeId());
+        }
+
+        if (reservationDao.existsByDateAndTimeId(request.getDate(), time.getId())) {
+            throw new DuplicateReservationException(request.getDate().toString(), time.getTime().toString());
+        }
+
+        Reservation reservation = new Reservation(null, request.getName(), request.getDate(), time);
         Long generatedId = reservationDao.insert(reservation);
-        return Reservation.toEntity(reservation, generatedId);
+
+        return new Reservation.Response(Reservation.toEntity(reservation, generatedId));
     }
 
-    public List<Reservation> findAllReservations() {
-        return reservationDao.findAll();
+    public List<Reservation.Response> findAllReservations() {
+        return reservationDao.findAll().stream()
+                .map(Reservation.Response::new)
+                .toList();
     }
 
     public void deleteReservation(Long id) {
         int updatedRows = reservationDao.deleteById(id);
-
         if (updatedRows == 0) {
             throw new NotFoundReservationException(id);
         }
