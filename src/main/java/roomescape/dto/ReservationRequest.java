1f79846e61327bb718cb677d71ef62e8d7cfbebf
chaehyunL <chaehyun1012@gmail.com>
2026-05-27 13:29:33 +0900

    refector: @NotBlank 이용

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -1,26 +1,14 @@
 package roomescape.dto;
 
-import roomescape.exception.InvalidReservationException;
+import jakarta.validation.constraints.NotBlank;
 
 public record ReservationRequest(
         Long id,
+        @NotBlank(message = "ERROR:이름을 입력하시오.")
         String name,
+        @NotBlank(message = "ERROR:날짜를 입력하시오.")
         String date,
+        @NotBlank(message = "ERROR:시간을 입력하시오.")
         String time
 ) {
-    public ReservationRequest {
-        validateReservationRequest(name, date, time);
-    }
-
-    private void validateReservationRequest(String name, String date, String time) {
-        if (name == null || name.isBlank()) {
-            throw new InvalidReservationException("ERROR: 이름을 작성하여야 합니다.");
-        }
-        if (date == null || date.isBlank()) {
-            throw new InvalidReservationException("ERROR: 날짜를 작성하여야 합니다.");
-        }
-        if (time == null || time.isBlank()) {
-            throw new InvalidReservationException("ERROR: 시간을 작성하여야 합니다.");
-        }
-    }
 }
