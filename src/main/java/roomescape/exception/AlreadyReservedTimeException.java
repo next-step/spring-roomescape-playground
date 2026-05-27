8257d8c61c2cc43565be456ff14a755a66e74b9e
chaehyunL <chaehyun1012@gmail.com>
2026-05-27 13:44:55 +0900

    refector: AlreadyReservedTimeException.java->UnableReservationtime.java로 변경

--- src/main/java/roomescape/exception/AlreadyReservedTimeException.java
+++
@@ -1,8 +0,0 @@
-package roomescape.exception;
-
-public class AlreadyReservedTimeException extends RuntimeException {
-    public AlreadyReservedTimeException() {
-        super("이미 예약된 시간입니다.");
-    }
-}
-
---
+++ src/main/java/roomescape/exception/UnableReservationTimeException.java
@@ -0,0 +1,8 @@
+package roomescape.exception;
+
+public class UnableReservationTimeException extends RuntimeException {
+    public UnableReservationTimeException(String message) {
+        super(message);
+    }
+}
+
