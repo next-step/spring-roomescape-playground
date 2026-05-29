bdcbc8d4fbe69c6bf9fa37c7b548fea1fd79664d
Kdahyn <k0d5h16@gmail.com>
2026-05-29 21:11:13 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

---
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -0,0 +1,85 @@
+package roomescape.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.model.Reservation;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.Time;
+import java.util.List;
+
+@Repository
+public class ReservationRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Reservation> findAll() {
+
+        String sql = """
+                SELECT id, name, date, time
+                FROM reservation
+                """;
+
+        return jdbcTemplate.query(
+                sql,
+                (resultSet, rowNum) ->
+                        new Reservation(
+                                resultSet.getLong("id"),
+                                resultSet.getString("name"),
+                                resultSet.getDate("date").toLocalDate(),
+                                resultSet.getTime("time").toLocalTime()
+                        )
+        );
+    }
+
+    public Reservation save(Reservation reservation) {
+
+        String sql = """
+                INSERT INTO reservation(name, date, time)
+                VALUES (?, ?, ?)
+                """;
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement statement = connection.prepareStatement(sql, new String[]{"id"});
+            statement.setString(1, reservation.name());
+            statement.setDate(2, Date.valueOf((reservation.date())));
+            statement.setTime(3, Time.valueOf(reservation.time()));
+
+            return statement;
+        }, keyHolder);
+
+        Number key = keyHolder.getKey();
+
+        if (key == null) {
+            throw new IllegalStateException("생성된 ID를 가져올 수 없습니다.");
+        }
+
+        Long id = key.longValue();
+
+        return new Reservation(
+                id,
+                reservation.name(),
+                reservation.date(),
+                reservation.time()
+        );
+    }
+
+    public int delete(Long id) {
+
+        String sql = """
+                DELETE FROM reservation
+                WHERE id = ?
+                """;
+
+        return jdbcTemplate.update(sql, id);
+    }
+}
--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -4,38 +4,38 @@ import org.springframework.stereotype.Service;
 import roomescape.dto.ReservationRequest;
 import roomescape.exception.NotFoundReservationException;
 import roomescape.model.Reservation;
+import roomescape.repository.ReservationRepository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class ReservationService {
-    private final List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong index = new AtomicLong(1);
+    private final ReservationRepository reservationRepository;
+
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
 
     public List<Reservation> findReservations() {
-        return reservations;
+        return reservationRepository.findAll();
     }
 
     public Reservation createReservation(ReservationRequest reservationRequest) {
-        long id = index.getAndIncrement();
+        Reservation reservation =
+                new Reservation(
+                        null,
+                        reservationRequest.name(),
+                        reservationRequest.date(),
+                        reservationRequest.time()
+                );
 
-        Reservation reservation = new Reservation(
-                id,
-                reservationRequest.name(),
-                reservationRequest.date(),
-                reservationRequest.time()
-        );
-
-        reservations.add(reservation);
-        return reservation;
+        return reservationRepository.save(reservation);
     }
 
     public void deleteReservation(Long id) {
-        boolean removed = reservations.removeIf(reservation -> reservation.id().equals(id));
+        int affectedRows = reservationRepository.delete(id);
 
-        if (!removed) {
+        if (affectedRows == 0) {
             throw new NotFoundReservationException();
         }
     }
