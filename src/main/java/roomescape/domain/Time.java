1e55bcef2c370a299ffabcba1bdab7009871da19
TheDeveloperKwon <akongwon@gmail.com>
2026-05-31 16:21:20 +0900

    refactor: time 도메인 검증 로직 적용

--- src/main/java/roomescape/domain/Time.java
+++ src/main/java/roomescape/domain/Time.java
@@ -11,12 +11,12 @@ public class Time {
     private final LocalTime time;
 
     public Time(Long id, LocalTime time) {
+        validate(id, time);
         this.id = id;
         this.time = time;
     }
 
-    private void validate(String name, LocalTime time) {
-        Objects.requireNonNull(name, "name은 필수입니다.");
+    private void validate(Long id, LocalTime time) {
         Objects.requireNonNull(time, "time은 필수입니다.");
     }
 }
