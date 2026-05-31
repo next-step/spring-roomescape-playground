108a6a6a2ecf0649f76c0eba730854543f73f5a8
TheDeveloperKwon <akongwon@gmail.com>
2026-05-31 17:31:27 +0900

    refactor: 중복 예약에 대한 처리 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE time
 (
     id   BIGINT NOT NULL AUTO_INCREMENT,
-    time TIME NOT NULL,
+    time TIME   NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (time)
 );
@@ -11,7 +11,8 @@ CREATE TABLE reservation
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
     date    DATE         NOT NULL,
-    time_id BIGINT,
+    time_id BIGINT       NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES time (id)
+    FOREIGN KEY (time_id) REFERENCES time (id),
+    UNIQUE (date, time_id)
 );
