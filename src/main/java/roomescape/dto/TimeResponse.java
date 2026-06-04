33b1525446699b648616005c953b26d9b96eda9f
pce0303 <ivychoen@gmail.com>
2026-06-04 12:51:23 +0900

    fix: time format 수정

--- src/main/java/roomescape/dto/TimeResponse.java
+++ src/main/java/roomescape/dto/TimeResponse.java
@@ -1,10 +1,13 @@
 package roomescape.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 import roomescape.domain.Time;
 
 public record TimeResponse(
         Long id,
+
+        @JsonFormat(pattern = "HH:mm")
         LocalTime time
 ) {
 
