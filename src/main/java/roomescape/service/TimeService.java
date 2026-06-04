a41db31b9db99baab864768fa80fe2d95eb02438
pce0303 <ivychoen@gmail.com>
2026-06-04 12:53:50 +0900

    fix: 예약존재시간 삭제 예외 처리

--- src/main/java/roomescape/service/TimeService.java
+++ src/main/java/roomescape/service/TimeService.java
@@ -2,6 +2,7 @@ package roomescape.service;
 
 import java.time.LocalTime;
 import java.util.List;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import roomescape.domain.Time;
 import roomescape.exception.BadRequestException;
@@ -25,10 +26,14 @@ public class TimeService {
     }
 
     public void delete(Long id) {
-        boolean deleted = timeRepository.deleteById(id);
+        try {
+            boolean deleted = timeRepository.deleteById(id);
 
-        if (!deleted) {
-            throw new BadRequestException("시간번호가 " + id + "인 시간은 존재하지 않습니다.");
+            if (!deleted) {
+                throw new BadRequestException("시간번호가 " + id + "인 시간은 존재하지 않습니다.");
+            }
+        } catch (DataIntegrityViolationException e) {
+            throw new BadRequestException("예약이 존재하는 시간은 삭제할 수 없습니다.");
         }
     }
 }
