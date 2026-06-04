f7a18295390944bf10efef9d865ab526b5e96459
pce0303 <ivychoen@gmail.com>
2026-06-04 12:51:15 +0900

    fix: time 표시되지 않는 문제 수정

--- src/main/java/roomescape/dto/ReservationResponse.java
+++ src/main/java/roomescape/dto/ReservationResponse.java
@@ -1,21 +1,20 @@
 package roomescape.dto;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import roomescape.domain.Reservation;
 
 public record ReservationResponse(
         Long id,
         String name,
         LocalDate date,
-        LocalTime time
+        TimeResponse time
 ) {
     public static ReservationResponse from(Reservation reservation) {
         return new ReservationResponse(
                 reservation.getId(),
                 reservation.getName(),
                 reservation.getDate(),
-                TimeResponse.from(reservation.getTime()).time()
+                TimeResponse.from(reservation.getTime())
         );
     }
 }
