af75120521bf9d499574d82295004e683bf8394a
chaehyunL <chaehyun1012@gmail.com>
2026-06-20 14:09:19 +0900

    feat:timeservice 생성

---
+++ src/main/java/roomescape/service/TimeService.java
@@ -0,0 +1,41 @@
+package roomescape.service;
+
+import org.springframework.stereotype.Service;
+import roomescape.domain.Reservation;
+import roomescape.domain.Time;
+import roomescape.dto.ReservationRequest;
+import roomescape.dto.TimeRequest;
+import roomescape.exception.NotFoundReservationException;
+import roomescape.repository.ReservationRepository;
+import roomescape.repository.TimeRepository;
+
+import java.util.List;
+
+@Service
+public class TimeService {
+    private final TimeRepository timeRepository;
+
+    public TimeService(TimeRepository timeRepository) {
+        this.timeRepository = timeRepository;
+    }
+
+    public List<Time> findAll(){
+        return timeRepository.findAll();
+    }
+
+    public Time create(TimeRequest timeRequest){
+        long id = timeRepository.insert(timeRequest);
+        return new Time(
+                id,
+                timeRequest.time()
+        );
+    }
+
+    public void delete(Long id){
+        boolean isRemoved =timeRepository.delete(id);
+
+        if(!isRemoved){
+            throw new NotFoundReservationException("time not found");
+        }
+    }
+}
