94ea4d2eee54f023c98f773bc8ffd868ccde803c
mgim9316-a11y <mgim9316@gmail.com>
2026-06-22 15:11:02 +0900

    refactor: time 객체 분리

---
+++ src/main/java/roomescape/domain/Time.java
@@ -0,0 +1,14 @@
+package roomescape.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Time {
+
+    private Long id;
+    private String time;
+}
