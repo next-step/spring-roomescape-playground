72d831fad25206f49f68fd2a344f898a8b3a1f83
haeun92e0 <gracekim8729@gmail.com>
2026-06-22 16:40:13 +0900

    Feat: 시간 정보를 관리하는 Time 도메인 클래스 추가

---
+++ src/main/java/roomescape/domain/Time.java
@@ -0,0 +1,18 @@
+package roomescape.domain;
+
+import java.time.LocalTime;
+
+public class Time {
+    private Long id;
+    private LocalTime time;
+
+    public Time() {}
+
+    public Time(Long id, LocalTime time) {
+        this.id = id;
+        this.time = time;
+    }
+
+    public Long getId() { return id; }
+    public LocalTime getTime() { return time; }
+}
