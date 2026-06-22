f229435bf6808fe6a68114b280043338be7e7776
htdufhc-bob <htdufhc@gmail.com>
2026-06-22 15:20:03 +0900

    refactor: 예약 요청 및 시간 등록 요청 검증 메시지 개선

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -4,13 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record ReservationRequest(
-        @NotBlank(message = "이름은 비어있을 수 없습니다.")
+        @NotBlank(message = "예약자 이름은 비어있을 수 없습니다.")
         String name,
 
-        @NotBlank(message = "날짜를 입력해주세요.")
+        @NotBlank(message = "예약 날짜를 입력해주세요.")
         String date,
 
-        @NotNull(message = "Reservation 시간을 입력해주세요.")
+        @NotNull(message = "예약 시간을 입력해주세요.")
         Long time
 ) {
 }
--- src/main/java/roomescape/dto/TimeRequest.java
+++ src/main/java/roomescape/dto/TimeRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 
 public record TimeRequest(
-        @NotNull(message = "시간을 입력해주세요.")
+        @NotNull(message = "등록할 시간을 입력해주세요.")
         LocalTime time
 ) {
 }
