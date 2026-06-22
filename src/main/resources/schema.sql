28ea2f34092e1e7ed3cbf494a0abcee799e6cedf
haeun92e0 <gracekim8729@gmail.com>
2026-06-22 16:40:53 +0900

    Chore: time 테이블 생성 및 reservation 테이블 외래키(time_id) 제약조건 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,19 @@
 DROP TABLE IF EXISTS reservation;
-//기존에 reservation 테이블이 있으면 삭제
+DROP TABLE IF EXISTS time;
+
+CREATE TABLE time
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    time VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE reservation
 (
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
-    date    DATE NOT NULL,
-    time    TIME NOT NULL,
-    PRIMARY KEY (id) //id를 기본키로 지정
+    date    VARCHAR(255) NOT NULL,
+    time_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES time(id)
 );
