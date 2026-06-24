eb6e23992dca9177715ff6037e46b38c07fe0c98
haeun92e0 <gracekim8729@gmail.com>
2026-06-24 22:40:29 +0900

    Chore: 데이터 무결성 보장을 위해 schema.sql 내 날짜 및 시간 컬럼 타입을 DATE, TIME으로 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS time;
 CREATE TABLE time
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT, //id 컬럼
-    time VARCHAR(255) NOT NULL,
+    time TIME         NOT NULL,
     PRIMARY KEY (id) //id를 primary key로 사용
 );
 //예약 테이블이 시간 테이블을 참조하도록
@@ -12,7 +12,7 @@ CREATE TABLE reservation
 (
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
-    date    VARCHAR(255) NOT NULL,
+    date    DATE         NOT NULL,
     time_id BIGINT, //time 테이블의 id를 저장
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES time(id)
