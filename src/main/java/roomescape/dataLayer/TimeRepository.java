9ea066f78bd21dd87e533817a2e78e5bb9cbab30
이태규 <58386574+Cappucciyes@users.noreply.github.com>
2026-06-25 06:24:45 +0900

    fix : fixed wrong error type

--- src/main/java/roomescape/dataLayer/TimeRepository.java
+++ src/main/java/roomescape/dataLayer/TimeRepository.java
@@ -33,7 +33,7 @@ public class TimeRepository {
         List<Time> possibleTime = jdbcTemplate.query("SELECT * FROM time WHERE id = ?", this::extractTimeFromResultSet, id);
 
         if (possibleTime.isEmpty()) {
-            throw new IllegalArgumentException("존재하지 않는 시간입니다");
+            throw new TimeNotFoundException("존재하지 않는 시간입니다");
         }
 
         return possibleTime.get(0);
