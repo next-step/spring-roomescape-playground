78a58bf5e42a546edb837461a5179196a26e189c
htdufhc-bob <htdufhc@gmail.com>
2026-06-26 10:24:00 +0900

    refactor: 사용하지 않은 필드 제거

--- src/main/java/roomescape/controller/TimeViewController.java
+++ src/main/java/roomescape/controller/TimeViewController.java
@@ -2,16 +2,9 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import roomescape.service.TimeService;
 
 @Controller
 public class TimeViewController {
-
-    private final TimeService timeService;
-
-    public TimeViewController(TimeService timeService) {
-        this.timeService = timeService;
-    }
 
     @GetMapping("/time")
     public String timePage() {
