31ac03d1bedff0c2bafbe893022109b00087f185
haeun92e0 <gracekim8729@gmail.com>
2026-06-29 17:21:37 +0900

    Chore: member 테이블 추가 및 초기 데이터 스크립트 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,30 +1,31 @@
 DROP TABLE IF EXISTS reservation;
-<<<<<<< HEAD
 DROP TABLE IF EXISTS time;
 
-CREATE TABLE time
-(
-    id   BIGINT       NOT NULL AUTO_INCREMENT, //id 컬럼
-    time TIME         NOT NULL,
-    PRIMARY KEY (id) //id를 primary key로 사용
+CREATE TABLE time (
+                      id BIGINT NOT NULL AUTO_INCREMENT,
+                      time VARCHAR(255) NOT NULL,
+                      PRIMARY KEY (id)
 );
-//예약 테이블이 시간 테이블을 참조하도록
-=======
-//기존에 reservation 테이블이 있으면 삭제
 
->>>>>>> next-step/haeun92e0
-CREATE TABLE reservation
-(
-    id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255) NOT NULL,
-<<<<<<< HEAD
-    date    DATE         NOT NULL,
-    time_id BIGINT, //time 테이블의 id를 저장
-    PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES time(id)
-=======
-    date    DATE NOT NULL,
-    time    TIME NOT NULL,
-    PRIMARY KEY (id) //id를 기본키로 지정
->>>>>>> next-step/haeun92e0
+CREATE TABLE reservation (
+                             id BIGINT NOT NULL AUTO_INCREMENT,
+                             name VARCHAR(255) NOT NULL,
+                             date VARCHAR(255) NOT NULL,
+                             time_id BIGINT,
+                             PRIMARY KEY (id),
+                             FOREIGN KEY (time_id) REFERENCES time(id)
 );
+
+CREATE TABLE member
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role     VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+INSERT INTO member (name, email, password, role)
+VALUES ('어드민', 'admin@email.com', 'password', 'ADMIN');
