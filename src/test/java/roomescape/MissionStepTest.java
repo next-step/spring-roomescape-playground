5672670a2552a3ff51c68a7c0c53ced7818b6159
KDYoung5606 <2271323@hansung.ac.kr>
2026-08-05 20:44:52 +0900

    test(reservation): /reservation, /reservations 응답 테스트 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,5 +1,7 @@
 package roomescape;
 
+import static org.hamcrest.CoreMatchers.is;
+
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,11 +13,30 @@ import org.springframework.test.annotation.DirtiesContext;
 public class MissionStepTest {
 
     @Test
-    @DisplayName("[테스트코드] : localhost:8080을 요청했을 때, home 화면이 나오는지 테스트")
-    void test_실행_후_home_화면으로_전환() {
+    @DisplayName("localhost:8080 요청 시 home 화면을 반환하는지 테스트")
+    void test_home_요청시_home_화면을_반환하는지_테스트() {
         RestAssured.given().log().all()
                 .when().get("/")
                 .then().log().all()
                 .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("/reservation 요청 시 reservation 화면을 반환하는지 테스트")
+    void test_reservation_요청시_reservation_화면을_반환하는지_테스트() {
+        RestAssured.given().log().all()
+            .when().get("/reservation")
+            .then().log().all()
+            .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("/reservations 요청 시 저장된 예약 3건을 반환하는지 테스트")
+    void test_reservations_요청시_저장된_예약_3건을_반환하는지_테스트() {
+        RestAssured.given().log().all()
+            .when().get("/reservations")
+            .then().log().all()
+            .statusCode(200)
+            .body("size()", is(3));
     }
 }
