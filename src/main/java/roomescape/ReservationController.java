e020039494be229bce47d7b2be3328deabb9584d
KDYoung5606 <2271323@hansung.ac.kr>
2026-08-05 20:41:11 +0900

    feat(reservation): 예약 페이지 및 목록 조회 요청 처리 메서드 추가

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,12 +1,26 @@
 package roomescape;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ReservationController {
-  @GetMapping("/")
-  public String home(){
-    return "home";
-  }
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> readReservations() {
+        return ResponseEntity.ok(new Reservations().getReservations());
+    }
 }
