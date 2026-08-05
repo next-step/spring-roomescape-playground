f01d15a5c73df96ad5e4dd9a4c773e1b84341f45
KDYoung5606 <2271323@hansung.ac.kr>
2026-08-05 20:40:34 +0900

    feat(reservation): 예약 도메인 객체 Reservation, Reservations 추가

---
+++ src/main/java/roomescape/Reservations.java
@@ -0,0 +1,26 @@
+package roomescape;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Reservations {
+    private final List<Reservation> reservations;
+
+    public Reservations() {
+        this.reservations = new ArrayList<>();
+        createReservations();
+    }
+
+    public List<Reservation> getReservations() {
+        return Collections.unmodifiableList(reservations);
+    }
+
+    private void createReservations() {
+        reservations.add(new Reservation(1L, "브라운", LocalDate.of(2026, 1, 1), LocalTime.of(10, 0)));
+        reservations.add(new Reservation(2L, "브라운", LocalDate.of(2026, 2, 1), LocalTime.of(11, 0)));
+        reservations.add(new Reservation(3L, "브라운", LocalDate.of(2026, 3, 1), LocalTime.of(12, 0)));
+    }
+}
