87191c1b4d50b43dab228b5943b19b522bbc1953
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-08-09 19:29:17 +0900

    test: Entity의 수정 메소드가 기대하는 바와 같이 작동했는지 검증하는 메소드 작성

---
+++ src/test/java/cholog/entity/MemberTest.java
@@ -0,0 +1,44 @@
+package cholog.entity;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemberTest {
+
+    private AtomicLong index;
+
+    @BeforeEach
+    void setUp() {
+        index = new AtomicLong(0);
+    }
+
+    @AfterEach
+    void tearDown() {
+        index = null;
+    }
+
+    @Test
+    @DisplayName("수정 이후에도 논리적으로 같은 객체인지 비교")
+    public void updateAndCompareMember() {
+        // given
+        Member before = createMember("Alice", 20);
+        Member after = new Member("Bob", 30);
+
+        // when
+        Member updated = before.update(after);
+
+        // then
+        assertThat(before).isEqualTo(updated);
+
+    }
+
+    private Member createMember(String name, Integer age) {
+        return Member.toEntityWithId(index.incrementAndGet(), new Member(name, age));
+    }
+}
