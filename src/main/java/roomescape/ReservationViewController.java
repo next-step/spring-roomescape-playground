b12e198570e3dd3b87e5c86977cc5ffdda6ceff9
JisuHan <ivyjisu@gmail.com>
2026-08-12 17:12:08 +0900

    feat: 예약 관리 페이지 및 예약 목록 조회 API 구현

--- src/main/java/roomescape/ReservationViewController.java
+++ src/main/java/roomescape/ReservationViewController.java
@@ -1,13 +1,37 @@
 package roomescape;
 
+import io.micrometer.observation.transport.ResponseContext;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ReservationViewController {
+    private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
 
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> reservations() {
+        reservations.add(new Reservation(1L, "브라운", "2023-01-01", "10:00"));
+        reservations.add(new Reservation(2L, "브라운", "2023-01-02", "11:00"));
+        reservations.add(new Reservation(3L, "브라운", "2023-01-03", "12:00"));
+
+        return reservations;
     }
 }
