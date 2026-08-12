92ac15966410e6cffe983d45a82a66e4e5d92f8c
장서인 <seoin1893@dankook.ac.kr>
2026-08-12 21:11:29 +0900

    feat (ReservationController) : 2단계 - ReservationController 클래스에 reservations 메서드 추가

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -2,19 +2,28 @@ package roomescape;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReservationController {
 
+    public ReservationController() {
+        reservations.add(new Reservation(1, "브라운", "2023-01-01", "10:00"));
+        reservations.add(new Reservation(2, "브라운", "2023-01-02", "11:00"));
+    }
     @GetMapping("/reservation")
     public String reservations() {
         return "reservation";
     }
 
-    private List<Reservation> reservations = new ArrayList<>();
+    private final List<Reservation> reservations = new ArrayList<>();
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> Reservations() {
+        return reservations;
+    }
 }
