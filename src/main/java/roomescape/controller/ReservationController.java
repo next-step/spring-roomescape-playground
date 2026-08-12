491e2c364a9a8e2978d52d5676667cab12024c94
johaeunn <jheun04@naver.com>
2026-08-12 21:03:30 +0900

    refactor: controller, domain, dto 패키지 분리

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,8 +1,10 @@
-package roomescape;
+package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import roomescape.domain.Reservation;
+import roomescape.dto.ReservationResponse;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
