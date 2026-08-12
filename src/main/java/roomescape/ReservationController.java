a63e0a54dbfb556b2cf7bad93bdb392bd8b0d7e7
johaeunn <jheun04@naver.com>
2026-08-12 20:59:29 +0900

    refactor: 예약 조회 응답 DTO 분리

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -11,11 +11,7 @@ import java.util.List;
 @Controller
 public class ReservationController {
 
-    private final List<Reservation> reservations = List.of(
-            new Reservation(1L, "브라운", LocalDate.of(2023, 1, 1), LocalTime.of(10, 0)),
-            new Reservation(2L, "브라운", LocalDate.of(2023, 1, 2), LocalTime.of(11, 0)),
-            new Reservation(3L, "브라운", LocalDate.of(2023, 1, 3), LocalTime.of(12, 0))
-    );
+    private final List<Reservation> reservations = List.of(new Reservation(1L, "브라운", LocalDate.of(2023, 1, 1), LocalTime.of(10, 0)), new Reservation(2L, "브라운", LocalDate.of(2023, 1, 2), LocalTime.of(11, 0)), new Reservation(3L, "브라운", LocalDate.of(2023, 1, 3), LocalTime.of(12, 0)));
 
     @GetMapping("/reservation")
     public String reservationPage() {
@@ -24,7 +20,9 @@ public class ReservationController {
 
     @ResponseBody
     @GetMapping("/reservations")
-    public List<Reservation> reservations() {
-        return List.copyOf(reservations);
+    public List<ReservationResponse> reservations() {
+        return reservations.stream()
+                    .map(ReservationResponse::from)
+                    .toList();
     }
 }
