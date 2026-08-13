d7d958a9a759e9bfbbaae17b9497af37d41a344e
JisuHan <ivyjisu@gmail.com>
2026-08-13 14:48:43 +0900

    refactor: 사용하지 않는 import문 제거

--- src/main/java/roomescape/ReservationViewController.java
+++ src/main/java/roomescape/ReservationViewController.java
@@ -1,7 +1,5 @@
 package roomescape;
 
-import io.micrometer.observation.transport.ResponseContext;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
