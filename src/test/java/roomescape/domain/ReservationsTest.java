d413cad9bff7399af83567fe3fcf865636eea7d9
jh-lee0 <justin030627@naver.com>
2026-08-13 21:47:30 +0900

    test: 기존 테스트코드 수정  및 ReservationControllerTest 추가

--- src/test/java/roomescape/domain/ReservationsTest.java
+++ src/test/java/roomescape/domain/ReservationsTest.java
@@ -5,6 +5,7 @@ import roomescape.exception.NotFoundReservationException;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -38,5 +39,38 @@ class ReservationsTest {
         assertThatThrownBy(() -> reservations.delete(999))
                 .isInstanceOf(NotFoundReservationException.class)
                 .hasMessage("해당 id의 예약을 찾을 수 없습니다.");
+    }
+
+    @Test
+    void 여러_예약_중_특정_id의_예약만_삭제된다() {
+        //Given
+        Reservations reservations = new Reservations();
+        Reservation first = new Reservation(1, "이준환", LocalDate.of(2026,8,5), LocalTime.of(10,0));
+        Reservation second = new Reservation(2, "이준환", LocalDate.of(2026,8,6), LocalTime.of(11,0));
+        reservations.add(first);
+        reservations.add(second);
+
+        //When
+        reservations.delete(1);
+
+        //Then
+        List<Reservation> remaining = reservations.getReservations();
+        assertThat(remaining).hasSize(1);
+        assertThat(remaining.get(0).getId()).isEqualTo(2);
+    }
+
+    @Test
+    void getReservations로_받은_리스트는_외부에서_수정할_수_없다() {
+        // Given
+        Reservations reservations = new Reservations();
+        Reservation first = new Reservation(1, "이준환", LocalDate.of(2026, 8, 5), LocalTime.of(10, 0));
+        Reservation second = new Reservation(2, "이준환2", LocalDate.of(2026, 8, 6), LocalTime.of(11, 0));
+        reservations.add(first);
+        reservations.add(second);
+
+        // When & Then
+        List<Reservation> list = reservations.getReservations();
+        assertThatThrownBy(() -> list.add(new Reservation(2, "이준환3", LocalDate.of(2026, 8, 6), LocalTime.of(11, 0))))
+                .isInstanceOf(UnsupportedOperationException.class);
     }
 }
