b9ab9350152964b8c4d08d9adec14d5872673518
JisuHan <ivyjisu@gmail.com>
2026-08-14 13:53:01 +0900

    refactor: 컨트롤러 역할 분리 (PageController, DataController)

--- src/main/java/roomescape/DataController.java
+++ src/main/java/roomescape/DataController.java
@@ -1,37 +1,26 @@
 package roomescape;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-@Controller
-public class ReservationViewController {
+@RestController
+public class DataController {
     private List<Reservation> reservations = new ArrayList<>();
     private AtomicLong index = new AtomicLong(1);
 
-    public ReservationViewController() {
+    public DataController() {
         reservations.add(new Reservation(1L, "브라운", "2023-01-01", "10:00"));
         reservations.add(new Reservation(2L, "브라운", "2023-01-02", "11:00"));
         reservations.add(new Reservation(3L, "브라운", "2023-01-03", "12:00"));
     }
 
-    @GetMapping("/")
-    public String home() {
-        return "home";
-    }
-
-    @GetMapping("/reservation")
-    public String reservation() {
-        return "reservation";
-    }
-
     @GetMapping("/reservations")
-    @ResponseBody
     public List<Reservation> reservations() {
         return reservations;
     }
 }
+
