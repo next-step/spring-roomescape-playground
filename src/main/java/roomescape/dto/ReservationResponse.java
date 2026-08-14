1a4b3cd04dba56723808420e053cce8526ad09db
johaeunn <jheun04@naver.com>
2026-08-15 02:38:06 +0900

    refactor: ReservationResponse를 record로 변경

--- src/main/java/roomescape/dto/ReservationResponse.java
+++ src/main/java/roomescape/dto/ReservationResponse.java
@@ -6,19 +6,12 @@ import roomescape.domain.Reservation;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class ReservationResponse {
-    private final Long id;
-    private final String name;
-    private final LocalDate date;
-    @JsonFormat(pattern = "HH:mm")
-    private final LocalTime time;
-
-    public ReservationResponse(Long id, String name, LocalDate date, LocalTime time) {
-        this.id = id;
-        this.name = name;
-        this.date = date;
-        this.time = time;
-    }
+public record ReservationResponse (
+        Long id,
+        String name,
+        LocalDate date,
+        @JsonFormat(pattern = "HH:mm") LocalTime time
+){
 
     public static ReservationResponse from(Reservation reservation) {
         return new ReservationResponse(
@@ -27,21 +20,5 @@ public class ReservationResponse {
                 reservation.getDate(),
                 reservation.getTime()
         );
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getTime() {
-        return time;
     }
 }
