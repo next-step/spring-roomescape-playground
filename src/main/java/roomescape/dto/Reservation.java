15638b09132ab28dd8a9ada90b19c59c1f2c92e8
juhee0223 <pjuhee23@dankook.ac.kr>
2026-08-14 17:40:35 +0900

    refactor: 명세에 맞게 시간형식 수정

--- src/main/java/roomescape/dto/Reservation.java
+++ src/main/java/roomescape/dto/Reservation.java
@@ -1,5 +1,6 @@
 package roomescape.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -7,6 +8,8 @@ public class Reservation {
     private final Long id;
     private final String name;
     private final LocalDate date;
+
+    @JsonFormat(pattern = "HH:mm")
     private final LocalTime time;
 
     public Reservation(Long id, String name, LocalDate date, LocalTime time) {
