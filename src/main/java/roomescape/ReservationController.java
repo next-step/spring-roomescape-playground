249b72d020d6f9d49323010d7219214d2eeb2988
se0ye2n <hsy040925@gmail.com>
2026-08-16 17:44:48 +0900

    feat: ReservationController 클래스 수정

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ReservationController {
+
+    private final ReservationRepository reservationRepository;
+
+    public ReservationController(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
 
     @GetMapping("/reservation")
     public String reservation() {
@@ -18,21 +23,6 @@ public class ReservationController {
     @GetMapping("/reservations")
     @ResponseBody
     public List<Reservation> reservations() {
-
-        List<Reservation> reservations = new ArrayList<>();
-
-        reservations.add(
-                new Reservation(1L, "브라운", "2026-08-12", "10:00")
-        );
-
-        reservations.add(
-                new Reservation(2L, "브라운", "2026-08-13", "11:00")
-        );
-
-        reservations.add(
-                new Reservation(3L, "브라운", "2026-08-14", "12:00")
-        );
-
-        return reservations;
+        return reservationRepository.findAll();
     }
 }
