f4a9d2e5566ff747d6d55b0606adb7cfce74774c
Jisu Han <ivyjisu@gmail.com>
2026-08-16 16:38:50 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

---
+++ src/main/java/roomescape/Reservation.java
@@ -0,0 +1,42 @@
+package roomescape;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+    private Long id;
+    private String name;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    LocalDate date;
+    @JsonFormat(pattern = "HH:mm")
+    LocalTime time;
+
+    public Reservation() {
+    }
+
+    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+}
+
