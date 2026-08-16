6aed53fbc89517ae6099496c08513e8614bbdbd1
Park Juhee <140599228+juhee0223@users.noreply.github.com>
2026-08-16 22:53:44 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

---
+++ src/main/java/roomescape/dto/Reservation.java
@@ -0,0 +1,37 @@
+package roomescape.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+    private final Long id;
+    private final String name;
+    private final LocalDate date;
+
+    @JsonFormat(pattern = "HH:mm")
+    private final LocalTime time;
+
+    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+}
