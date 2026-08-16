bbe25dd763b5fab744c0c5b4c1381df3465341e0
LeeJiin <5616jiin@gmail.com>
2026-08-16 20:17:54 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

---
+++ src/main/java/roomescape/HomeController.java
@@ -0,0 +1,32 @@
+package roomescape;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HomeController {
+    private List<Reservation> reservations = new ArrayList<>(List.of(new Reservation(1L, "브라운", "2023-01-01", "10:00"),
+                                                                     new Reservation(2L, "하이", "2024-05-23", "10:00"),
+                                                                     new Reservation(2L, "바이", "2026-08-12", "10:00")
+                                                                    ));
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
+    }
+
+    @GetMapping("reservations")
+    public ResponseEntity<List<Reservation>> reservations() {
+        return ResponseEntity.ok().body(reservations);
+    }
+
+}
