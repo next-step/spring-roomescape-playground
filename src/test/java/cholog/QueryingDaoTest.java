6869836cc0759697aad0e706cdc9804eedb1db66
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-08-17 12:54:54 +0900

    feat: QueryingDaoTest.count 성공

---
+++ src/test/java/cholog/QueryingDaoTest.java
@@ -0,0 +1,49 @@
+package cholog;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+public class QueryingDaoTest {
+
+    private QueryingDAO queryingDAO;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        queryingDAO = new QueryingDAO(jdbcTemplate);
+
+        jdbcTemplate.execute("drop table customers if exists");
+        jdbcTemplate.execute("create table customers(" +
+                "id SERIAL, first_name varchar(255), last_name varchar(255)) ");
+
+        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+    }
+
+    @Test
+    void count() {
+        // given
+        int expect = 4;
+
+        // when
+        int actual = queryingDAO.count();
+
+        // then
+        assertThat(actual).isEqualTo(expect);
+    }
+}
