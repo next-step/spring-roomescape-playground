73c2d0d0dcf2517094b9729f9caac39f8930d50b
se0ye2n <hsy040925@gmail.com>
2026-08-18 00:53:34 +0900

    feat: 3단계 ReservationController의 import 추가

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,10 +1,16 @@
 package roomescape;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.net.URI;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
