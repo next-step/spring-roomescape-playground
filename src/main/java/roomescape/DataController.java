4929d04209d6d46d61a0d6fa60a283facbce5260
JisuHan <ivyjisu@gmail.com>
2026-08-17 20:58:04 +0900

    feat: 예약 생성 API 기능 구현

--- src/main/java/roomescape/DataController.java
+++ src/main/java/roomescape/DataController.java
@@ -1,10 +1,12 @@
 package roomescape;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
@@ -14,15 +16,17 @@ public class DataController {
     private List<Reservation> reservations = new ArrayList<>();
     private AtomicLong index = new AtomicLong(1);
 
-    public DataController() {
-        reservations.add(new Reservation(index.getAndIncrement(), "브라운", LocalDate.of(2023, 1, 1), LocalTime.of(10, 0)));
-        reservations.add(new Reservation(index.getAndIncrement(), "브라운", LocalDate.of(2023, 1, 2), LocalTime.of(11, 0)));
-        reservations.add(new Reservation(index.getAndIncrement(), "브라운", LocalDate.of(2023, 1, 3),LocalTime.of(12, 0)));
-    }
 
     @GetMapping("/reservations")
     public List<Reservation> reservations() {
         return reservations;
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> create(@RequestBody Reservation reservation) {
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        reservations.add(newReservation);
+        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
     }
 }
 
