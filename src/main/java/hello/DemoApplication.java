8def2bbdc6aa47f22a1121511e74becc638b436b
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-08-17 12:30:09 +0900

    feat: hello.DemoApplication을 실행할 때마다 ddl-auto=create-drop 설정을 ComandLineRunner.run 메소드에 오버라이딩하여 작성, Customer 테이블을 생성하고, 더미값 작성하는 것 추가

--- src/main/java/hello/DemoApplication.java
+++ src/main/java/hello/DemoApplication.java
@@ -1,8 +1,51 @@
 package hello;
 
-public class DemoApplication {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
     public static void main(String[] args) {
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... args) throws Exception {
+        // ddl-auto = create-drop setup
+        jdbcTemplate.execute("drop table customer if exists");
+        jdbcTemplate.execute("create table customers(id SERIAL, first_name varchar(255), last_name varchar(255))");
+
+        saveDummyNames();
+        getDummyNames();
+    }
+
+    private void saveDummyNames() {
+        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+    }
+
+    private void getDummyNames() {
+        List<Customer> customers = jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[]{"Josh"},
+                (rs, rowNum) -> new Customer(
+                        rs.getLong("id"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name")
+                )
+        );
     }
 }
