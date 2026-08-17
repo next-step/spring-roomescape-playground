02c801a158b8aa208703aecf12673e210fae8454
JisuHan <ivyjisu@gmail.com>
2026-08-17 21:34:11 +0900

    feat: 예약 삭제 기능 구현

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,24 +1,22 @@
 package roomescape;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-public class DataController {
+public class ReservationController {
     private List<Reservation> reservations = new ArrayList<>();
     private AtomicLong index = new AtomicLong(1);
 
 
     @GetMapping("/reservations")
-    public ResponseEntity<List<Reservation>> reservations() {
+    public ResponseEntity<List<Reservation>> read() {
         return ResponseEntity.ok().body(reservations);
     }
 
@@ -27,6 +25,16 @@ public class DataController {
         Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
         reservations.add(newReservation);
         return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+        reservations.remove(reservation);
+        return ResponseEntity.noContent().build();
     }
 }
 
