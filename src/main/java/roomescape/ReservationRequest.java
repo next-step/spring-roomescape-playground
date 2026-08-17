faca37b893a140e7cc26c79c5d55c5028e116c11
se0ye2n <hsy040925@gmail.com>
2026-08-17 22:20:09 +0900

    feat: 3단계 ReservationRequest 구현

---
+++ src/main/java/roomescape/ReservationRequest.java
@@ -0,0 +1,23 @@
+package roomescape;
+
+public class ReservationRequest {
+
+    private String name;
+    private String date;
+    private String time;
+
+    public ReservationRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
