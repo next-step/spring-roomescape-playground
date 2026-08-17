5d4a0feacf6c3696bf166c2cc539bfc9cb505a17
se0ye2n <hsy040925@gmail.com>
2026-08-17 22:22:54 +0900

    feat: 3단계 ReservationRepositor 수정

--- src/main/java/roomescape/ReservationRepository.java
+++ src/main/java/roomescape/ReservationRepository.java
@@ -1,27 +1,27 @@
 package roomescape;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class ReservationRepository {
 
     private final List<Reservation> reservations = new ArrayList<>();
 
-    public ReservationRepository() {
-        reservations.add(
-                new Reservation(1L, "브라운", "2026-08-12", "10:00")
-        );
-
-        reservations.add(
-                new Reservation(2L, "브라운", "2026-08-13", "11:00")
-        );
-
-        reservations.add(
-                new Reservation(3L, "브라운", "2026-08-14", "12:00")
-        );
-    }
-
     public List<Reservation> findAll() {
         return reservations;
+    }
+
+    public Reservation save(Reservation reservation) {
+        reservations.add(reservation);
+        return reservation;
+    }
+
+    public boolean deleteById(Long id) {
+        return reservations.removeIf(
+                reservation -> reservation.getId().equals(id)
+        );
     }
 }
