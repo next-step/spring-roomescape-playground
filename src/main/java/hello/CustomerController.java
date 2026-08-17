3987d2759a45243895604cff8b1441449816770a
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-08-17 12:40:17 +0900

    feat: Customer 1개를 생성, 전체를 조회하는 API 작성

---
+++ src/main/java/hello/CustomerController.java
@@ -0,0 +1,47 @@
+package hello;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public CustomerController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @PostMapping("/customers")
+    public ResponseEntity<Void> save(
+            @RequestBody Customer customer
+    ) {
+        String sql = "insert into customers(first_name, last_name) values (?, ?)";
+        jdbcTemplate.update(sql, customer.getFirstName(), customer.getLastName());
+        return ResponseEntity
+                .status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/customers")
+    public ResponseEntity<List<Customer>> list() {
+        String sql = "select id, first_name, last_name from customers";
+        List<Customer> customers = jdbcTemplate.query(
+                sql, (resultSet, rowNum) -> {
+                    Customer customer = new Customer(
+                            resultSet.getLong("id"),
+                            resultSet.getString("first_name"),
+                            resultSet.getString("last_name")
+                    );
+                    return customer;
+                });
+        return ResponseEntity.ok(customers);
+    }
+}
