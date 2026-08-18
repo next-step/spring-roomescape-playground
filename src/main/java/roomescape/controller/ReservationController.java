855c65ec905e01a4df439a086e3f10bd82c4b388
MunJaeWon <wodnjs3961@naver.com>
2026-08-19 01:12:07 +0900

    feat: 7단계 - DB에서 데이터 추가/삭제

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,6 +2,8 @@ package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import roomescape.domain.Reservation;
@@ -10,18 +12,15 @@ import roomescape.dto.ReservationResponse;
 import roomescape.exception.NotFoundReservationException;
 
 import java.net.URI;
+import java.sql.PreparedStatement;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ReservationController {
-    private final List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong index = new AtomicLong(1);
     private JdbcTemplate jdbcTemplate;
+    private static final int NO_ROWS = 0;
 
     public ReservationController(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -29,7 +28,7 @@ public class ReservationController {
 
     @GetMapping("/reservations")
     public ResponseEntity<List<ReservationResponse>> readReservationList() {
-        List<ReservationResponse> respones = jdbcTemplate.query(
+        List<ReservationResponse> reservationResponses = jdbcTemplate.query(
                         "select id, name, date, time from reservation",
                         (rs, rowNum) -> new Reservation(
                                 rs.getLong("id"),
@@ -40,7 +39,7 @@ public class ReservationController {
                 ).stream()
                 .map(ReservationResponse::from)
                 .toList();
-        return ResponseEntity.ok(respones);
+        return ResponseEntity.ok(reservationResponses);
     }
 
     @GetMapping("/reservations/{id}")
@@ -52,26 +51,56 @@ public class ReservationController {
 
     @PostMapping("/reservations")
     public ResponseEntity<ReservationResponse> addReservation(@RequestBody ReservationRequest request) {
-        Reservation newReservation = request.toEntity(index.getAndIncrement());
-        reservations.add(newReservation);
+        Reservation newReservation = request.toEntity();
+        String sql = "insert into reservation (name, date, time) values (?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    sql,
+                    new String[]{"id"});
+            ps.setString(1, request.name());
+            ps.setObject(2, request.date());
+            ps.setObject(3, request.time());
+            return ps;
+        }, keyHolder);
 
-        return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId()))
-                .body(ReservationResponse.from(newReservation));
+        Long id = keyHolder.getKey().longValue();
+
+        Reservation savedReservation = new Reservation(
+                id,
+                newReservation.getName(),
+                newReservation.getDate(),
+                newReservation.getTime()
+        );
+
+        return ResponseEntity.created(URI.create("/reservations/" + savedReservation.getId()))
+                .body(ReservationResponse.from(savedReservation));
     }
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
-        Reservation reservation = findReservationById(id);
+        String sql = "delete from reservation where id = ?";
+        int deletedCount = jdbcTemplate.update(sql, Long.valueOf(id));
 
-        reservations.remove(reservation);
-
+        if (deletedCount == NO_ROWS) {
+            throw new NotFoundReservationException("예약을 찾을 수 없습니다.");
+        }
         return ResponseEntity.noContent().build();
     }
 
     private Reservation findReservationById(Long id) {
-        return reservations.stream()
-                .filter(it -> Objects.equals(it.getId(), id))
-                .findFirst()
-                .orElseThrow(() -> new NotFoundReservationException("예약을 찾을 수 없습니다."));
+        String sql = "select id, name, date, time from reservation where id = ?";
+
+        return jdbcTemplate.queryForObject(
+                sql,
+                (rs, rowNum) -> {
+                    Reservation reservation = new Reservation(
+                            rs.getLong("id"),
+                            rs.getString("name"),
+                            rs.getObject("date", LocalDate.class),
+                            rs.getObject("time", LocalTime.class)
+                    );
+                    return reservation;
+                }, id);
     }
 }
