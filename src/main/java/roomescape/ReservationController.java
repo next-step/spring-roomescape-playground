1a4c0318b01e74e413fcde4f5fdfff922dc8df18
장서인 <seoin1893@dankook.ac.kr>
2026-08-19 13:24:36 +0900

    feat (ReservationController) : 3단계 - 예약 추가 기능 구현

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -2,20 +2,23 @@ package roomescape;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ReservationController {
 
     private final List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
 
     public ReservationController() {
-        reservations.add(new Reservation(1, "브라운", "2023-01-01", "10:00"));
-        reservations.add(new Reservation(2, "브라운", "2023-01-02", "11:00"));
     }
+
     @GetMapping("/reservation")
     public String reservation() {
         return "reservation";
@@ -23,7 +26,20 @@ public class ReservationController {
 
     @GetMapping("/reservations")
     @ResponseBody
-    public List<Reservation> reservations() {
+    public List<Reservation> getReservations() {
         return reservations;
+    }
+
+    @PostMapping("/reservations")
+    @ResponseBody
+    public void postReservations(@RequestBody ReservationRequest reservationRequest) {
+        Reservation reservation = new Reservation(
+                index.incrementAndGet(),
+                reservationRequest.getDate(),
+                reservationRequest.getName(),
+                reservationRequest.getTime()
+        );
+
+        reservations.add(reservation);
     }
 }
