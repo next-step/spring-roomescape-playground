dbae99ea50aa06c28eb5e518bedd264e8d4d4e19
juhee0223 <pjuhee23@dankook.ac.kr>
2026-08-19 14:25:59 +0900

    feat: 예약 검증 실패(400)와 예약 없음(404)을 표현하는 커스텀 예외 추가

---
+++ src/main/java/roomescape/exception/ReservationInvalidException.java
@@ -0,0 +1,7 @@
+package roomescape.exception;
+
+public class ReservationInvalidException extends RuntimeException {
+    public ReservationInvalidException(String message) {
+        super(message);
+    }
+}
