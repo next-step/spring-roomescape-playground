5e02f76303c95a0c44a6e58f2be674d4df99c456
se0ye2n <hsy040925@gmail.com>
2026-08-19 21:37:55 +0900

    refactor: 예약 요청 검증을 DTO로 분리

--- src/main/java/roomescape/ReservationRequest.java
+++ src/main/java/roomescape/ReservationRequest.java
@@ -1,12 +1,25 @@
 package roomescape;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class ReservationRequest {
 
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String date;
+
+    @NotBlank
     private String time;
 
     public ReservationRequest() {
+    }
+
+    public ReservationRequest(String name, String date, String time) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
     }
 
     public String getName() {
