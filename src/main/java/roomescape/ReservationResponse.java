4a4af416c78e55e2ceaacfc035759169ab2d3827
se0ye2n <hsy040925@gmail.com>
2026-08-19 21:46:01 +0900

    feat: 응답 전용 ReservationResponse 구현

---
+++ src/main/java/roomescape/ReservationResponse.java
@@ -0,0 +1,41 @@
+package roomescape;
+
+public class ReservationResponse {
+
+    private final Long id;
+    private final String name;
+    private final String date;
+    private final String time;
+
+    public ReservationResponse(Long id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public static ReservationResponse from(Reservation reservation) {
+        return new ReservationResponse(
+                reservation.getId(),
+                reservation.getName(),
+                reservation.getDate(),
+                reservation.getTime()
+        );
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
