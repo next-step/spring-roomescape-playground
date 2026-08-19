36768745cd45225b998e30536d4a4c95d18e9774
anrwkd9902 <anrwkd9902@naver.com>
2026-08-19 18:41:34 +0900

    feat : 미션 7단계 예약 추가/취소 API가 DB를 활용하도록 변경

--- src/main/java/roomescape/dao/ReservationDAO.java
+++ src/main/java/roomescape/dao/ReservationDAO.java
@@ -1,4 +1,59 @@
 package roomescape.dao;
 
-public class ReservationDao {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.domain.Reservation;
+
+import java.sql.PreparedStatement;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Repository
+public class ReservationDAO {
+    private JdbcTemplate jdbcTemplate;
+
+    public ReservationDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Reservation> findAllReservations() {
+        String sql = "select id, name, date, time from reservation";
+        return jdbcTemplate.query(
+                sql,
+                (resultSet, rowNum) -> {
+                    Reservation reservation = new Reservation(
+                            resultSet.getLong("id"),
+                            resultSet.getString("name"),
+                            LocalDate.parse(resultSet.getString("date")),
+                                    LocalTime.parse(resultSet.getString("time"))
+                    );
+                    return reservation;
+                });
+    }
+
+
+    public int delete(Long id) {
+        int delete = jdbcTemplate.update("delete from reservation where id = ?", Long.valueOf(id));
+        return delete;
+    }
+
+    public Long insertWithKeyHolder(Reservation reservation) {
+        String sql = "insert into reservation (name, date, time) values (?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    sql,
+                    new String[]{"id"});
+            ps.setString(1, reservation.getName());
+            ps.setString(2, reservation.getDate().toString());
+            ps.setString(3, reservation.getTime().toString());
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
+    }
 }
