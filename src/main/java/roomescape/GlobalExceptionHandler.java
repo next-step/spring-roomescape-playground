31ba68575663cac248ff0c7ab4f442140bf7bf1b
juhee0223 <pjuhee23@dankook.ac.kr>
2026-08-19 14:26:30 +0900

    feat: 커스텀 예외 전역처리하는 GlobalExceptionHandler 구현

---
+++ src/main/java/roomescape/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package roomescape;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import roomescape.exception.ReservationInvalidException;
+import roomescape.exception.ReservationNotFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ReservationInvalidException.class)
+    public ResponseEntity<Void> handleInvalidException(ReservationInvalidException e) {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(ReservationNotFoundException.class)
+    public ResponseEntity<Void> handleException(ReservationNotFoundException e) {
+        return ResponseEntity.notFound().build();
+    }
+}
