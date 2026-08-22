e054d385a497e5a3201d08aa832a65aee60f8d81
seungminbluebox <parksm1415@gmail.com>
2026-08-22 14:06:26 +0900

    feat(ReservationRequest) : DTO 클래스 생성

---
+++ src/main/java/roomescape/ReservationRequest.java
@@ -0,0 +1,17 @@
+package roomescape;
+
+public class ReservationRequest {
+    private String name;
+    private String date;
+    private String time;
+
+    public String getName(){
+        return name;
+    }
+    public String getDate(){
+        return date;
+    }
+    public String getTime(){
+        return time;
+    }
+}
