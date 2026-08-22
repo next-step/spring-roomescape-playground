d24d5c3d15851427bddee49d1610fe0ba5f2690e
se0ye2n <hsy040925@gmail.com>
2026-08-22 15:21:02 +0900

    refactor: HomeController 수정

--- src/main/java/roomescape/HomeController.java
+++ src/main/java/roomescape/HomeController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "home";
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
+    }
+
+    @GetMapping("/time")
+    public String time() {
+        return "time";
     }
 }
