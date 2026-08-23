a805d9498e190bc585b101be9c25ac273a9d8e75
se0ye2n <hsy040925@gmail.com>
2026-08-24 08:19:09 +0900

    fix: home 매핑 복구

--- src/main/java/roomescape/HomeController.java
+++ src/main/java/roomescape/HomeController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
     @GetMapping("/reservation")
     public String reservation() {
         return "reservation";
