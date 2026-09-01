37b4d90bbb2f73b62ca533c1e882add8f91f7203
KDYoung5606 <2271323@hansung.ac.kr>
2026-09-02 01:46:17 +0900

    feat(reservation): 예약시간을 LocalTime 대신 ReservationTime 객체로 변경

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -1,21 +1,13 @@
 package roomescape.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import roomescape.domain.Reservation;
 
 @Getter
 @AllArgsConstructor
 public class ReservationRequest {
     private final String name;
     private final LocalDate date;
-    @JsonFormat(pattern = "HH:mm")
-    private final LocalTime time;
-
-    public Reservation toDomain(Long id){
-        return new Reservation(id, name, date, time);
-    }
+    private final Long time;
 }
