3f22ee60f96c5a84745641e0d417b80a3c6d0472
anrwkd9902 <anrwkd9902@naver.com>
2026-09-02 19:41:11 +0900

    feat : 10단계 ReservationService, ReservationController 분리

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,12 +2,9 @@ package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import roomescape.dao.ReservationDAO;
-import roomescape.dao.TimeDAO;
 import roomescape.domain.reservation.ReservationRequest;
-import roomescape.domain.time.Time;
-import roomescape.exception.NotFoundReservationException;
 import roomescape.domain.reservation.Reservation;
+import roomescape.service.ReservationService;
 import jakarta.validation.Valid;
 import java.net.URI;
 import java.util.List;
@@ -15,34 +12,27 @@ import java.util.List;
 
 @RestController
 public class ReservationController {
-    private final ReservationDAO reservationDAO;
-    private final TimeDAO timeDAO;
+    private final ReservationService reservationService;
 
-    public ReservationController(ReservationDAO reservationDAO, TimeDAO timeDAO) {
-        this.reservationDAO = reservationDAO;
-        this.timeDAO = timeDAO;
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
     }
 
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> read() {
-        return ResponseEntity.ok(reservationDAO.findAllReservations());
+        return ResponseEntity.ok(reservationService.read());
     }
 
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> create(@Valid @RequestBody ReservationRequest request) {
-        Long generatedId = reservationDAO.insertWithKeyHolder(request);
-        Time time = timeDAO.findById(request.getTime());
-        Reservation newReservation = Reservation.toEntity(request, generatedId, time);
+        Reservation newReservation = reservationService.create(request);
 
         return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId())).body(newReservation);
     }
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        int deletedCount = reservationDAO.delete(id);
-        if (deletedCount == 0) {
-            throw new NotFoundReservationException("해당 예약을 찾을 수 없습니다");
-        }
+        reservationService.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/roomescape/service/ReservationService.java
@@ -0,0 +1,40 @@
+package roomescape.service;
+
+import org.springframework.stereotype.Service;
+import roomescape.dao.ReservationDAO;
+import roomescape.dao.TimeDAO;
+import roomescape.domain.reservation.Reservation;
+import roomescape.domain.reservation.ReservationRequest;
+import roomescape.domain.time.Time;
+import roomescape.exception.NotFoundReservationException;
+
+import java.util.List;
+
+@Service
+public class ReservationService {
+    private final ReservationDAO reservationDAO;
+    private final TimeDAO timeDAO;
+
+    public ReservationService(ReservationDAO reservationDAO, TimeDAO timeDAO) {
+        this.reservationDAO = reservationDAO;
+        this.timeDAO = timeDAO;
+    }
+
+    public List<Reservation> read() {
+        return reservationDAO.findAllReservations();
+    }
+
+    public Reservation create(ReservationRequest request) {
+        Long generatedId = reservationDAO.insertWithKeyHolder(request);
+        Time time = timeDAO.findById(request.getTime());
+
+        return Reservation.toEntity(request, generatedId, time);
+    }
+
+    public void delete(Long id) {
+        int deletedCount = reservationDAO.delete(id);
+        if (deletedCount == 0) {
+            throw new NotFoundReservationException("해당 예약을 찾을 수 없습니다");
+        }
+    }
+}
