57e49b826e5d6cbf1ff2f4a4193c20d2258a6d54
se0ye2n <hsy040925@gmail.com>
2026-09-02 17:09:43 +0900

    test: [5단계] H2 데이터베이스 연결 및 테이블 생성 검증

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -3,14 +3,23 @@ package roomescape;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     void 일단계() {
@@ -44,5 +53,24 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(404)
                 .body("message", equalTo("예약을 찾을 수 없습니다."));
+    }
+
+    @Test
+    void 오단계() {
+        try (Connection connection =
+                     jdbcTemplate.getDataSource().getConnection()) {
+
+            assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+
+            assertThat(
+                    connection.getMetaData()
+                            .getTables(null, null, "RESERVATION", null)
+                            .next()
+            ).isTrue();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
