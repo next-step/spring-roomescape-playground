0a74423dfb354f335f9bb49ee471f46e4ca6b3ee
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-09-03 15:29:58 +0900

    refactor: 예외 패키징을 목적에 맞게 분리

--- src/main/java/com/cholog/roomescape/roomescape/enums/RoomEscapeExceptionCode.java
+++ src/main/java/com/cholog/roomescape/roomescape/enums/RoomEscapeExceptionCode.java
@@ -3,8 +3,10 @@ package com.cholog.roomescape.roomescape.exception.code;
 public enum RoomEscapeExceptionCode {
 
     RESERVATION_NOT_FOUND("해당 예약을 찾을 수 없습니다."),
+    RESERVATION_CONFLICT("이미 예약된 시각입니다."),
 
-    TIME_NOT_FOUND("해당 시간을 찾을 수 없습니다.")
+    TIME_NOT_FOUND("해당 시간을 찾을 수 없습니다."),
+    TIME_CONFLICT("이미 존재하는 시각입니다."),
     ;
 
     private final String message;
