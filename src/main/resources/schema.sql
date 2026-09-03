a660265dc205d5c9ed33ab98d5cb245093aa27db
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-09-03 15:30:29 +0900

    fix: time 테이블 생성 구문과 reservation 테이블 내에 외래 키 및 제약조건 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,20 @@
 drop table if exists reservation;
 drop table if exists time;
 
+create table time
+(
+    id bigint not null auto_increment,
+    time time not null unique,
+    primary key (id)
+);
+
 create table reservation
 (
     id bigint not null auto_increment,
     name varchar(255) not null,
     date date not null,
-    time time not null,
-    primary key (id)
-);
-
-create table time
-(
-    id bigint not null auto_increment,
-    time time not null,
-    primary key (id)
+    time_id bigint not null,
+    primary key (id),
+    foreign key (time_id) references time(id),
+    constraint uk_user_reserve_time unique (date, time_id)
 );
