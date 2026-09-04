82c9ab561b0e6c374065e198d8cad02a74acc712
juhee0223 <pjuhee23@dankook.ac.kr>
2026-09-04 21:33:21 +0900

    refactor: 예약요청 검증메시지 추가

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -6,7 +6,12 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record ReservationRequest(
-        @NotBlank String name,
-        @NotNull LocalDate date,
-        @NotNull LocalTime time) {
+        @NotBlank(message = "예약자 이름은 비워둘 수 없습니다.")
+        String name,
+
+        @NotNull(message = "예약 날짜는 비워둘 수 없습니다.")
+        LocalDate date,
+
+        @NotNull(message = "예약 시간은 비워둘 수 없습니다.")
+        LocalTime time) {
 }
