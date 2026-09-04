5da6e15e8143b7b74b42d48e2a06610114903345
jh-lee0 <justin030627@naver.com>
2026-09-04 20:57:11 +0900

    refactor: GlobalExceptionHandler 예외 별  로그 레벨 수정

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -19,37 +19,37 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundReservationException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundReservation(NotFoundReservationException e) {
-        log.warn(e.getMessage());
+        log.info(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(BlankReservationException.class)
     public ResponseEntity<ErrorResponse> handleBlankReservation(BlankReservationException e) {
-        log.warn(e.getMessage());
+        log.info(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException e) {
-        log.warn(e.getMessage());
+        log.info(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("잘못된 요청입니다. id는 숫자여야 합니다."));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> handleMessageNotReadable(HttpMessageNotReadableException e) {
-        log.warn(e.getMessage());
+        log.info(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("요청 형식이 올바르지 않습니다."));
     }
 
     @ExceptionHandler(NotFoundTimeException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundTime(NotFoundTimeException e) {
-        log.warn(e.getMessage());
+        log.info(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(BlankTimeException.class)
     public ResponseEntity<ErrorResponse> handleBlankTime(BlankTimeException e) {
-        log.warn(e.getMessage());
+        log.info(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
     }
 
@@ -61,7 +61,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ErrorResponse> handleDataIntegrityViolation(DataIntegrityViolationException e) {
-        log.warn(e.getMessage());
+        log.error(e.getMessage());
         Throwable cause = e.getRootCause();
 
         if (cause instanceof SQLException sqlException) {
