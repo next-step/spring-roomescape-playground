f9c4418513ade111a23821b603651cbd5b3cc72f
se0ye2n <hsy040925@gmail.com>
2026-09-06 01:27:15 +0900

    refactor: 예약 예외 응답의 상태 코드를 HttpStatus로 표현

--- src/main/java/roomescape/GlobalExceptionHandler.java
+++ src/main/java/roomescape/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package roomescape;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,16 +10,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundReservationException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidRequest() {
+    public ResponseEntity<ErrorResponse> handleNotFound(
+            NotFoundReservationException exception
+    ) {
         return ResponseEntity
-                .status(404)
+                .status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(exception.getMessage()));
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidRequest(
-            MethodArgumentNotValidException exception
-    ) {
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            InvalidReservationException.class
+    })
+    public ResponseEntity<ErrorResponse> handleInvalidRequest() {
         return ResponseEntity
                 .badRequest()
                 .body(new ErrorResponse("예약 정보가 올바르지 않습니다."));
