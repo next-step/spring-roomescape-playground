8768cdf0b616b838955f48f24d1a3e97c0c14c7a
장서인 <seoin1893@dankook.ac.kr>
2026-09-08 17:24:30 +0900

    feat : Time 클래스 작성

---
+++ src/main/java/roomescape/domain/Time.java
@@ -0,0 +1,16 @@
+package roomescape.domain;
+
+import java.time.LocalTime;
+
+public class Time {
+
+    private final LocalTime time;
+
+    public Time(LocalTime time) {
+        this.time = time;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+}
