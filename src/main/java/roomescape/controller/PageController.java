f9012f7e6ed41fc61f78a2919f35b798a20a2c05
장서인 <seoin1893@dankook.ac.kr>
2026-09-08 17:30:55 +0900

    feat : 클래스 이름 변경 + 코드 작성

--- src/main/java/roomescape/controller/PageController.java
+++ src/main/java/roomescape/controller/PageController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ReservationPageController {
+public class PageController {
 
     @GetMapping("/reservation")
     public String reservation() {
         return "reservation";
+    }
+
+    @GetMapping("/time")
+    public String time() {
+        return "time";
     }
 }
