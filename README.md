a7cd8d6b86c2b3a2ed15f92d6ebe640d6ac1d4c9
이지인 <5616jiin@gmail.com>
2026-09-09 23:34:46 +0900

    fix: ReservationService에 ReservationDao 생성자 주입

--- README.md
+++ README.md
@@ -78,6 +78,7 @@
 
 - [x] `ReservationService`를 생성한다.
 - [x] `ReservationService`를 Spring Bean으로 등록한다.
+- [x] `ReservationService`에 `ReservationDao`를 생성자 주입한다.
 - [ ] `ReservationController`의 예약 처리 로직을 `ReservationService`로 분리한다.
 - [ ] `ReservationController`가 `ReservationService`를 사용하도록 수정한다.
 
--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,7 +1,14 @@
 package roomescape.service;
 
 import org.springframework.stereotype.Service;
+import roomescape.repository.ReservationDao;
 
 @Service
 public class ReservationService {
+
+    private final ReservationDao reservationDao;
+
+    public ReservationService(ReservationDao reservationDao) {
+        this.reservationDao = reservationDao;
+    }
 }
