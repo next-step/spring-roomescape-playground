dca6b0107421a9840384f410f5afa3cf8d79cf18
장서인 <seoin1893@dankook.ac.kr>
2026-09-11 17:27:56 +0900

    fix : Reservation time 수정

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,16 +1,15 @@
 package roomescape.domain;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 
 public class Reservation {
 
     private final long id;
     private final String name;
     private final LocalDate date;
-    private final LocalTime time;
+    private final Time time;
 
-    public Reservation(long id, String name, LocalDate date, LocalTime time) {
+    public Reservation(long id, String name, LocalDate date, Time time) {
         this.id = id;
         this.name = name;
         this.date = date;
@@ -29,7 +28,7 @@ public class Reservation {
         return date;
     }
 
-    public LocalTime getTime() {
+    public Time getTime() {
         return time;
     }
 }
