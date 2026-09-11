30d501b82c0398bb065651cc4b282851881c14f7
장서인 <seoin1893@dankook.ac.kr>
2026-09-11 17:38:55 +0900

    fix : 예약 컨트롤러 수정

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,34 +2,27 @@ package roomescape.controller;
 
 import java.net.URI;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import roomescape.domain.Reservation;
-import roomescape.domain.Time;
 import roomescape.dto.ReservationRequest;
-import roomescape.repository.ReservationRepository;
-import roomescape.repository.TimeRepository;
+import roomescape.service.ReservationService;
 
 @RestController
 public class ReservationController {
 
-    private final ReservationRepository reservationRepository;
-    private final TimeRepository timeRepository;
+    private final ReservationService reservationService;
 
-    public ReservationController(
-            ReservationRepository reservationRepository,
-            TimeRepository timeRepository
-    ) {
-        this.reservationRepository = reservationRepository;
-        this.timeRepository = timeRepository;
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
     }
 
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> getReservations() {
 
-        List<Reservation> reservations = reservationRepository.getReservations();
+        List<Reservation> reservations =
+                reservationService.getReservations();
 
         return ResponseEntity.ok().body(reservations);
     }
@@ -39,20 +32,10 @@ public class ReservationController {
             @RequestBody ReservationRequest reservationRequest
     ) {
 
-        if (reservationRequest.getName() == null
-                || reservationRequest.getName().trim().isBlank()
-                || reservationRequest.getDate() == null
-                || reservationRequest.getTime() == null) {
-            throw new IllegalArgumentException();
-        }
-
-        Time time = timeRepository.getTime(reservationRequest.getTime())
-                .orElseThrow(NoSuchElementException::new);
-
-        Reservation reservation = reservationRepository.saveReservation(
+        Reservation reservation = reservationService.createReservation(
                 reservationRequest.getName(),
                 reservationRequest.getDate(),
-                time
+                reservationRequest.getTime()
         );
 
         return ResponseEntity.created(
@@ -61,22 +44,22 @@ public class ReservationController {
     }
 
     @GetMapping("/reservations/{id}")
-    public ResponseEntity<Reservation> getReservation(@PathVariable long id) {
+    public ResponseEntity<Reservation> getReservation(
+            @PathVariable long id
+    ) {
 
-        Reservation reservation = reservationRepository.getReservation(id)
-                .orElseThrow(NoSuchElementException::new);
+        Reservation reservation =
+                reservationService.getReservation(id);
 
         return ResponseEntity.ok().body(reservation);
     }
 
     @DeleteMapping("/reservations/{id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable long id) {
+    public ResponseEntity<Void> deleteReservation(
+            @PathVariable long id
+    ) {
 
-        int deletedCount = reservationRepository.deleteReservation(id);
-
-        if (deletedCount == 0) {
-            throw new NoSuchElementException();
-        }
+        reservationService.deleteReservation(id);
 
         return ResponseEntity.noContent().build();
     }
