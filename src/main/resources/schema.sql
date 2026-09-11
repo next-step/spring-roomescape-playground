b4dcf805cf6aa6aa8ec285e9565a25cc18a0f2fb
장서인 <seoin1893@dankook.ac.kr>
2026-09-11 17:28:25 +0900

    fix : 스키마 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,16 @@
-CREATE TABLE RESERVATION
-(
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    date VARCHAR(255) NOT NULL,
-    time_id BIGINT,                           // TODO: 수정
-    PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES time(id) // TODO: 추가
-    );
-
 CREATE TABLE TIME
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     time VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE RESERVATION
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    date VARCHAR(255) NOT NULL,
+    time_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES TIME(id)
+    );
