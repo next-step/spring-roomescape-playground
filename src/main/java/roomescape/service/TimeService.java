6efef2906c4af4bbc28d3006bde43778f594ec6f
장서인 <seoin1893@dankook.ac.kr>
2026-09-11 17:39:14 +0900

    feat : 시간 서비스 클래스 추가

---
+++ src/main/java/roomescape/service/TimeService.java
@@ -0,0 +1,46 @@
+package roomescape.service;
+
+import java.time.LocalTime;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+import roomescape.domain.Time;
+import roomescape.repository.TimeRepository;
+
+@Service
+public class TimeService {
+
+    private final TimeRepository timeRepository;
+
+    public TimeService(TimeRepository timeRepository) {
+        this.timeRepository = timeRepository;
+    }
+
+    public List<Time> getTimes() {
+        return timeRepository.getTimes();
+    }
+
+    public Time createTime(LocalTime time) {
+
+        if (time == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return timeRepository.saveTime(time);
+    }
+
+    public Time getTime(long id) {
+        return timeRepository.getTime(id)
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    public void deleteTime(long id) {
+
+        int deletedCount = timeRepository.deleteTime(id);
+
+        if (deletedCount == 0) {
+            throw new NoSuchElementException();
+        }
+    }
+}
