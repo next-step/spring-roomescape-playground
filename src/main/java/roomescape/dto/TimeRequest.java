61db16cd57fd6bb0d85ed3b5359c6ac41be4f60a
장서인 <seoin1893@dankook.ac.kr>
2026-09-11 16:43:42 +0900

    fix : request 오류 수정

--- src/main/java/roomescape/dto/TimeRequest.java
+++ src/main/java/roomescape/dto/TimeRequest.java
@@ -1,12 +1,16 @@
 package roomescape.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalTime;
 
 public class TimeRequest {
 
     private final LocalTime time;
 
-    public TimeRequest(LocalTime time) {
+    @JsonCreator
+    public TimeRequest(@JsonProperty("time") LocalTime time) {
         this.time = time;
     }
 
