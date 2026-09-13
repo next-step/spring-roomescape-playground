49cf0ec240e8e23acfddb4fbbcbe95561c28eae4
장서인 <seoin1893@dankook.ac.kr>
2026-09-13 22:00:23 +0900

    fix : LocalTime 형식 수정

--- src/main/java/roomescape/dto/TimeResponse.java
+++ src/main/java/roomescape/dto/TimeResponse.java
@@ -1,10 +1,10 @@
 package roomescape.dto;
 
 import roomescape.domain.Time;
-
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 
-public record TimeResponse(long id, LocalTime time) {
+public record TimeResponse(long id, @JsonFormat(pattern = "HH:mm") LocalTime time) {
     public static TimeResponse convert(Time time) {
         return new TimeResponse(
                 time.getId(),
