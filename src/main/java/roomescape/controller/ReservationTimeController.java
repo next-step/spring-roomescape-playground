b6bcff51c903f1f23215aaf93819eac5bdaa17c1
juhee0223 <pjuhee23@dankook.ac.kr>
2026-09-14 00:06:54 +0900

    refactor: 요청/응답 DTO를 controller.dto 패키지로 이동

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import roomescape.domain.ReservationTime;
-import roomescape.dto.ReservationTimeRequest;
-import roomescape.dto.ReservationTimeResponse;
+import roomescape.controller.dto.ReservationTimeRequest;
+import roomescape.controller.dto.ReservationTimeResponse;
 import roomescape.service.ReservationTimeService;
 
 @Controller
--- src/main/java/roomescape/dto/ReservationTimeRequest.java
+++
@@ -1,9 +0,0 @@
-package roomescape.dto;
-
-import jakarta.validation.constraints.NotNull;
-import java.time.LocalTime;
-
-public record ReservationTimeRequest(
-        @NotNull(message = "시간은 비워둘 수 없습니다.")
-        LocalTime time) {
-}
