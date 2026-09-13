720644d6a32081e00fb4e376926c05c41a46e237
장서인 <seoin1893@dankook.ac.kr>
2026-09-13 11:54:48 +0900

    feat : ReservationResponse 클래스 작성

---
+++ src/main/java/roomescape/dto/ReservationResponse.java
@@ -0,0 +1,51 @@
+package roomescape.dto;
+
+import roomescape.domain.Reservation;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ReservationResponse {
+
+    private final long id;
+    private final String name;
+    private final LocalDate date;
+    private final LocalTime time;
+
+    public ReservationResponse(
+            long id,
+            String name,
+            LocalDate date,
+            LocalTime time
+    ) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public static ReservationResponse convert(Reservation reservation) {
+        return new ReservationResponse(
+                reservation.getId(),
+                reservation.getName(),
+                reservation.getDate(),
+                reservation.getTime().getTime()
+        );
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+}
